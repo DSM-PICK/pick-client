c051b42378536a9794b7317653178515258ff475
YeongG <yeonggil223@gmail.com>
2020-08-04 16:50:40 +0900

    [CREATE] ScheduleContainer 생성

---
+++ src/container/Schedule/ScheduleContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ScheduleHeader from "../../component/ScheduleHeader/ScheduleHeader";
+import Calander from "../../component/Calander/Calander";
+
+const ScheduleContainer = () => {
+  return (
+    <>
+      <ScheduleHeader />
+      <Calander />
+    </>
+  );
+};
+
+export default ScheduleContainer;
