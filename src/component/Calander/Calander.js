3256a466f64ccc9ae787921e6d6edca66c57346c
YeongG <yeonggil223@gmail.com>
2020-08-06 09:02:57 +0900

    [FIX] Calander 컴포넌트 수정

--- src/component/Calander/Calander.js
+++ src/component/Calander/Calander.js
@@ -1,7 +1,15 @@
 import React from "react";
+import * as S from "./styles";
+import CalanderHeader from "./header/CalanderHeader";
+import CalanderBody from "./body/CalanderBody";
 
 const Calander = () => {
-  return <div></div>;
+  return (
+    <S.Container>
+      <CalanderHeader />
+      <CalanderBody />
+    </S.Container>
+  );
 };
 
 export default Calander;
