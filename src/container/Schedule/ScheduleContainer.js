9272cd47340c443c3883e6f3525f8f35ad57ff7c
YeongG <yeonggil223@gmail.com>
2020-08-06 09:01:04 +0900

    [FIX] ScheduleContainer 수정

--- src/container/Schedule/ScheduleContainer.js
+++ src/container/Schedule/ScheduleContainer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ScheduleHeader from "../../component/ScheduleHeader/ScheduleHeader";
 import Calander from "../../component/Calander/Calander";
+import * as G from "../../GlobalStyle";
 
 const ScheduleContainer = () => {
   return (
-    <>
+    <G.GlobalContainer>
       <ScheduleHeader />
       <Calander />
-    </>
+    </G.GlobalContainer>
   );
 };
 
