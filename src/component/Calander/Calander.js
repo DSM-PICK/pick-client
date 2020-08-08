07adcf3ac10ca61997bdd5e0da498dabb2951d61
YeongG <yeonggil223@gmail.com>
2020-08-08 14:38:15 +0900

    [FIX] CalanderFunctionButton 랜더링

--- src/component/Calander/Calander.js
+++ src/component/Calander/Calander.js
@@ -2,12 +2,14 @@ import React from "react";
 import * as S from "./styles";
 import CalanderHeader from "./header/CalanderHeader";
 import CalanderBody from "./body/CalanderBody";
+import FunctionButton from "./FunctionButton/FunctionButton";
 
 const Calander = () => {
   return (
     <S.Container>
       <CalanderHeader />
       <CalanderBody />
+      <FunctionButton />
     </S.Container>
   );
 };
