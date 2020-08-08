c06c675c28744695e5f49bffacde40d32d6ddef3
YeongG <yeonggil223@gmail.com>
2020-08-08 16:13:03 +0900

    [FIX] img 추가

---
+++ src/asset/index.js
@@ -0,0 +1,3 @@
+export { default as MenuAddIcon } from "./schedule/MenuAddIcon.png";
+export { default as FocusIcon } from "./schedule/FocusIcon.png";
+export { default as ChangeScheduleIcon } from "./schedule/ChangeScheduleIcon.png";
