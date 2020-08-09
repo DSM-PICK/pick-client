64df88c4ea8600f437f20597e1b9a355feab692c
YeongG <yeonggil223@gmail.com>
2020-08-09 23:08:46 +0900

    [FIX] Dispatch 랜더링 함수 최적화

--- src/component/Calander/FunctionButton/HiddenMenu/HiddenMenu.js
+++ src/component/Calander/FunctionButton/HiddenMenu/HiddenMenu.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useCallback, useEffect, memo } from "react";
 import * as S from "../styles";
 import HiddenMenuItem from "./HiddenMenuItem/HiddenMenuItem";
 import { ChangeScheduleIcon, FocusIcon } from "../../../../asset";
+import { useDispatch, useSelector } from "react-redux";
+import { showMySchedule } from "../../../../module/action/calander";
 
 const HiddenMenu = ({ isOpen }) => {
+  const { state } = useSelector((state) => state.calander);
+  const dispatch = useDispatch();
+
+  const dispatchShowMySchedule = useCallback(() => {
+    if (state === "SHOW_MY_SCHEDULE") return;
+    dispatch(showMySchedule("오유신"));
+  }, [dispatch, state]);
+
   return (
     <S.AnimationWrap isOpen={isOpen}>
-      <HiddenMenuItem content="내 일정 보기" src={FocusIcon} />
+      <HiddenMenuItem
+        onClick={dispatchShowMySchedule}
+        content="내 일정 보기"
+        src={FocusIcon}
+      />
       <HiddenMenuItem content="일정 교체" src={ChangeScheduleIcon} />
     </S.AnimationWrap>
   );
