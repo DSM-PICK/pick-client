7181555edbae1a5b71c87e1dd8eaa1e5dac18c31
CutyApple <dbtldhs0915@naver.com>
2020-08-11 13:51:34 +0900

    [CREATE] <AttendanceRouting.js> 출석 라우팅 파일 분할

--- src/container/Attendance/AttendanceContainer.js
+++ src/container/Attendance/AttendanceContainer.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import * as G from '../../GlobalStyle';
 import { Switch, Route } from 'react-router-dom';
-import Attendance from '../../component/Attendance/Attendance';
-import AttendanceClass from '../../component/Attendance/ClassAttendance';
-import AttendanceClub from '../../component/Attendance/ClubAttendance';
+import AttendanceRouting from '../../component/Attendance/AttendanceRouting';
 
 const AttendanceContainer = () => {
 	return (
 		<G.GlobalContainer>
-			<Switch>
-				<Route exact path="/attendance" component={Attendance} />
-				<Route path="/attendance/class" component={AttendanceClass} />
-				<Route path="/attendance/club" component={AttendanceClub} />
-			</Switch>
+			<AttendanceRouting />
 		</G.GlobalContainer>
 	);
 };
