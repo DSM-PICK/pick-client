e890c0a4669d265fc4a37f76a5f83db03896e4c2
CutyApple <dbtldhs0915@naver.com>
2020-08-12 11:38:37 +0900

    [ADD] <Constant.js> imgOnLink 속성 추가

--- src/component/Footer/Constant.js
+++ src/component/Footer/Constant.js
@@ -4,13 +4,19 @@ export const FooterNav = [
 		imgOnLink: '../../../src/asset/Footer/homeOn.png',
 		link: './main',
 	},
-	{ imgLink: '../../../src/asset/Footer/calender.png', link: './schedule' },
+	{
+		imgLink: '../../../src/asset/Footer/calender.png',
+		imgOnLink: '../../../src/asset/Footer/calenderOn.png',
+		link: './schedule',
+	},
 	{
 		imgLink: '../../../src/asset/Footer/bugreport.png',
+		imgOnLink: '../../../src/asset/Footer/bugreportOn.png',
 		link: './attendance/class',
 	},
 	{
 		imgLink: '../../../src/asset/Footer/tonggei.png',
+		imgOnLink: '../../../src/asset/Footer/tonggeiOn.png',
 		link: './attendance/club',
 	},
 ];
