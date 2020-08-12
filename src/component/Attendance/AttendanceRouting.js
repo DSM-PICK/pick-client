527cd9c0bf1fd3de920845f8b24469e818c7473d
CutyApple <dbtldhs0915@naver.com>
2020-08-12 09:31:12 +0900

    [FIX] <AttendanceRouting.js> 라우팅 설정

--- src/component/Attendance/AttendanceRouting.js
+++ src/component/Attendance/AttendanceRouting.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import * as S from './styles';
 import { Switch, Route } from 'react-router-dom';
-import Attendance from '../../component/Attendance/Attendance';
 import AttendanceClass from '../../component/Attendance/ClassAttendance';
 import AttendanceClub from '../../component/Attendance/ClubAttendance';
 
@@ -9,7 +8,6 @@ const AttendanceRouting = () => {
 	return (
 		<S.Container>
 			<Switch>
-				<Route exact path="/attendance" component={Attendance} />
 				<Route path="/attendance/class" component={AttendanceClass} />
 				<Route path="/attendance/club" component={AttendanceClub} />
 			</Switch>
