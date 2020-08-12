673e732f4432e660e60e27321aa9402a14aace49
YeongG <yeonggil223@gmail.com>
2020-08-12 14:11:47 +0900

    [FIX] redux saga 연결

--- src/module/store/index.js
+++ src/module/store/index.js
@@ -1,7 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSaga from "redux-saga";
+import rootSaga from "../saga";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware()));
+const saga = createSaga();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(saga))
+);
+
+saga.run(rootSaga);
 
 export default store;
