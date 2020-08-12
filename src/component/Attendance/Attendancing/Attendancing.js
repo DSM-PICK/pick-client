f7dad073a401d3cf35724e348205cea4bd1b5586
CutyApple <dbtldhs0915@naver.com>
2020-08-12 21:38:51 +0900

    [ADD] <Attendancing.js> UI

--- src/component/Attendance/Attendancing/Attendancing.js
+++ src/component/Attendance/Attendancing/Attendancing.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import * as S from './styles';
+import Header from '../../Header/Header';
 
 const Attendancing = () => {
-	return <S.Container></S.Container>;
+	return (
+		<S.Container>
+			<Header></Header>
+		</S.Container>
+	);
 };
 
 export default Attendancing;
