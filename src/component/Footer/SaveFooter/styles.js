fd1d1a29a92c84b44104b6464066f03b10470488
CutyApple <dbtldhs0915@naver.com>
2020-08-12 13:16:51 +0900

    [CREATE] <SaveFooter.js> files init

---
+++ src/component/Footer/SaveFooter/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100px;
+	background: #f5f5f5;
+	margin-top: auto;
+	align-items: center;
+	justify-content: center;
+`;
