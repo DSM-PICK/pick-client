0e8e6838c7a3a7ab941cf9bb57b5fd387e325dd1
CutyApple <dbtldhs0915@naver.com>
2020-08-13 08:58:07 +0900

    [CREATE] <AttendanceNav.js> files init

---
+++ src/component/Attendance/Attendancing/AttendanceNav/AttendanceNav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as S from './styles';
+
+const AttendanceNav = (props) => {
+	const { classes } = props;
+
+	console.log(classes);
+
+	return (
+		<S.Container>
+			{/* {
+            classes.map(class => 
+                    
+            )
+        } */}
+		</S.Container>
+	);
+};
+
+export default AttendanceNav;
---
+++ src/component/Attendance/Attendancing/AttendanceNav/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	padding: 24px 0;
+	border-bottom: 1px solid #c4c4c4;
+`;
