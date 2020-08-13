04b151561a2528dc2b30f4345dd1a8e8707658e1
YeongG <yeonggil223@gmail.com>
2020-08-13 16:53:59 +0900

    [FIX] 라우팅 변경

--- src/Routing.js
+++ src/Routing.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import ScheduleContainer from './container/Schedule/ScheduleContainer';
-import AttendanceContainer from './container/Attendance/AttendanceContainer';
-import Main from './component/Main/Main';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ScheduleContainer from "./container/Schedule/ScheduleContainer";
+import AttendanceContainer from "./container/Attendance/AttendanceContainer";
+import Main from "./component/Main/Main";
+import IndexContainer from "./container/Index/IndexContainer";
 
 const Routing = () => {
-	return (
-		<Switch>
-			<Route exact path="/schedule" component={ScheduleContainer} />
-
-			<Route path="/attendance" component={AttendanceContainer} />
-			<Route path="/main" component={Main} />
-			{/* 404PAGE <Route component={NotFound} /> */}
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route exact path="/schedule" component={ScheduleContainer} />
+      <Route exact path="/attendance" component={AttendanceContainer} />
+      <Route exact path="/main" component={Main} />
+      <Route exact path="/" component={IndexContainer} />
+      {/* 404PAGE <Route component={NotFound} /> */}
+    </Switch>
+  );
 };
 
 export default Routing;
