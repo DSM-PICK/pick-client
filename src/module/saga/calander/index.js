c7b4c772005e4b3c83cf53afb38190bd4cdccc73
YeongG <yeonggil223@gmail.com>
2020-08-13 10:42:19 +0900

    [FIX] Redux calander saga 로직

--- src/module/saga/calander/index.js
+++ src/module/saga/calander/index.js
@@ -1,10 +1,12 @@
-import { takeEvery, select, put } from "redux-saga/effects";
+import { takeEvery, select, put, call } from "redux-saga/effects";
 import {
-  SELECT_DAY_SAGA,
-  selectDay,
-  SELECT_FIRST_DAY,
   SELECT_FIRST_DAY_SAGA,
   selectFirstDay,
+  RESOLVE_CHANGE_TEACHER_SAGA,
+  REJECT_CHANGE_TEACHER_SAGA,
+  defaultStatus,
+  failChangeTeacher,
+  successChangeTeacher,
 } from "../../action/calander";
 
 function* selectFirstDaySaga({ payload }) {
@@ -20,8 +22,26 @@ function* selectFirstDaySaga({ payload }) {
   yield put(selectFirstDay(payloadDate));
 }
 
+function* rejectChangeTeacherSaga() {
+  yield put(defaultStatus());
+}
+
+function* resolveChangeTeacherSaga() {
+  try {
+    // yield call(axios.post, "URL");
+    yield put(defaultStatus());
+    yield successChangeTeacher(200);
+    alert("성공하였습니다");
+  } catch (err) {
+    yield put(failChangeTeacher(err.response.status));
+    alert("실패했습니다");
+  }
+}
+
 function* calanderSaga() {
   yield takeEvery(SELECT_FIRST_DAY_SAGA, selectFirstDaySaga);
+  yield takeEvery(RESOLVE_CHANGE_TEACHER_SAGA, resolveChangeTeacherSaga);
+  yield takeEvery(REJECT_CHANGE_TEACHER_SAGA, rejectChangeTeacherSaga);
 }
 
 export default calanderSaga;
