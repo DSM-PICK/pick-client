2c3239f34aa3c0956c21c71b48a2fafce7f89ea4
YeongG <yeonggil223@gmail.com>
2020-08-13 10:39:49 +0900

    [FIX] ArrowIcon 이미지 추가

--- src/asset/index.js
+++ src/asset/index.js
@@ -1,3 +1,4 @@
 export { default as MenuAddIcon } from "./schedule/MenuAddIcon.png";
 export { default as FocusIcon } from "./schedule/FocusIcon.png";
 export { default as ChangeScheduleIcon } from "./schedule/ChangeScheduleIcon.png";
+export { default as arrowIcon } from "./schedule/arrowIcon.png";
