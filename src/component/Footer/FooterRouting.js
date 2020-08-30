f16f5fef02117056adbdfc3675c9a9d7ed761108
YeongG <yeonggil223@gmail.com>
2020-08-31 03:36:02 +0900

    [MODIFY] Footer 수정

--- src/component/Footer/FooterRouting.js
+++ src/component/Footer/FooterRouting.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import NavFooter from './NavFooter/NavFooter';
-import SaveFooter from './SaveFooter/SaveFooter';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import NavFooter from "./NavFooter/NavFooter";
+import SaveFooter from "./SaveFooter/SaveFooter";
 
 const FooterRouting = () => {
-	return (
-		<Switch>
-			<Route
-				path={['/main', '/calendar', '/bugreport', '/tonggei']}
-				component={NavFooter}
-			/>
-			<Route
-				exact
-				path={['/attendance/class', '/attendance/club']}
-				component={NavFooter}
-			/>
-			<Route
-				path={['/attendance/class/', '/attendance/club/']}
-				component={SaveFooter}
-			/>
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route
+        path={["/main", "/calendar", "/bugreport", "/tonggei", "/schedule"]}
+        component={NavFooter}
+      />
+      <Route
+        exact
+        path={["/attendance/class", "/attendance/club"]}
+        component={NavFooter}
+      />
+      <Route
+        path={["/attendance/class/", "/attendance/club/"]}
+        component={SaveFooter}
+      />
+    </Switch>
+  );
 };
 
 export default FooterRouting;
