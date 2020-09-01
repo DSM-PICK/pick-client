ea1aa667952c05317e8e65a349f279015b9a555a
CutyApple <dbtldhs0915@naver.com>
2020-09-01 20:27:24 +0900

    [CREATE] <PreReportName.js> File init

---
+++ src/component/Main/PreReport/PreReportName/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.input`
+	position: absolute;
+	display: flex;
+	left: 80px;
+	width: 140px;
+	height: 27px;
+	border: none;
+	border-bottom: 1px solid #707070;
+	background: #fafafa;
+	color: black;
+	font-size: 24px;
+	text-align: center;
+`;
