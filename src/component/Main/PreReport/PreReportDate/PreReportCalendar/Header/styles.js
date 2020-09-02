a218fc4f02faaba24d5a7f124dfa0a8020013477
CutyApple <dbtldhs0915@naver.com>
2020-09-02 21:22:38 +0900

    [ADD] <Header.js> UI

--- src/component/Main/PreReport/PreReportDate/PreReportCalendar/Header/styles.js
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/Header/styles.js
@@ -3,3 +3,18 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	display: flex;
 `;
+
+export const DayHeader = styled.div`
+	flex: 1;
+	font-size: 16px;
+	padding: 1px 0 6px 4px;
+
+	&:nth-child(n) {
+		border-right: 1px solid #c4c4c4;
+	}
+	&:nth-last-child() {
+		border: none;
+	}
+
+	box-sizing: border-box;
+`;
