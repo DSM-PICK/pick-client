a8f07baed10fb8ff03890a0f922c32479a94ad48
CutyApple <dbtldhs0915@naver.com>
2020-09-02 21:16:51 +0900

    [ADD] <PreReportDate.js> UI

--- src/component/Main/PreReport/PreReportDate/PreReportDate.js
+++ src/component/Main/PreReport/PreReportDate/PreReportDate.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
+import PreReportCalander from './PreReportCalendar/PreReportCalander';
 
 const PreReportDate = () => {
+	const [preMonth, setPreMonth] = useState('');
+	const [preDay, setPreMDay] = useState('');
+	const [preClassValue, setPreClassValue] = useState('');
+	const [nextMonth, setNextMonth] = useState('');
+	const [nextDay, setNextDay] = useState('');
+	const [nextClassValue, setNextClassValue] = useState('');
+
+	const onPreClick = () => {};
+
+	const onNextClick = () => {};
+
 	return (
 		<S.Container>
-			<S.Date>
-				<S.Month>월</S.Month>
-				<S.Day>일</S.Day>
-				<S.Class>교시</S.Class>
+			<S.Date onClick={onPreClick}>
+				<S.Month month={preMonth}>월</S.Month>
+				<S.Day day={preDay}>일</S.Day>
+				<S.Class classValue={preClassValue}>교시</S.Class>
 				<S.Additional>~</S.Additional>
 			</S.Date>
-			<S.Date>
-				<S.Month month={1}>월</S.Month>
-				<S.Day day={2}>일</S.Day>
-				<S.Class classValue={3}>교시</S.Class>
+			<S.Date onClick={onNextClick}>
+				<S.Month month={nextMonth}>월</S.Month>
+				<S.Day day={nextDay}>일</S.Day>
+				<S.Class classValue={nextClassValue}>교시</S.Class>
 			</S.Date>
+			<PreReportCalander />
 		</S.Container>
 	);
 };
