10fa1538c44301f447153736288b7c2146bd34a1
CutyApple <dbtldhs0915@naver.com>
2020-09-02 21:17:01 +0900

    [CREATE] <Item.js> File init

---
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/Item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+const Item = (props) => {
+	const { isActive, date } = props;
+
+	return <S.Container></S.Container>;
+};
+
+export default Item;
