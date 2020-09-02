bda516e8bdc17d8f2085b1cf7003a231c7c7ca69
CutyApple <dbtldhs0915@naver.com>
2020-09-02 22:27:22 +0900

    [FIX] <PreReportCalander.js> CSS 변경

--- src/component/Main/PreReport/PreReportDate/PreReportCalendar/PreReportCalander.js
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/PreReportCalander.js
@@ -2,20 +2,48 @@ import React, { useState } from 'react';
 import * as S from './styles';
 import Body from './Body/Body';
 import Header from './Header/Header';
+import getDateObj from '../../../../../lib/calander';
 
 const PreReportCalander = () => {
 	const date = new Date();
-	const [month, setMonth] = useState(date.getMonth() + 1);
+	const [year, setYear] = useState(date.getFullYear());
+	const [month, setMonth] = useState(date.getMonth());
+	const [today, setToday] = useState(date.getDate());
+	const [calDate, setCalDate] = useState(getDateObj(year, month));
+
+	const checkDate = () => {
+		if (month < 0) {
+			setYear(year - 1);
+			setMonth(11);
+		} else if (month > 11) {
+			setYear(year + 1);
+			setMonth(0);
+		}
+	};
+
+	const prevMonth = () => {
+		setMonth(month - 1);
+		checkDate();
+		setCalDate(getDateObj(year, month));
+	};
+
+	const nextMonth = () => {
+		setMonth(month + 1);
+		checkDate();
+		setCalDate(getDateObj(year, month));
+	};
+
+	console.log(year, month, today, calDate);
 
 	return (
 		<S.Container>
 			<S.Month>
-				<S.MonthArrow />
-				<S.MonthText>{month}월</S.MonthText>
-				<S.MonthArrow />
+				<S.MonthArrow onClick={prevMonth} />
+				<S.MonthText>{month + 1}월</S.MonthText>
+				<S.MonthArrow onClick={nextMonth} />
 			</S.Month>
 			<Header />
-			<Body />
+			<Body calDate={calDate} today={today} month={month + 1} />
 		</S.Container>
 	);
 };
