c97a27bd270a4272289030eac3386317b8e99cfa
CutyApple <dbtldhs0915@naver.com>
2020-09-02 22:28:06 +0900

    [FIX] <Item.js> CSS 변경

--- src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/Item.js
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/Item.js
@@ -2,9 +2,18 @@ import React from 'react';
 import * as S from './styles';
 
 const Item = (props) => {
-	const { isActive, date } = props;
+	const now = new Date();
+	const { isActive, date, today, month } = props;
+	const isToday =
+		date == today ? (now.getMonth() + 1 == month ? true : false) : false;
 
-	return <S.Container isActive={isActive}>{date}</S.Container>;
+	return (
+		<S.Container>
+			<S.Date isActive={isActive} isToday={isToday}>
+				{date}
+			</S.Date>
+		</S.Container>
+	);
 };
 
 export default Item;
--- src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/styles.js
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/styles.js
@@ -1,13 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
 	flex: 1;
 	padding: 4px 0 0 4px;
-
-	color: ${(props) => (props.isActive ? 'black' : '#c4c4c4')};
-	font-size: 18px;
-	font-weight: bold;
 
 	&:nth-child(n) {
 		border-right: 1px solid #c4c4c4;
@@ -15,4 +11,25 @@ export const Container = styled.div`
 	&:nth-last-child() {
 		border: none;
 	}
+`;
+
+export const Date = styled.div`
+	display: flex;
+	color: ${(props) => (props.isToday ? 'white' : 'black')};
+	color: ${(props) => !props.isActive && '#c4c4c4'};
+	font-size: 18px;
+	font-weight: bold;
+
+	width: 24px;
+	height: 24px;
+	border-radius: 50%;
+	${(props) =>
+		props.isToday &&
+		props.isActive &&
+		css`
+			background: #267dff;
+		`}
+
+	align-items: center;
+	justify-content: center;
 `;
