fcc34053838913a43fc007c2bc63a7a3554d3959
CutyApple <dbtldhs0915@naver.com>
2020-09-05 15:44:24 +0900

    [FIX] <Item.js> 날짜 선택 기능

--- src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/Item.js
+++ src/component/Main/PreReport/PreReportDate/PreReportCalendar/Body/Row/Item/Item.js
@@ -2,13 +2,20 @@ import React from 'react';
 import * as S from './styles';
 
 const Item = (props) => {
+	const { isActive, date, month } = props;
+	const { onSelectDay } = props;
+
 	const now = new Date();
-	const { isActive, date, today, month } = props;
+
 	const isToday =
-		date == today ? (now.getMonth() + 1 == month ? true : false) : false;
+		date == now.getDate()
+			? now.getMonth() + 1 == month
+				? true
+				: false
+			: false;
 
 	return (
-		<S.Container>
+		<S.Container onClick={() => onSelectDay(date)}>
 			<S.Date isActive={isActive} isToday={isToday}>
 				{date}
 			</S.Date>
