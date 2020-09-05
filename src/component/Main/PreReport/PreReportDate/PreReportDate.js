13c8593c22becbcb381aa61098b4eaf7b4e34870
CutyApple <dbtldhs0915@naver.com>
2020-09-05 16:36:14 +0900

    [FIX] <PreReportDate.js> 불필요 코드 제거

--- src/component/Main/PreReport/PreReportDate/PreReportDate.js
+++ src/component/Main/PreReport/PreReportDate/PreReportDate.js
@@ -32,98 +32,6 @@ const PreReportDate = (props) => {
 		onNextClassChange,
 	} = props;
 
-	// const date = new Date();
-
-	// const preClassInput = useRef('');
-	// const nextClassInput = useRef('');
-	// const [calcYear, setCalcYear] = useState(date.getFullYear());
-	// const [calcMonth, setCalcMonth] = useState(date.getMonth());
-	// const [calcDate, setCalcDate] = useState(getDateObj(calcYear, calcMonth));
-	// const [preMonth, setPreMonth] = useState('');
-	// const [nextMonth, setNextMonth] = useState('');
-	// const [preDay, setPreDay] = useState('');
-	// const [nextDay, setNextDay] = useState('');
-	// const [preClassValue, setPreClassValue] = useState('');
-	// const [nextClassValue, setNextClassValue] = useState('');
-	// const [modal, setModal] = useState(false);
-	// const [height, setHeight] = useState('30px');
-	// const [preNextState, setPreNextState] = useState('');
-
-	// const onOffModal = () => {
-	// 	setModal(!modal);
-	// };
-
-	// const onPreClick = () => {
-	// 	preClassInput.current.blur();
-	// 	setHeight('30px');
-	// 	setPreNextState('pre');
-	// 	onOffModal();
-	// };
-
-	// const onNextClick = () => {
-	// 	preClassInput.current.blur();
-	// 	setHeight('64px');
-	// 	setPreNextState('next');
-	// 	onOffModal();
-	// };
-
-	// const onPreSelect = (day) => {
-	// 	setPreMonth(calcMonth + 1);
-	// 	setPreDay(day);
-	// 	preClassInput.current.focus();
-	// };
-
-	// const onNextSelect = (day) => {
-	// 	setNextMonth(calcMonth + 1);
-	// 	setNextDay(day);
-	// 	nextClassInput.current.focus();
-	// };
-
-	// const prevCalcMonth = () => {
-	// 	if (calcMonth == 0) {
-	// 		setCalcYear(calcYear - 1);
-	// 		setCalcMonth(11);
-	// 	} else {
-	// 		setCalcMonth(calcMonth - 1);
-	// 	}
-	// 	setCalcDate(getDateObj(calcYear - 1, calcMonth - 1));
-	// };
-
-	// const nextCalcMonth = () => {
-	// 	if (calcMonth == 11) {
-	// 		setCalcYear(calcYear + 1);
-	// 		setCalcMonth(0);
-	// 	} else {
-	// 		setCalcMonth(calcMonth + 1);
-	// 	}
-	// 	setCalcDate(getDateObj(calcYear + 1, calcMonth + 1));
-	// };
-
-	// const onSelectDay = (day) => {
-	// 	preNextState === 'pre' ? onPreSelect(day) : onNextSelect(day);
-	// 	onOffModal();
-	// };
-
-	// const onPreClassChange = useCallback((e) => {
-	// 	if (Number(e.target.value) > 10) {
-	// 		setPreClassValue(10);
-	// 	} else if (Number(e.target.value) < 0) {
-	// 		setPreClassValue(0);
-	// 	} else {
-	// 		setPreClassValue(e.target.value);
-	// 	}
-	// }, []);
-
-	// const onNextClassChange = useCallback((e) => {
-	// 	if (Number(e.target.value) > 10) {
-	// 		setNextClassValue(10);
-	// 	} else if (Number(e.target.value) < 0) {
-	// 		setNextClassValue(0);
-	// 	} else {
-	// 		setNextClassValue(e.target.value);
-	// 	}
-	// }, []);
-
 	return (
 		<S.Container>
 			<S.Date onClick={onPreClick}>
@@ -167,7 +75,6 @@ const PreReportDate = (props) => {
 				/>
 			)}
 			{modal && <PreReportModal onOffModal={onOffModal} />}
-			{console.log(preMonth, preDay, preClassValue)}
 		</S.Container>
 	);
 };
