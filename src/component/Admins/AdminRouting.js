4c459b9a2d3d056feee944e261bfdb816aac51f9
CutyApple <dbtldhs0915@naver.com>
2020-09-10 10:55:06 +0900

    [CREATE] <AdminRouting.js> file init

---
+++ src/component/Admins/AdminRouting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './styles';
+import { Switch } from 'react-router-dom';
+
+const AdminRouting = () => {
+	return (
+		<S.Container>
+			<Switch></Switch>
+		</S.Container>
+	);
+};
+
+export default AdminRouting;
