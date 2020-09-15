388fa32aeb1b40f81cf88ca09458ab1a4c76db8a
YeongG <yeonggil223@gmail.com>
2020-09-15 18:09:36 +0900

    [MODIFY] style 수정

--- src/component/Developer/styles.js
+++ src/component/Developer/styles.js
@@ -1,9 +1,58 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 300px;
   background-color: #fafafa;
-  position: fixed;
   width: 100%;
-  bottom: 0;
+  padding: 50px 80px;
+  color: black;
+  box-sizing: border-box;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+
+  > div > div {
+    font-size: 15px;
+    margin-top: 15px;
+  }
+`;
+
+export const Field = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+export const Hr = styled.div`
+  border-top: 2px solid #c4c4c4;
+  width: 100%;
+  height: 0;
+  margin: 40px 0;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+
+  > div {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    white-space: nowrap;
+    color: #707070;
+
+    > img {
+      width: 130px;
+      margin-right: 10px;
+    }
+  }
+
+  > div:nth-child(2) {
+    justify-content: flex-end;
+
+    a {
+      margin-left: 10px;
+    }
+  }
 `;
