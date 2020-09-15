0d4431831837ba9c384c1b218ec66d2495b4352e
YeongG <yeonggil223@gmail.com>
2020-09-15 18:09:20 +0900

    [MODIFY] export default 추가

--- src/asset/index.js
+++ src/asset/index.js
@@ -2,3 +2,8 @@ export { default as MenuAddIcon } from "./schedule/MenuAddIcon.png";
 export { default as FocusIcon } from "./schedule/FocusIcon.png";
 export { default as ChangeScheduleIcon } from "./schedule/ChangeScheduleIcon.png";
 export { default as arrowIcon } from "./schedule/arrowIcon.png";
+export { default as IndexBackground } from "./login/background.png";
+export { default as IndexIcon } from "./login/icon.svg";
+export { default as PickLogo } from "./Logo/PickLogo.svg";
+export { default as GithubLogo } from "./Logo/github.svg";
+export { default as FacebookLogo } from "./logo/facebook.svg";
