2eb59dd7b59570f5d9d3f8f484e862c6c4714052
YeongG <yeonggil223@gmail.com>
2020-09-17 10:19:44 +0900

    [CREATE] ModalItem component/style 생성

---
+++ src/component/default/ModalClubItem/ModalClubItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const ModalClubItem = () => {
+  return <S.Container>1101 김민석</S.Container>;
+};
+
+export default ModalClubItem;
---
+++ src/component/default/ModalClubItem/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 15px 15px;
+  font-size: 20px;
+  border-radius: 16px;
+  border: 1px solid rgb(112, 112, 112, 0.5);
+  margin: 5px;
+`;
