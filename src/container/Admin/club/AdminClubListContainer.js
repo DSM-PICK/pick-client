ecc4ed7f706a16cd176fdab8e0c6c39db1ba5117
YeongG <yeonggil223@gmail.com>
2020-09-22 23:37:47 +0900

    [MODIFY] AdminClubList Container

--- src/container/Admin/club/AdminClubListContainer.js
+++ src/container/Admin/club/AdminClubListContainer.js
@@ -3,38 +3,38 @@ import { useDispatch } from "react-redux";
 import AdminClubList from "../../../component/Admin/club/AdminClubList";
 import { updateClubList } from "../../../module/action/club";
 
-const staticData = [
+export const staticData = [
   {
     name: "제1동아리",
-    where: "제1동아리 실",
+    where: "제1동아리 실"
   },
   {
     name: "제2동아리",
-    where: "제2동아리 실",
+    where: "제2동아리 실"
   },
   {
     name: "제3동아리",
-    where: "제3동아리 실",
+    where: "제3동아리 실"
   },
   {
     name: "제1동아리",
-    where: "제1동아리 실",
+    where: "제1동아리 실"
   },
   {
     name: "제2동아리",
-    where: "제2동아리 실",
+    where: "제2동아리 실"
   },
   {
     name: "제3동아리",
-    where: "제3동아리 실",
-  },
+    where: "제3동아리 실"
+  }
 ];
 
 const AdminClubListContainer = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(updateClubList([...staticData]));
-  }, []);
+    dispatch(updateClubList(staticData));
+  }, [staticData]);
   return <AdminClubList />;
 };
 
