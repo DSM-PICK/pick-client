c9cf52df1de22fb0a03929d6b9425615800836e4
YeongG <yeonggil223@gmail.com>
2020-10-06 23:37:23 +0900

    [MODIFY] LoginModal 폰트 변경

--- src/component/Index/styles.js
+++ src/component/Index/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Container as ModalContainer } from "./Header/Modal/styles";
 import { IndexBackground } from "../../asset";
 
 export const Container = styled.div`
@@ -10,5 +11,9 @@ export const Container = styled.div`
   background-size: cover;
   * {
     font-family: "SangSangShinb7" !important;
+  }
+
+  ${ModalContainer} * {
+    font-family: "나눔고딕" !important;
   }
 `;
