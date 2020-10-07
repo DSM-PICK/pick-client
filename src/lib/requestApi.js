6a4c724f6030a113c446196f8de704cdd960a5da
YeongG <yeonggil223@gmail.com>
2020-10-07 23:37:49 +0900

    [MODIFY] requestApi 수정

--- src/lib/requestApi.js
+++ src/lib/requestApi.js
@@ -1,33 +1,29 @@
 import axios from "axios";
 
 const BASE_URL = "http://api.dsm-pick.com";
-export const requestGetApiWithAccessToken = async (url, headers) => {
-  try {
-    const { data } = await axios.get(BASE_URL + url, {
-      headers
-    });
-    return data;
-  } catch (err) {}
+
+export const methodType = {
+  GET: "get",
+  POST: "post",
+  PUT: "put",
+  PATCH: "patch",
+  DELETE: "delete"
 };
 
-export const requestGetApi = async (method, url, headers) => {
-  try {
-    const { data } = await axios.get(BASE_URL + url, {
-      headers
-    });
+export const requestGetApiWithAccessToken = async (url, headers) =>
+  axios.get(BASE_URL + url, { headers });
 
-    return data;
-  } catch (err) {}
-};
+export const requestGetApi = async (method, url, headers) =>
+  axios.get(BASE_URL + url, { headers });
 
 export const requestApi = async (method, url, body, headers) => {
   try {
-    const res = await axios[method](BASE_URL + url, body, {
-      headers
-    });
-
+    const res = await axios[method](BASE_URL + url, body, { headers });
     return res;
-  } catch (err) {}
+  } catch (err) {
+    !err.response && alert("네트워크 상태를 확인해 주세요");
+    throw err.response;
+  }
 };
 
 export const requestApiWithAccessToken = async (method, url, body, headers) => {
@@ -39,3 +35,5 @@ export const requestApiWithAccessToken = async (method, url, body, headers) => {
     return data;
   } catch (err) {}
 };
+
+// export const isLogined = async();
