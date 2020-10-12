6ee488ca2c251b5de7903b0abfe93866f3216fb9
YeongG <yeonggil223@gmail.com>
2020-10-12 22:53:11 +0900

    [MODIFY] Calander 수정

--- src/component/Calander/Calander.js
+++ src/component/Calander/Calander.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./styles";
 import CalanderHeader from "./CalanderHeader/CalanderHeader";
 import CalanderBody from "./CalanderBody/CalanderBody";
 import FunctionButtonContainer from "./FunctionButton/FunctionButtonContainer";
+import { useDispatch } from "react-redux";
+import { getScheduleSaga } from "../../module/action/calander";
 
 const Calander = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getScheduleSaga());
+  }, []);
+
   return (
     <S.Container>
       <CalanderHeader />
