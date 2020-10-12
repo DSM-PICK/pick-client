fbeace8df683a87a352ac99a76099504465d5d10
CutyApple <dbtldhs0915@naver.com>
2020-10-12 10:11:55 +0900

    [MODIFY] <PreReport.js> style 변경

--- src/component/Main/PreReport/PreReportName/NameSelect/NameSelect.js
+++ src/component/Main/PreReport/PreReportName/NameSelect/NameSelect.js
@@ -1,50 +1,16 @@
-import React, { useState } from 'react';
-import * as S from './styles';
-import ExName from './ExName/ExName';
-import PreReportModal from '../../PreReportDate/PreReportModal/PreReportModal';
+import React, { useState } from "react";
+import * as S from "./styles";
+import ExName from "./ExName/ExName";
+import PreReportModal from "../../PreReportDate/PreReportModal/PreReportModal";
 
 const NameSelect = () => {
-	const names = [
-		{
-			name: '2201 유시온',
-		},
-		{
-			name: '2202 유시온',
-		},
-		{
-			name: '2203 유시온',
-		},
-		{
-			name: '2204 유시온',
-		},
-		{
-			name: '2202 이진혁',
-		},
-		{
-			name: '2202 손정우',
-		},
-		{
-			name: '2203 손정우',
-		},
-		{
-			name: '2204 손정우',
-		},
-	];
+  const names = [];
 
-	const [modal, setModal] = useState(false);
-
-	const onOffModal = () => {
-		setModal(!modal);
-	};
-
-	return (
-		<S.Container>
-			{names.map((name) => (
-				<ExName key={name.name} name={name} />
-			))}
-			{modal && <PreReportModal onOffModal={onOffModal} />}
-		</S.Container>
-	);
+  return (
+    <S.Container names={names}>
+      {names && names.map(name => <ExName key={name.name} name={name} />)}
+    </S.Container>
+  );
 };
 
 export default NameSelect;
