3f76051e90eaa62d2822d31ab3cf865437cad6bf
YeongG <yeonggil223@gmail.com>
2020-10-13 23:36:26 +0900

    [MODIFY] index 접근 차단

--- src/container/Index/IndexContainer.js
+++ src/container/Index/IndexContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import Developer from "../../component/Developer/Developer";
 import Index from "../../component/Index/Index";
@@ -8,11 +8,10 @@ const IndexContainer = () => {
   const history = useHistory();
   useEffect(() => {
     checkIsLogin().then(isLogin => {
-      if (isLogin) {
-        history.push("/main");
-      }
+      isLogin && history.push("/main");
     });
   }, []);
+
   return (
     <div>
       <Index />
