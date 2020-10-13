cb5555167e8ded4dfc148713a89263e68e888266
YeongG <yeonggil223@gmail.com>
2020-10-13 23:37:34 +0900

    [MODIFY] schedule 접근 차단

--- src/container/Schedule/ScheduleContainer.js
+++ src/container/Schedule/ScheduleContainer.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ScheduleHeader from "../../component/ScheduleHeader/ScheduleHeader";
 import Calander from "../../component/Calander/Calander";
 import Footer from "../../component/Footer/Footer";
 import * as G from "../../GlobalStyle";
+import { useHistory } from "react-router";
+import { checkIsLogin } from "../../lib/requestApi";
 
 const ScheduleContainer = () => {
+  const history = useHistory();
+  useEffect(() => {
+    checkIsLogin().then(isLogin => {
+      !isLogin && history.push("/");
+    });
+  }, []);
   return (
     <G.GlobalContainer>
       <ScheduleHeader />
