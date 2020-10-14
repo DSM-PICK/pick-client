6b86a8227b63d229556c9da4d01877f42a32e740
YeongG <yeonggil223@gmail.com>
2020-10-14 23:44:01 +0900

    [MODIFY] 스타일 변경

--- src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInputPW/styles.js
+++ src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInputPW/styles.js
@@ -1,28 +1,21 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-	display: flex;
-	border: none;
-	border-bottom: 1px solid #c4c4c4;
-	padding-right: 5px;
-	align-items: center;
+  display: flex;
+  border: none;
+  border-bottom: 1px solid #c4c4c4;
+  padding-right: 5px;
+  align-items: center;
 
-	margin: 25px 0;
+  margin: 25px 0;
 `;
 
 export const ShowBtn = styled.button`
-	display: flex;
-	width: 25px;
-	height: 18px;
-	border: none;
-	background: ${(props) =>
-			props.inputType === 'password'
-				? `url("src/asset/Admin/Login/show_pw.svg") `
-				: `url("src/asset/Admin/Login/show_pw.svg")`}
-		no-repeat center/100% 100%;
-	margin-left: 5px;
-
-	&:hover {
-		cursor: pointer;
-	}
+  display: flex;
+  width: 25px;
+  height: 18px;
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
 `;
--- src/component/Admin/AdminLogin/LoginSection/SectionForm/styles.js
+++ src/component/Admin/AdminLogin/LoginSection/SectionForm/styles.js
@@ -1,7 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.form`
-	display: flex;
-	align-items: center;
-	flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  div > input {
+    display: flex;
+    border: none;
+    border-bottom: 1px solid #c4c4c4;
+    padding-right: 5px;
+    align-items: center;
+    margin: 25px 0;
+  }
+`;
+
+export const PasswordWrap = styled.div`
+  position: relative;
+
+  > img {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+  }
 `;
