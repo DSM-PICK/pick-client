e2c436d57bdb3ca81ff1c91438c409387e20ca52
YeongG <yeonggil223@gmail.com>
2020-10-14 23:41:29 +0900

    [MODIFY] 라우팅 변경 & 데스크탑 접근 불가

--- src/Routing.js
+++ src/Routing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import ScheduleContainer from "./container/Schedule/ScheduleContainer";
 import AttendanceContainer from "./container/Attendance/AttendanceContainer";
@@ -6,15 +6,39 @@ import IndexContainer from "./container/Index/IndexContainer";
 import AdminRouting from "./component/Admin/AdminRouting";
 import MainContainer from "./container/Main/MainContainer";
 
+function checkDesktop() {
+  const { userAgent } = window.navigator;
+  const { pathname } = window.location;
+  if (
+    (userAgent.includes("Win64") || userAgent.includes("Win32")) &&
+    !pathname.includes("admin")
+  ) {
+    return true;
+  }
+  return false;
+}
+
 const Routing = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  // useEffect(() => {
+  //   if (checkDesktop()) {
+  //     alert("데스크랍은 이용할 수 없습니다. \n모바일로 접속해 주세요");
+  //     window.history.back();
+  //   }
+  //   setIsChecked(true);
+  // }, []);
   return (
     <Switch>
-      <Route exact path="/" component={IndexContainer} />
-      <Route exact path="/schedule" component={ScheduleContainer} />
-      <Route exact path="/main" component={MainContainer} />
+      {/* {isChecked && ( */}
+      <>
+        <Route exact path="/" component={IndexContainer} />
+        <Route exact path="/schedule" component={ScheduleContainer} />
+        <Route exact path="/main" component={MainContainer} />
 
-      <Route path="/attendance" component={AttendanceContainer} />
-      <Route path="/admin" component={AdminRouting} />
+        <Route path="/attendance" component={AttendanceContainer} />
+        <Route path="/admin" component={AdminRouting} />
+      </>
+      )}
     </Switch>
   );
 };
