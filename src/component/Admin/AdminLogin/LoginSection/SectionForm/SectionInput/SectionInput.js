14e1d271e5bb00434ae6a9ced836f6d0ecab9db2
YeongG <yeonggil223@gmail.com>
2020-10-14 23:43:48 +0900

    [MODIFY] props 추가

--- src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInput.js
+++ src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInput.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import * as S from './styles';
+import React from "react";
+import * as S from "./styles";
 
-const SectionInput = (props) => {
-	const { type, ph_text, minus_width } = props;
-
-	return (
-		<S.Container placeholder={ph_text} minus_width={minus_width} type={type} />
-	);
+const SectionInput = props => {
+  const {
+    type,
+    ph_text,
+    minus_width,
+    customRef,
+    onChange,
+    name,
+    value
+  } = props;
+  return (
+    <S.Container
+      value={value}
+      name={name}
+      onChange={onChange}
+      placeholder={ph_text}
+      minus_width={minus_width}
+      ref={customRef || undefined}
+      type={type}
+    />
+  );
 };
 
 export default SectionInput;
