7fa23aec6754aba63475a2a6d3d6bd2769a682ee
YeongG <yeonggil223@gmail.com>
2020-10-14 23:43:24 +0900

    [MODIFY] redux club action / 리스트 불러오기 생성

--- src/module/action/club/index.js
+++ src/module/action/club/index.js
@@ -1,12 +1,23 @@
 export const UPDATE_CLUB_LIST = "club/UPDATE_CLUB_LIST";
-export const UPDATE_CLUB_DETAIL = "club/UPDATE_CLUB_DETAIL";
+export const UPDATE_CLUB_LIST_SAGA = "club/UPDATE_CLUB_LIST_SAGA";
 
-export const updateClubList = (payload) => ({
+export const UPDATE_CLUB_DETAIL = "club/UPDATE_CLUB_DETAIL";
+export const UPDATE_CLUB_DETAIL_SAGA = "club/UPDATE_CLUB_DETAIL_SAGA";
+
+export const updateClubList = payload => ({
   type: UPDATE_CLUB_LIST,
-  payload,
+  payload
 });
 
-export const updateClubDetail = (payload) => ({
+export const updateClubListSaga = () => ({
+  type: UPDATE_CLUB_LIST_SAGA
+});
+
+export const updateClubDetail = payload => ({
   type: UPDATE_CLUB_DETAIL,
-  payload,
+  payload
+});
+
+export const updateClubDetailSaga = () => ({
+  type: UPDATE_CLUB_DETAIL_SAGA
 });
