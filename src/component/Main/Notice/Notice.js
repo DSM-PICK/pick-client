d6b98b7b7ec4dc08b713d42b60c95d0c42d74649
CutyApple <dbtldhs0915@naver.com>
2020-10-15 16:50:35 +0900

    [CREATE] <Main/Notice.js> 동아리 현황

---
+++ src/component/Main/Notice/Notice.js
@@ -0,0 +1,49 @@
+import React from "react";
+import * as S from "./styles";
+
+const Notice = () => {
+  return (
+    <S.Container>
+      <S.Section>
+        <S.Header>동아리원</S.Header>
+        <S.InformationWrap>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+          <S.Information>
+            김민석 학생이 GRAM에서 UP(으)로 이동했습니다
+          </S.Information>
+        </S.InformationWrap>
+      </S.Section>
+      <S.Section>
+        <S.Header>동아리</S.Header>
+      </S.Section>
+    </S.Container>
+  );
+};
+
+export default Notice;
---
+++ src/component/Main/Notice/styles.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  width: calc(100% - 40px);
+  height: 260px;
+  margin: 10px 0;
+  border-radius: 60px;
+  box-shadow: 0 6px 9px 0 rgba(0, 0, 0, 0.16);
+  background: #fafafa;
+  padding: 20px !important;
+  align-items: center;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 50%;
+    width: 1px;
+    height: 100%;
+    background: #707070;
+  }
+`;
+
+export const Section = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  color: #000000;
+  font-size: 24px;
+  align-items: center;
+`;
+
+export const InformationWrap = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  flex-direction: column;
+`;
+
+export const Information = styled.p`
+  display: flex;
+  width: 100%;
+  height: auto;
+  padding: 3px;
+  color: #000000;
+  font-size: 22px;
+  text-align: left;
+`;
