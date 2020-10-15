14f229c6bc711d828e0f9439b229f8b427a219a5
CutyApple <dbtldhs0915@naver.com>
2020-10-15 15:19:24 +0900

    [MODIFY] <AttendanceCell> Style 변경

--- src/component/Attendance/body/anchor/AttendanceAnchor.js
+++ src/component/Attendance/body/anchor/AttendanceAnchor.js
@@ -2,7 +2,10 @@ import React, { useCallback, useEffect } from "react";
 import * as S from "./styles";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { getFloorDataSaga } from "../../../../module/action/attendance";
+import {
+  getAttendanceStdDataSaga,
+  getFloorDataSaga
+} from "../../../../module/action/attendance";
 import { getLocationState } from "../../../../lib/attendanceAPI";
 
 const AttendanceBody = props => {
@@ -16,6 +19,14 @@ const AttendanceBody = props => {
     },
     [dispatch]
   );
+  const getAttendanceStdDate = useCallback(
+    floor => {
+      dispatch(getAttendanceStdDataSaga({ floor, priority: 0 }));
+    },
+    [dispatch]
+  );
+
+  const floor = link.split("/")[3][link.split("/")[3].length - 1];
 
   useEffect(() => {
     if (getLocationState() === "class" || getLocationState() === "club") {
@@ -24,7 +35,13 @@ const AttendanceBody = props => {
   }, []);
 
   return (
-    <S.Container as={Link} to={link} url={imgLink} text={text}>
+    <S.Container
+      as={Link}
+      to={link}
+      url={imgLink}
+      text={text}
+      onClick={() => getAttendanceStdDate(floor)}
+    >
       {text}
     </S.Container>
   );
