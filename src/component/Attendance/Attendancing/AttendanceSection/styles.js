06f636c784f743f9e2ccdd7939b9f5083e783835
CutyApple <dbtldhs0915@naver.com>
2020-10-18 00:58:13 +0900

    .

--- src/component/Attendance/Attendancing/AttendanceSection/styles.js
+++ src/component/Attendance/Attendancing/AttendanceSection/styles.js
@@ -1,45 +1,45 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-	display: flex;
-	height: calc(100% - 50px);
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  height: calc(100% - 50px);
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Article = styled.div`
-	display: flex;
-	width: 100%;
-	margin: 61px 0 13px 0;
-	flex-direction: column;
-	justify-content: space-around;
+  display: flex;
+  width: 100%;
+  margin: 61px 0 13px 0;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 
 export const Name = styled.div`
-	text-align: center;
-	font-size: 34px;
-	font-weight: bold;
-	color: #20223e;
+  text-align: center;
+  font-size: 34px;
+  font-weight: bold;
+  color: #20223e;
 `;
 
 export const Location = styled.div`
-	text-align: center;
-	font-size: ${(props) => (props.location === '자습실' ? '32px' : '28px')};
-	font-weight: ${(props) => (props.location === '자습실' ? 'bold' : '300')};
-	color: #20223e;
+  text-align: center;
+  font-size: ${props => (props.location === "자습실" ? "32px" : "28px")};
+  font-weight: ${props => (props.location === "자습실" ? "bold" : "300")};
+  color: #20223e;
 `;
 
 export const Head = styled.div`
-	position: absolute;
-	right: 100px;
-	font-size: 28px;
+  position: absolute;
+  right: 50px;
+  font-size: 28px;
 `;
 
 export const Attendance = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	overflow-y: auto;
-	flex-direction: column;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+  flex-direction: column;
 `;
