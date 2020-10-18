b88ee207f8e334e474c4d51412ceff0398b8c40a
CutyApple <dbtldhs0915@naver.com>
2020-10-18 22:41:02 +0900

    [MODIFY] <DeleteModal.js>

--- src/component/Main/Modal/DeleteModal.js
+++ src/component/Main/Modal/DeleteModal.js
@@ -1,10 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import * as S from "./styles";
 import { useDispatch } from "react-redux";
 import { deletePreAbsenceSaga } from "../../../module/action/pre_absence";
+import { getPreAbsenceText } from "../../../lib/attendanceAPI";
 
 const DeleteModal = props => {
-  const { curAbsenceId } = props;
+  const { curAbsenceId, preAbsenceList } = props;
   const { onOffDelModal, setCurPreAbsenceData } = props;
 
   const dispatch = useDispatch();
@@ -15,14 +16,48 @@ const DeleteModal = props => {
     dispatch(deletePreAbsenceSaga(curAbsenceId));
   }, [dispatch]);
 
+  const [selectAbsenceData, setSelectAbsenceData] = useState({
+    end_date: "",
+    end_period: "",
+    id: "",
+    name: "",
+    start_date: "",
+    start_period: "",
+    state: "",
+    stdnum: ""
+  });
+
   const onNoModalClick = event => {
     event.stopPropagation();
   };
+
+  const dataChangeText = () => {
+    const {
+      name,
+      stdnum,
+      start_date,
+      start_period,
+      end_date,
+      end_period
+    } = selectAbsenceData;
+    const textArr = [
+      `${stdnum} ${name}`,
+      ...getPreAbsenceText(start_date, start_period, end_date, end_period)
+    ];
+    return textArr;
+  };
+
+  useEffect(() => {
+    setSelectAbsenceData(
+      ...preAbsenceList.filter(data => data.id === curAbsenceId)
+    );
+  }, []);
 
   return (
     <S.Container onClick={onOffDelModal}>
       <S.Modal onClick={event => onNoModalClick(event)}>
         <S.ModalText>삭제하시겠습니까?</S.ModalText>
+        <S.ModalSubText>{dataChangeText().map(text => text)}</S.ModalSubText>
         <S.ModalBtnWrap>
           <S.ModalCancle onClick={onOffDelModal}>취소</S.ModalCancle>
           <S.ModalOkay onClick={() => onDelete(curPreAbsenceData)} as={`div`}>
