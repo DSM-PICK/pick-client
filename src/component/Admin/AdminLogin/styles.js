c5b3fe53f404b3e579eda32f3d1cacc590510148
CutyApple <dbtldhs0915@naver.com>
2020-10-20 20:17:11 +0900

    [MODIFY] <AdminLogin> 수정

--- src/component/Admin/AdminLogin/styles.js
+++ src/component/Admin/AdminLogin/styles.js
@@ -6,7 +6,11 @@ export const Container = styled.div`
   z-index: 100;
   width: 100%;
   height: 100%;
-  background: ${props => `url(${props.url})`} 100% 100%;
+  background: white;
   align-items: center;
   justify-content: center;
+`;
+
+export const ContainerBackground = styled(Container)`
+  background: ${props => `url(${props.url})`} 100% 100%;
 `;
--- src/component/Admin/AdminNav/styles.js
+++ src/component/Admin/AdminNav/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   width: 15vw;
-  min-width: 180px;
+  min-width: 220px;
   height: 100%;
   background: #202831;
   align-items: center;
