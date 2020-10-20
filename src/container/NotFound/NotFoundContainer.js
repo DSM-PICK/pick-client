647945815598cddb8bdc2dabc95ce8df08f1a2e2
CutyApple <dbtldhs0915@naver.com>
2020-10-20 12:08:00 +0900

    [MODIFY] <NOT_FOUND> 수정

--- src/container/NotFound/NotFoundContainer.js
+++ src/container/NotFound/NotFoundContainer.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import NotFound from "../../component/NotFound/NotFound";
 
 const NotFoundContainer = () => {
-  //   useEffect(() => {
-  //     alert("지원하지 않는 페이지입니다.");
-  //     window.location.href = "/main";
-  //   }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      alert("지원하지 않는 페이지입니다.");
+      window.location.href = "/main";
+    }, 500);
+  }, []);
   return <NotFound />;
 };
 
