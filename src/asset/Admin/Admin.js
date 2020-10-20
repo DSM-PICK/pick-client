2593b7820af4f02be12928b677eb3e91290959bd
CutyApple <dbtldhs0915@naver.com>
2020-10-20 19:58:11 +0900

    [MODIFY] <asset> svg 관리

--- src/asset/Admin/Admin.js
+++ src/asset/Admin/Admin.js
@@ -11,3 +11,6 @@ export { default as NOTFOUND } from "./NOT_FOUND.svg";
 export { default as NotFound } from "./NotFound.svg";
 export { default as NotFound404 } from "./NotFound404.svg";
 export { default as NotFoundText } from "./NotFoundText.svg";
+
+export { default as AdminLoginBackground } from "./Login/background.svg";
+export { default as ShowPwIcon } from "./Login/show_pw.svg";
