f50c8ac45e7d01eb4f048d75d5b2bf0c6e56083f
CutyApple <dbtldhs0915@naver.com>
2020-10-21 11:46:27 +0900

    [MODIFY] <FAVICON>

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import Favicon from "react-favicon";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./module/store";
-import { PICK_FAVICON } from "./asset/index";
 
 ReactDOM.render(
   <Provider store={store}>
-    {/* <Favicon url="./asset/favicon/favicon.ico" /> */}
+    <Favicon url="./asset/favicon/favicon.ico" />
     <App />
   </Provider>,
   document.getElementById("root")
