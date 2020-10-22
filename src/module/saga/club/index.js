8e72a9de37e7e4f6733346fdde994e4268fa29f2
YeongG <yeonggil223@gmail.com>
2020-10-22 18:14:40 +0900

    [MODIFY] index.js asset/action/reducer/saga

--- src/module/saga/club/index.js
+++ src/module/saga/club/index.js
@@ -1,4 +1,4 @@
-import { put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from "redux-saga/effects";
 import {
   CHANGE_CLUB_DATA_SAGA,
   updateClubDetail,
@@ -7,7 +7,10 @@ import {
   UPDATE_CLUB_LIST_SAGA,
   ADD_CLUB_SAGA,
   DELETE_CLUB_SAGA,
-  updateClubListSaga as updateClubListSagaCreater
+  updateClubListSaga as updateClubListSagaCreater,
+  GET_CLUB_LOCATION_SAGA,
+  getClubLocation,
+  MOVE_STUDENT_SAGA
 } from "../../action/club";
 import {
   methodType,
@@ -34,7 +37,7 @@ function* upadteClubDetailSaga(action) {
 function* changeClubDataSaga(action) {
   const { name } = action.payload;
   try {
-    const res = yield requestAdminApiWithAccessToken(
+    yield requestAdminApiWithAccessToken(
       methodType.PATCH,
       `/venus/club/${name}`,
       action.payload
@@ -45,26 +48,73 @@ function* changeClubDataSaga(action) {
 
 function* addClubSaga(action) {
   try {
-    const res = yield requestAdminApiWithAccessToken(
+    yield requestAdminApiWithAccessToken(
       methodType.POST,
       "/venus/club",
-      action.payload
+      action.payload.clubData
     );
+
+    yield requestAdminApiWithAccessToken(
+      methodType.PATCH,
+      `/venus/club/students`,
+      {
+        students_num: action.payload.member,
+        to_club_name: action.payload.clubData.name
+      }
+    );
+
     alert("추가에 성공했습니다");
+
     yield put(updateClubListSagaCreater());
-  } catch (err) {}
+  } catch (err) {
+    alert("오류가 발생했습니다");
+  }
 }
 
 function* deleteClubSaga(action) {
   try {
-    const data = action.payload.map(async clubName => {
-      const data = await requestDeleteApiWithAccessToken(
-        `/venus/club/${clubName}`
+    for (let i = 0; i < action.payload.length; i++) {
+      yield call(
+        requestDeleteApiWithAccessToken,
+        `/venus/club/${action.payload[i]}`
       );
-    });
+    }
     alert("성공적으로 삭제했습니다");
-    window.location.reload();
-  } catch (err) {}
+    yield put(updateClubListSagaCreater());
+  } catch (err) {
+    alert("오류가 발생했습니다");
+  }
+}
+
+function* getClubLocationSaga(action) {
+  try {
+    const res = yield call(
+      requestAdminGetApiWithAccessToken,
+      `/venus/locations/search?location=${action.payload}`
+    );
+    yield put(getClubLocation(res.data));
+  } catch (err) {
+    yield put(getClubLocation([]));
+  }
+}
+
+function* moveClubSaga(action) {
+  try {
+    const { to, students } = action.payload;
+    yield requestAdminApiWithAccessToken(
+      methodType.PATCH,
+      `/venus/club/students`,
+      {
+        students_num: students,
+        to_club_name: to
+      }
+    );
+
+    yield put(updateClubListSagaCreater());
+    alert("성공 했습니다");
+  } catch (err) {
+    alert("오류가 발생했습니다");
+  }
 }
 
 function* clubSaga() {
@@ -73,6 +123,8 @@ function* clubSaga() {
   yield takeEvery(CHANGE_CLUB_DATA_SAGA, changeClubDataSaga);
   yield takeEvery(ADD_CLUB_SAGA, addClubSaga);
   yield takeEvery(DELETE_CLUB_SAGA, deleteClubSaga);
+  yield takeEvery(GET_CLUB_LOCATION_SAGA, getClubLocationSaga);
+  yield takeEvery(MOVE_STUDENT_SAGA, moveClubSaga);
 }
 
 export default clubSaga;
