0e41ad923bb7fa83b49eae52e389f097d1f44bcf
YeongG <yeonggil223@gmail.com>
2020-10-22 18:17:02 +0900

    [MODIFY] sectionInput component

--- src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInput.js
+++ src/component/Admin/AdminLogin/LoginSection/SectionForm/SectionInput/SectionInput.js
@@ -2,10 +2,11 @@ import React from "react";
 import * as S from "./styles";
 
 const SectionInput = props => {
-  const { type, ph_text, onChange, name, value } = props;
+  const { type, ph_text, onChange, name, value, customRef } = props;
 
   return (
     <S.Container
+      ref={customRef}
       placeholder={ph_text}
       type={type}
       onChange={onChange}
