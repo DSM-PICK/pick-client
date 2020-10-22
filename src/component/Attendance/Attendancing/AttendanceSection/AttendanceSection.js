4835d989dd307babcee2142a89ff3a26a0cdb35a
CutyApple <dbtldhs0915@naver.com>
2020-10-22 15:59:34 +0900

    [MODIFY] <component/Attendance> style 변경

--- src/component/Attendance/Attendancing/AttendanceSection/AttendanceSection.js
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceSection.js
@@ -6,47 +6,47 @@ import { useSelector } from "react-redux";
 import { getLocationState } from "../../../../lib/attendanceApi";
 
 const AttendanceSection = props => {
-  !props.locations
-    ? getLocationState() === "club"
+  try {
+    const { location, name: clubName } = props.locations;
+
+    const information = useSelector(state => state.attendance);
+    const { clubHead, attendanceData } = information;
+
+    const isSevenNull =
+      !!Object.keys(attendanceData).length &&
+      attendanceData[0].state.seven === null;
+
+    return (
+      <S.Container>
+        {clubName !== "self-study" ? (
+          <S.Article>
+            <S.Name>{clubName}</S.Name>
+            <S.Location>{location}</S.Location>
+            {!!clubHead && <S.Head>{`부장 : ${clubHead}`}</S.Head>}
+          </S.Article>
+        ) : (
+          <S.Article>
+            <S.Location location="자습실">{`자습실`}</S.Location>
+          </S.Article>
+        )}
+        <AttendanceRowTop isSevenNull={isSevenNull} />
+        <S.Attendance>
+          {!!Object.keys(attendanceData).length &&
+            attendanceData.map((attendance, index) => (
+              <AttendanceRow
+                key={attendance.gradeClassNumber}
+                index={index}
+                attendance={attendance}
+              />
+            ))}
+        </S.Attendance>
+      </S.Container>
+    );
+  } catch (err) {
+    getLocationState() === "club"
       ? (window.location.href = "/attendance/club")
-      : (window.location.href = "/attendance/class")
-    : "";
-
-  const { location, name: clubName } = props.locations;
-
-  const information = useSelector(state => state.attendance);
-  const { clubHead, attendanceData } = information;
-
-  const isSevenNull =
-    !!Object.keys(attendanceData).length &&
-    attendanceData[0].state.seven === null;
-
-  return (
-    <S.Container>
-      {clubName !== "self-study" ? (
-        <S.Article>
-          <S.Name>{clubName}</S.Name>
-          <S.Location>{location}</S.Location>
-          {!!clubHead && <S.Head>{`부장 : ${clubHead}`}</S.Head>}
-        </S.Article>
-      ) : (
-        <S.Article>
-          <S.Location location="자습실">{`자습실`}</S.Location>
-        </S.Article>
-      )}
-      <AttendanceRowTop isSevenNull={isSevenNull} />
-      <S.Attendance>
-        {!!Object.keys(attendanceData).length &&
-          attendanceData.map((attendance, index) => (
-            <AttendanceRow
-              key={attendance.gradeClassNumber}
-              index={index}
-              attendance={attendance}
-            />
-          ))}
-      </S.Attendance>
-    </S.Container>
-  );
+      : (window.location.href = "/attendance/class");
+  }
 };
 
 export default AttendanceSection;
