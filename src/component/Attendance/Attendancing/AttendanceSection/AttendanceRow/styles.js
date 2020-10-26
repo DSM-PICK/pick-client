fd492c7269b1e0372ec9c085af07bd3ad29c3326
CutyApple <dbtldhs0915@naver.com>
2020-10-26 23:27:48 +0900

    [MODIFY] <AttendanceRow> Style 변경

--- src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/styles.js
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/styles.js
@@ -1,64 +1,66 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Containter = styled.div`
-	display: flex;
-	width: 100%;
-	height: 80px;
-	border-bottom: 1px solid #c4c4c4;
+  display: flex;
+  width: 100%;
+  height: 50px;
+  border-bottom: 1px solid #c4c4c4;
 `;
 
 export const SectionSpan = styled.span`
-	display: flex;
-	height: 100%;
-	font-size: 28px;
-	color: #20223e;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  height: 100%;
+  font-size: 16px;
+  color: #20223e;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SectionSeq = styled(SectionSpan)`
-	font-size: 24px;
-	color: #707070;
-	min-width: 54px;
+  font-size: 16px;
+  color: #707070;
+  min-width: 44px;
 `;
 
 export const SectionStdNum = styled(SectionSpan)`
-	min-width: 170px;
+  font-size: 20px;
+  min-width: 100px;
 `;
 
 export const SectionName = styled(SectionSpan)`
-	min-width: 120px;
+  font-size: 20px;
+  min-width: 100px;
 `;
 
 export const SectionClassWrap = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	align-items: center;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
 `;
 
 export const SectionClass = styled.div`
-	display: flex;
-	width: 100%;
-	height: 100%;
-	font-size: 22px;
-	color: #707070;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  font-size: 16px;
+  color: #707070;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SectionSelectClass = styled.select`
-	display: flex;
-	width: 80%;
-	height: 50%;
-	font-size: 22px;
-	color: #707070;
-	border: 1px solid #707070;
-	border-radius: 20px;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  width: 80%;
+  height: 50%;
+  font-size: 14px;
+  color: #707070;
+  border: 1px solid #707070;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SectionOptionClass = styled.option`
-	display: flex;
+  display: flex;
 `;
