6eaac1103d9d4deb1416eeb696a5065ad802fa86
CutyApple <dbtldhs0915@naver.com>
2020-10-26 22:49:30 +0900

    [MODIFY] <AttendanceNav> Style 변경

--- src/component/Attendance/Attendancing/AttendanceNav/AttendanceNavItem/styles.js
+++ src/component/Attendance/Attendancing/AttendanceNav/AttendanceNavItem/styles.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   min-width: max-content;
-  padding: 0 35px;
+  padding: 0 15px;
   color: ${props => (props.done === "done" ? "#C4C4C4" : "black")};
-  font-size: 26px;
+  font-size: 20px;
   font-weight: bold;
   text-decoration: none;
   transform: translateY(-5px);
