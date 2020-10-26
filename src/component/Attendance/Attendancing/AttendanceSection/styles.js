78032aeb0fa1b5f745cb016bbb2140c4db3aa5ef
CutyApple <dbtldhs0915@naver.com>
2020-10-26 23:01:38 +0900

    [MODIFY] <AttendanceSection> Style 변경

--- src/component/Attendance/Attendancing/AttendanceSection/styles.js
+++ src/component/Attendance/Attendancing/AttendanceSection/styles.js
@@ -11,35 +11,38 @@ export const Container = styled.div`
 export const Article = styled.div`
   display: flex;
   width: 100%;
-  margin: 61px 0 13px 0;
+  margin: 31px 0 13px 0;
   flex-direction: column;
   justify-content: space-around;
 `;
 
 export const Name = styled.div`
   text-align: center;
-  font-size: 34px;
+  font-size: 24px;
   font-weight: bold;
   color: #20223e;
 `;
 
 export const Location = styled.div`
   text-align: center;
-  font-size: ${props => (props.location === "자습실" ? "32px" : "28px")};
+  font-size: ${props => (props.location === "자습실" ? "32px" : "20px")};
   font-weight: ${props => (props.location === "자습실" ? "bold" : "300")};
+  padding-top: ${props => (props.location === "자습실" ? "0" : "10px")};
   color: #20223e;
 `;
 
 export const Head = styled.div`
   position: absolute;
   right: 50px;
-  font-size: 28px;
+  font-size: 20px;
+  font-weight: 300;
 `;
 
 export const Attendance = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
+  padding: 0 10px;
   overflow-y: auto;
   flex-direction: column;
 `;
