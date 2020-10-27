9b8dc10d806ccf32b1370c44f57e55cf873bad27
CutyApple <dbtldhs0915@naver.com>
2020-10-27 09:45:34 +0900

    [MODIFY] <Body> Style 변경

--- src/component/Body/styles.js
+++ src/component/Body/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   width: 100%;
   height: ${props =>
-    props.ismain === "main" ? "auto" : "calc(100vh - 175px)"};
+    props.ismain === "main" ? "auto" : "calc(100vh - 135px)"};
   background-color: white;
   padding: 10px 30px 30px 30px;
   border-radius: 40px 40px 0 0;
