a50a8272374537b9ffe1c1aa9d188723b037f520
CutyApple <dbtldhs0915@naver.com>
2020-11-02 17:12:46 +0900

    [MODIFY] <anchor> Style 변경

--- src/component/Attendance/body/anchor/styles.js
+++ src/component/Attendance/body/anchor/styles.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
   display: flex;
   width: 100%;
   height: ${props => (props.ismain === "main" ? "130px" : "150px")};
   margin: 10px 0;
   margin-top: ${props => (props.text === "4층" ? "30px" : "10px")};
   background: ${props => (props.url ? `url(${props.url})` : "#fafafa")}
-    no-repeat center;
+    no-repeat center/100%;
   font-size: 30px;
   color: ${props => (props.url ? "white" : "black")};
   text-decoration: none;
@@ -17,13 +18,15 @@ export const Container = styled.div`
   text-align: center;
   justify-content: center;
   flex-direction: column;
-
+  /* 
   &::before {
     content: "";
     position: absolute;
+    width: 100%;
+    height: 100%;
     top: 0;
     left: 0;
-    z-index: -1;
-    filter: blur(2.5px);
-  }
+    z-index: 1;
+    filter: blur(10px);
+  } */
 `;
