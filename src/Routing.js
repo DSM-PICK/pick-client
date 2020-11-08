2f71230bb5c351d966e1859e37868512c9aff35e
CutyApple <dbtldhs0915@naver.com>
2020-11-09 01:33:05 +0900

    [FEAT] <Routing.js> Routing 기능 추가

--- src/Routing.js
+++ src/Routing.js
@@ -6,10 +6,13 @@ import IndexContainer from "./container/Index/IndexContainer";
 import AdminRouting from "./component/Admin/AdminRouting";
 import MainContainer from "./container/Main/MainContainer";
 import NotFoundContainer from "./container/NotFound/NotFoundContainer";
+import DesktopMainContainer from "./container/Desktop/DesktopMainContainer";
 
 const Routing = () => {
   return (
     <Switch>
+      <Route exact path="/m/main" component={DesktopMainContainer} />
+
       <Route exact path="/" component={IndexContainer} />
       <Route exact path="/schedule" component={ScheduleContainer} />
       <Route exact path="/main" component={MainContainer} />
