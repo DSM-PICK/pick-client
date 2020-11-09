5f8205c60d94c27cf566bc98cef9463f09a98cf6
CutyApple <dbtldhs0915@naver.com>
2020-11-09 21:11:46 +0900

    [FEAT] <Molecules/HeaderImg> Add File

---
+++ src/component/Desktop/Molecules/Img/HeaderImg/HeaderImg.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Logo } from "../../../../../asset";
+import Img from "../../../Atoms/Img/Img";
+import * as S from "./styles";
+
+const HeaderImg = () => {
+  const img = Logo;
+
+  return (
+    <S.Container>
+      <Img src={img} width={"100px"} height={"22px"} />
+    </S.Container>
+  );
+};
+
+export default HeaderImg;
