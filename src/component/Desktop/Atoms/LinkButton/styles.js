d37616c7eba1733014175a4fb4db4609929e1443
CutyApple <dbtldhs0915@naver.com>
2020-11-09 21:01:08 +0900

    [FIX] <Atoms/LinkButton/styles.js> Change CSS Props

--- src/component/Desktop/Atoms/LinkButton/styles.js
+++ src/component/Desktop/Atoms/LinkButton/styles.js
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Container = styled(Link)`
   display: flex;
-  width: ${props => (props.css.width ? props.css.width : "auto")};
-  height: ${props => (props.css.height ? props.css.height : "auto")};
+  flex: ${props => props.css.flex};
   margin: ${props => (props.css.margin ? props.css.margin : "0")};
   padding: ${props => (props.css.padding ? props.css.padding : "0")};
-  color: ${props => (props.css.color ? props.css.color : "#ffffff")};
+  color: ${props => props.css.color};
   font-size: ${props => (props.css.fontSize ? props.css.fontSize : "15px")};
   font-weight: ${props =>
-    props.css.fontWeight ? props.css.fontWeight : "bold"};
+    props.css.fontWeight ? props.css.fontWeight : "normal"};
   border: ${props => (props.css.border ? props.css.border : "none")};
   background: ${props =>
     props.css.background ? props.css.background : "none"};
