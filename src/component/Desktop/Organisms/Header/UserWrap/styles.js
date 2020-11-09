f4343b07b1a516734561c77cb38a16272cdb8a3b
CutyApple <dbtldhs0915@naver.com>
2020-11-09 23:24:32 +0900

    [FIX] <Organisms/UserWrap/styles.js> Change css

--- src/component/Desktop/Organisms/Header/UserWrap/styles.js
+++ src/component/Desktop/Organisms/Header/UserWrap/styles.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  min-width: 86px;
+  min-width: 106px;
   height: 32px;
-  padding: 0 10px;
+  padding: 0 5px;
   border: 2px solid #ffffff;
-  border-radius: 15px;
+  border-radius: 16px;
+
+  box-sizing: border-box;
   align-items: center;
   justify-content: center;
 `;
