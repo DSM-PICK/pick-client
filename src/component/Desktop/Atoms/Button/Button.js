ba8f53475239b59ea108c46b60e33e46ceb22e32
CutyApple <dbtldhs0915@naver.com>
2020-11-09 20:44:42 +0900

    [REFACTOR] <Atoms/Button> 코드 변경

--- src/component/Desktop/Atoms/Button/Button.js
+++ src/component/Desktop/Atoms/Button/Button.js
@@ -2,18 +2,14 @@ import React from "react";
 import * as S from "./styles";
 
 const Button = props => {
-  const {
-    text,
-    width,
-    height,
-    color,
-    fontSize,
-    fontWeight,
-    background
-  } = props;
+  const { css, value } = props;
+
+  const { text, BtnClick } = value;
+  const { width, height, color, fontSize, fontWeight, background } = css;
 
   return (
     <S.Container
+      onClick={BtnClick}
       width={width}
       height={height}
       color={color}
