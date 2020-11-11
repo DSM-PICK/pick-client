8a1cf5d5d3b3b91cfe1d6a86c09226b955a38e47
CutyApple <dbtldhs0915@naver.com>
2020-11-11 11:26:57 +0900

    [FEAT] <asset/Desktop/index.js> Add Search.svg

--- src/asset/Desktop/index.js
+++ src/asset/Desktop/index.js
@@ -1,4 +1,5 @@
 export { default as UserDefaultSvg } from "./Header/UserDefaultSvg.svg";
 
-export { default as PrevBtn } from "./PreReport/PrevBtn.svg";
-export { default as NextBtn } from "./PreReport/NextBtn.svg";
+export { default as PrevBtn } from "./PreAbsence/PrevBtn.svg";
+export { default as NextBtn } from "./PreAbsence/NextBtn.svg";
+export { default as Search } from "./PreAbsence/Search.svg";
