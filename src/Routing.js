8294889de9056e3c45b6a8ddffaf6a568fa66ba3
CutyApple <dbtldhs0915@naver.com>
2020-11-16 08:17:14 +0900

    [FIX] <Routing.js> Tablet Routing

--- src/Routing.js
+++ src/Routing.js
@@ -6,19 +6,19 @@ import IndexContainer from "./container/Index/IndexContainer";
 import AdminRouting from "./component/Admin/AdminRouting";
 import MainContainer from "./container/Main/MainContainer";
 import NotFoundContainer from "./container/NotFound/NotFoundContainer";
-import DesktopPreAbsenceContainer from "./container/Desktop/DesktopPreAbsenceContainer";
+import DesktopRouting from "./container/Desktop/DesktopRouting";
 
 const Routing = () => {
   return (
     <Switch>
-      <Route exact path="/d" component={DesktopPreAbsenceContainer} />
-
       <Route exact path="/t/" component={IndexContainer} />
       <Route exact path="/t/schedule" component={ScheduleContainer} />
       <Route exact path="/t/main" component={MainContainer} />
       <Route path="/t/attendance" component={AttendanceContainer} />
 
       <Route path="/admin" component={AdminRouting} />
+
+      <Route path="/" component={DesktopRouting} />
 
       <Route path="*" component={NotFoundContainer} />
     </Switch>
