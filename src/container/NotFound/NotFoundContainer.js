c807651cd96086ebd3dcf40fb1ab4a942475dd64
CutyApple <dbtldhs0915@naver.com>
2020-11-16 01:37:52 +0900

    [FIX] <NotFoundContainer.js> Tablet Routing

--- src/container/NotFound/NotFoundContainer.js
+++ src/container/NotFound/NotFoundContainer.js
@@ -5,7 +5,7 @@ const NotFoundContainer = () => {
   useEffect(() => {
     setTimeout(() => {
       alert("지원하지 않는 페이지입니다.");
-      window.location.href = "/main";
+      window.location.href = "/t/main";
     }, 500);
   }, []);
   return <NotFound />;
