a9a65eefc8bac385a221239c1919e131843cd9c2
CutyApple <dbtldhs0915@naver.com>
2020-11-16 13:51:31 +0900

    [REFACTOR] <Atoms/WaterDropWrap>

--- src/component/Desktop/Atoms/Wrap/WaterDropWrap/styles.js
+++ src/component/Desktop/Atoms/Wrap/WaterDropWrap/styles.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
 
   width: ${props => props.width};
   height: ${props => props.height};
-  padding: 10px 50px;
+  padding: ${props => (props.padding ? props.padding : "10px 50px")};
   border-top-right-radius: ${props => props.radiusSize};
   border-bottom-left-radius: ${props => props.radiusSize};
   box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.16);
