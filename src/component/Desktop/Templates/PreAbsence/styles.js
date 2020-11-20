a17dd766120d2a7d110ce10dfa1189277abdbf88
CutyApple <dbtldhs0915@naver.com>
2020-11-20 17:01:59 +0900

    [FIX] <D/Templates/Attendance/styles.js>

--- src/component/Desktop/Templates/PreAbsence/styles.js
+++ src/component/Desktop/Templates/PreAbsence/styles.js
@@ -3,16 +3,22 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
 `;
 
 export const MainWrap = styled.div`
-  display: flex;
-  margin: 40px 10%;
+  display: grid;
+  gap: 0 40px;
+  grid-template-columns: 9fr 5fr;
+  box-sizing: border-box;
+
+  width: 100%;
+  height: 100%;
+  padding: 40px 10%;
 `;
 
 export const SubWrap = styled.div`
-  display: flex;
-  margin: 0 0 0 40px;
-  flex-direction: column;
-  justify-content: space-between;
+  display: grid;
+  gap: 40px 0;
+  grid-template-rows: 1fr 1fr;
 `;
