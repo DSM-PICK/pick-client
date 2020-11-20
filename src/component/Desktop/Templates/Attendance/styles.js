0a1ba6e092348871ee5f83dd7bf0d4102b93648c
CutyApple <dbtldhs0915@naver.com>
2020-11-20 17:01:56 +0900

    [FIX] <D/Templates/Attendance/styles.js>

--- src/component/Desktop/Templates/Attendance/styles.js
+++ src/component/Desktop/Templates/Attendance/styles.js
@@ -10,6 +10,6 @@ export const MainWrap = styled.div`
   display: grid;
   grid-gap: 0 120px;
   grid-template: repeat(4, 1fr) / 3fr 2fr;
-  height: calc(100% - 60px);
-  padding: 0 10%;
+  height: calc(100% - 40px);
+  padding: 40px 10%;
 `;
