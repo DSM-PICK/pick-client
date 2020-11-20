022914ef721bff81ff8d10e81b991baad35d6b89
CutyApple <dbtldhs0915@naver.com>
2020-11-20 22:39:29 +0900

    [REFACTOR] <LogoutModal.js> Move File's Dir

---
+++ src/component/Modal/LogoutModal/LogoutModal.js
@@ -0,0 +1,36 @@
+import React, { useCallback } from "react";
+import * as S from "../Modal/styles";
+import { useDispatch } from "react-redux";
+import Modal from "../Modal";
+import { dropModal } from "../../../module/action/modal_wrap";
+import { Logout } from "../../../lib/requestApi";
+
+const LogoutModal = () => {
+  const dispatch = useDispatch();
+  const ModalOff = useCallback(() => {
+    dispatch(dropModal());
+  }, [dispatch]);
+
+  const LogoutClick = () => {
+    ModalOff();
+    Logout();
+  };
+
+  const MODAL_CSS = {
+    width: "640px",
+    radius: "60px",
+    background: "#FFFFFF"
+  };
+
+  return (
+    <Modal css={MODAL_CSS}>
+      <S.ModalText>로그아웃하시겠습니까?</S.ModalText>
+      <S.ModalBtnWrap>
+        <S.ModalCancle onClick={ModalOff}>취소</S.ModalCancle>
+        <S.ModalOkay onClick={LogoutClick}>확인</S.ModalOkay>
+      </S.ModalBtnWrap>
+    </Modal>
+  );
+};
+
+export default LogoutModal;
