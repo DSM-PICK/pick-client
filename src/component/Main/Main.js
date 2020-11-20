543d4cd123b3516a721ad8b861cb8b215c84f809
CutyApple <dbtldhs0915@naver.com>
2020-11-20 22:24:10 +0900

    [FEAT] <Main.js> Apply ModalWrap Feat

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -10,10 +10,10 @@ import PreReport from "./PreReport/PreReports";
 import Notice from "./Notice/Notice";
 import { Logo } from "../../asset";
 import { MAIN_ANCHOR_ITEMS } from "../Attendance/Constant";
-import { modalOn, modalOff } from "../../module/action/admin_modal";
 import { getPreAbsenceListSaga } from "../../module/action/pre_absence";
 import { getMainTextRemainingDateSaga } from "../../module/action/main_text";
 import { checkPageWithLogin } from "../../lib/requestApi";
+import { showModal } from "../../module/action/modal_wrap/index";
 
 const Main = () => {
   useEffect(() => {
@@ -23,18 +23,13 @@ const Main = () => {
   const anchorItems = MAIN_ANCHOR_ITEMS;
   const TEACHER_NAME = `teacherName`;
   const dispatch = useDispatch();
-  const isOpen = useSelector(state => state.adminModal.modalOn);
   const remainingDate = useSelector(state => state.mainText.remainingDate);
 
   const teacherName = window.localStorage.getItem(TEACHER_NAME);
 
-  const modalOpen = useCallback(() => dispatch(modalOn()), [dispatch]);
-  const modalClose = useCallback(() => dispatch(modalOff()), [dispatch]);
-
-  const onModalClick = () => {
-    const modalDoing = isOpen ? modalClose : modalOpen;
-    modalDoing();
-  };
+  const onModalClick = useCallback(() => {
+    dispatch(showModal(LogoutModal));
+  }, [dispatch]);
 
   useEffect(() => {
     checkPageWithLogin();
@@ -75,7 +70,6 @@ const Main = () => {
           <S.MainBodyBoxText>동아리현황</S.MainBodyBoxText>
           <Notice />
         </S.MainBodyBox>
-        {isOpen && <LogoutModal onModalClick={onModalClick} />}
       </Body>
       <Footer />
     </S.Container>
