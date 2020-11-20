a86560007268baa97a77a6bf3796a14b845e3e69
CutyApple <dbtldhs0915@naver.com>
2020-11-20 22:40:06 +0900

    [FEAT] <Modal/styles.js> Apply Props Style

--- src/component/Modal/styles.js
+++ src/component/Modal/styles.js
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: ${props => (props.width ? props.width : "auto")};
+  border-radius: ${props => props.radius};
+  background: ${props => props.background};
+`;
