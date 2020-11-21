417f84554457041ba5425c174147d923885922ae
CutyApple <dbtldhs0915@naver.com>
2020-11-21 17:17:13 +0900

    [REFACTOR] <PreReportDate.js>

--- src/component/Main/PreReport/PreReportDate/PreReportDate.js
+++ src/component/Main/PreReport/PreReportDate/PreReportDate.js
@@ -1,23 +1,14 @@
 import React from "react";
 import * as S from "./styles";
-import PreReportCalander from "./PreReportCalendar/PreReportCalander";
-import PreReportModal from "./PreReportModal/PreReportModal";
 import { useSelector } from "react-redux";
 
 const PreReportDate = props => {
-  const { modal, height, calcDate, calcMonth, calcYear } = props;
-  const {
-    onOffModal,
-    onSelectDay,
-    onClassChange,
-    onPreReportClick,
-    onClickCalcMonth
-  } = props;
+  const { refInputArr } = props;
+  const { onClassChange, onPreReportClick } = props;
 
   const preAbsenceData = useSelector(state => state.preReport);
   const { preDate, nextDate } = preAbsenceData;
   const dateArr = [preDate, nextDate];
-  const refInputArr = [props.preClassInput, props.nextClassInput];
 
   return (
     <S.Container>
@@ -35,25 +26,13 @@ const PreReportDate = props => {
               placeholder="__"
               ref={refInputArr[index]}
               value={date.period}
-              onChange={e => onClassChange(!index, e)}
+              onChange={e => onClassChange(!index, dateArr[index], e)}
             />
             <S.ClassText>교시</S.ClassText>
             {!index && <S.Additional>~</S.Additional>}
           </S.ClassWrap>
         </S.Date>
       ))}
-      {modal && (
-        <PreReportCalander
-          height={height}
-          calcDate={calcDate}
-          calcMonth={calcMonth}
-          calcYear={calcYear}
-          onOffModal={onOffModal}
-          onSelectDay={onSelectDay}
-          onClickCalcMonth={onClickCalcMonth}
-        />
-      )}
-      {modal && <PreReportModal onOffModal={onOffModal} />}
     </S.Container>
   );
 };
