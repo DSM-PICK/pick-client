0bae7d50c4616dde7daec2522ac1bfc226498ae0
CutyApple <dbtldhs0915@naver.com>
2020-11-21 22:51:45 +0900

    [FEAT] <Footer/Constant.js>

--- src/component/Footer/Constant.js
+++ src/component/Footer/Constant.js
@@ -2,21 +2,25 @@ export const FooterNav = [
   {
     imgLink: "/src/asset/Footer/home.svg",
     imgOnLink: "/src/asset/Footer/homeOn.svg",
-    link: "/t/main"
+    link: "/t/main",
+    text: "홈"
   },
   {
     imgLink: "/src/asset/Footer/calender.svg",
     imgOnLink: "/src/asset/Footer/calenderOn.svg",
-    link: "/t/schedule"
+    link: "/t/schedule",
+    text: "일정"
   },
   {
     imgLink: "/src/asset/Footer/bugreport.svg",
     imgOnLink: "/src/asset/Footer/bugreportOn.svg",
-    link: "/bugreport"
+    link: "/t/bugreport",
+    text: "신고"
   },
   {
-    imgLink: "/src/asset/Footer/tonggei.svg",
-    imgOnLink: "/src/asset/Footer/tonggeiOn.svg",
-    link: "/tonggei"
+    imgLink: "/src/asset/Footer/admin.svg",
+    imgOnLink: "/src/asset/Footer/adminOn.svg",
+    link: "/t/admin",
+    text: "관리"
   }
 ];
