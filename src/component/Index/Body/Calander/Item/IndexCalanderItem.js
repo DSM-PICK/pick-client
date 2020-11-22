ab7f8c8cb88676a8a1c9ac1a119010aca8a2c1bd
YeongG <yeonggil223@gmail.com>
2020-11-23 02:00:35 +0900

    [MODIFY] QA Hot-fix4

--- src/component/Index/Body/Calander/Item/IndexCalanderItem.js
+++ src/component/Index/Body/Calander/Item/IndexCalanderItem.js
@@ -5,7 +5,7 @@ const IndexCalanderItem = ({ floor, teacher }) => {
   return (
     <S.Container>
       <div>{floor}층</div>
-      <div>{teacher}</div>
+      <div>{teacher || "없어요"}</div>
       <div>선생님</div>
     </S.Container>
   );
