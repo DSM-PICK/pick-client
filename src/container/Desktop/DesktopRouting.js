c7b5a994d135679cb01d97d6b7a0e8ec688f0dee
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:23:08 +0900

    [FIX] <D/DesktopRouting.js> Set  "/login" Routing

--- src/container/Desktop/DesktopRouting.js
+++ src/container/Desktop/DesktopRouting.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFoundContainer from "../NotFound/NotFoundContainer";
+import DesktopLoginContainer from "./DesktopLoginContainer";
 
 const DesktopRouting = () => {
   return (
     <Switch>
-      <Route path="/login" component={NotFoundContainer} />
+      <Route path="/login" component={DesktopLoginContainer} />
 
       <Route path="*" component={NotFoundContainer} />
     </Switch>
