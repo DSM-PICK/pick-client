72e17aa8c6615be49474535edcf407440b909d7f
CutyApple <dbtldhs0915@naver.com>
2020-11-26 23:35:43 +0900

    [FEAT] <D/Organisms/LoginWrap> Apply Logo Img

--- src/component/Desktop/Organisms/Account/LoginWrap/LoginWrap.js
+++ src/component/Desktop/Organisms/Account/LoginWrap/LoginWrap.js
@@ -2,13 +2,15 @@ import React from "react";
 import * as S from "./styles";
 import LoginForm from "../../../Molecules/Form/LoginForm/LoginForm";
 import Link from "../../../Atoms/Link/Link";
+import Img from "../../../Atoms/Img/Img";
+import { LogoPick } from "../../../../../asset";
 
 const LoginWrap = () => {
   return (
     <S.Container>
-      <div>123</div>
+      <Img img={LogoPick} style={S.ImgStyle} />
       <LoginForm />
-      <Link to={"/register"} text={"계정이 없으신가요?"} style={S.LinkStyle} />
+      <Link to={"/signup"} text={"계정이 없으신가요?"} style={S.LinkStyle} />
     </S.Container>
   );
 };
--- src/component/Desktop/Organisms/Account/LoginWrap/styles.js
+++ src/component/Desktop/Organisms/Account/LoginWrap/styles.js
@@ -4,12 +4,21 @@ import { UpOn } from "../../../CSS/BoxShadow";
 export const Container = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   flex-direction: column;
   width: 630px;
   height: 655px;
+  padding: 80px 88px 30px 88px;
   border-radius: 32px;
-  box-shadow: ${props => UpOn};
+  box-sizing: border-box;
+  box-shadow: ${UpOn};
+  background: #efefef;
 `;
+
+export const ImgStyle = {
+  width: "246px",
+  height: "54px"
+};
 
 export const LinkStyle = {
   color: "#3FB3CC",
