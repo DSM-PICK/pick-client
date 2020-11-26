fd5aec514fb57bf632cb389d8f194f9a070852d6
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:24:12 +0900

    [FEAT] <D/Organisms/LoginWrap> Create LoginWrap Organism Files

---
+++ src/component/Desktop/Organisms/Login/Login/LoginWrap.js
@@ -0,0 +1,9 @@
+import React from "react";
+import * as S from "./styles";
+import LoginForm from "../../../Molecules/Form/LoginForm/LoginForm";
+
+const LoginWrap = () => {
+  return <LoginForm />;
+};
+
+export default LoginWrap;
