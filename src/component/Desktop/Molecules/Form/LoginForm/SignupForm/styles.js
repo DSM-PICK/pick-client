2e6b14fac06197d13ccba1f1ad16171e15b2110b
CutyApple <dbtldhs0915@naver.com>
2020-11-26 23:34:32 +0900

    [FEAT] <D/Molecules/SignupForm> Create SignupForm Molecules Files

---
+++ src/component/Desktop/Molecules/Form/LoginForm/SignupForm/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.form`
+  display: grid;
+  gap: 32px;
+  grid-template: 66px 66px 66px 1fr 80px / 1fr;
+  width: 100%;
+  height: 442px;
+`;
