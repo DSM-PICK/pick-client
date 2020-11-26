befd9e78363ae1836f7b9bbbe04aa4e3ce8b7e04
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:22:38 +0900

    [FEAT] <D/DesktopLoginContainer.js> Create Login Container File

---
+++ src/container/Desktop/DesktopLoginContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "../../component/Desktop/Templates/Login/Login";
+
+const DesktopLoginContainer = () => {
+  return <Login></Login>;
+};
+
+export default DesktopLoginContainer;
