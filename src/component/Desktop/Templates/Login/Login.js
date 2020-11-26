1fec943ae2742ce6c3daf937ef5800f2293222ad
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:23:43 +0900

    [FEAT] <D/Templates/Login> Create Login Template Files

---
+++ src/component/Desktop/Templates/Login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginWrap from "../../Organisms/Login/Login/LoginWrap";
+import * as S from "./styles";
+
+const Login = () => {
+  return (
+    <S.Container>
+      <LoginWrap />
+    </S.Container>
+  );
+};
+
+export default Login;
