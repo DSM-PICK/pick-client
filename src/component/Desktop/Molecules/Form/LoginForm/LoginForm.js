00969bb703a7f239c1060ce18b3eba2657f2e5cf
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:24:38 +0900

    [FEAT] <D/Molecules/LoginForm> Create LoginForm Molecule Files

---
+++ src/component/Desktop/Molecules/Form/LoginForm/LoginForm.js
@@ -0,0 +1,75 @@
+import React, { useCallback, useState } from "react";
+import * as S from "./styles";
+import Input from "../../../Atoms/Input/Input";
+
+const LoginForm = () => {
+  const [loginInfo, setLoginInfo] = useState({
+    id: "",
+    password: ""
+  });
+
+  const onChangeLoginInfo = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setLoginInfo(prev => ({
+        ...prev,
+        [name]: value
+      }));
+    },
+    [loginInfo]
+  );
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+
+      console.log(loginInfo.id, loginInfo.password);
+    },
+    [loginInfo]
+  );
+
+  const InputStyle = {
+    padding: "20px 40px",
+    color: "#555555",
+    fontSize: "18px",
+    fontWeight: "500",
+    borderRadius: "32px",
+    background: "#efefef",
+    boxShadow: "DownOn"
+  };
+
+  const SubmitStyle = {
+    padding: "20px 40px",
+    color: "#333333",
+    fontSize: "20px",
+    fontWeight: "bold",
+    borderRadius: "32px",
+    background: "linear-gradient(135deg, #e3e3e3, #fbfbfb)",
+    boxShadow: "UpOn"
+  };
+
+  return (
+    <S.Container onSubmit={onSubmit}>
+      <Input
+        name={"id"}
+        type={"text"}
+        style={InputStyle}
+        inputValue={loginInfo.id}
+        placeholder={"아이디를 입력하세요"}
+        onChange={onChangeLoginInfo}
+      />
+      <Input
+        name={"password"}
+        type={"password"}
+        style={InputStyle}
+        inputValue={loginInfo.password}
+        placeholder={"비밀번호를 입력하세요"}
+        onChange={onChangeLoginInfo}
+      />
+      <div />
+      <Input type={"submit"} style={SubmitStyle} value={"로그인"} />
+    </S.Container>
+  );
+};
+
+export default LoginForm;
---
+++ src/component/Desktop/Molecules/Form/LoginForm/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.form`
+  display: grid;
+  gap: 32px;
+  grid-template: 66px 66px 1fr 80px / 1fr;
+  height: 360px;
+`;
