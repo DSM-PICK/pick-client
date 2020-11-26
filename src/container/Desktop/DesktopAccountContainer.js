8168526ee694309f5909b3b28f3eeb7b09c05da3
CutyApple <dbtldhs0915@naver.com>
2020-11-26 21:59:23 +0900

    [REFACTOR] <D/DesktopAccountContainer.js> Change File Name

---
+++ src/container/Desktop/DesktopAccountContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Account from "../../component/Desktop/Templates/Login/Account";
+
+const DesktopAccountContainer = () => {
+  return <Account />;
+};
+
+export default DesktopAccountContainer;
--- src/container/Desktop/DesktopLoginContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "../../component/Desktop/Templates/Login/Login";
-
-const DesktopLoginContainer = () => {
-  return <Login></Login>;
-};
-
-export default DesktopLoginContainer;
