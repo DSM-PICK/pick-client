6a1bf4b70c0334850d04e32e63b23cf85e6120cb
CutyApple <dbtldhs0915@naver.com>
2020-12-04 08:52:29 +0900

    [FIX] <D/Molecules/SignupWrap> Modify Style

--- src/component/Desktop/Organisms/Account/SignupWrap/SignupWrap.js
+++ src/component/Desktop/Organisms/Account/SignupWrap/SignupWrap.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
 import Link from "../../../Atoms/Link/Link";
 import Img from "../../../Atoms/Img/Img";
 import SignupForm from "../../../Molecules/Form/LoginForm/SignupForm/SignupForm";
 import { LogoPick } from "../../../../../asset";
+import UserCheckForm from "../../../Molecules/Form/LoginForm/UserCheckForm/UserCheckForm";
 
 const SignupWrap = () => {
+  const [isAccounted, setIsAccounted] = useState(true);
+
   return (
     <S.Container>
       <Img img={LogoPick} style={S.ImgStyle} />
-      <SignupForm />
-      <Link to={"/login"} text={"이미 계정이 있습니다"} style={S.LinkStyle} />
+      {isAccounted ? (
+        <>
+          <UserCheckForm
+            isAccounted={isAccounted}
+            setIsAccounted={setIsAccounted}
+          />
+          <Link
+            to={"/login"}
+            text={"이미 계정이 있습니다"}
+            style={S.LinkStyle}
+          />
+        </>
+      ) : (
+        <>
+          <SignupForm />
+          <S.BackButton
+            onClick={() => setIsAccounted(isAccounted => !isAccounted)}
+            {...S.LinkStyle}
+          >
+            이전으로 돌아가기
+          </S.BackButton>
+        </>
+      )}
     </S.Container>
   );
 };
--- src/component/Desktop/Organisms/Account/SignupWrap/styles.js
+++ src/component/Desktop/Organisms/Account/SignupWrap/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { UpOn } from "../../../CSS/BoxShadow";
+import { UpOnShadow } from "../../../CSS/BoxShadow";
 
 export const Container = styled.div`
   display: flex;
@@ -7,12 +7,26 @@ export const Container = styled.div`
   justify-content: space-between;
   flex-direction: column;
   width: 630px;
-  height: 763px;
+  height: 655px;
   padding: 80px 88px 30px 88px;
   border-radius: 32px;
   box-sizing: border-box;
-  box-shadow: ${UpOn};
+  box-shadow: ${UpOnShadow};
   background: #efefef;
+`;
+
+export const BackButton = styled.button`
+  height: ${props => props.height};
+  border: none;
+  color: ${props => props.color};
+  font-size: ${props => props.fontSize};
+  font-weight: ${props => props.fontWeight};
+  line-height: 0;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const ImgStyle = {
@@ -21,6 +35,7 @@ export const ImgStyle = {
 };
 
 export const LinkStyle = {
+  height: "16px",
   color: "#3FB3CC",
   fontSize: "16px",
   fontWeight: "500"
