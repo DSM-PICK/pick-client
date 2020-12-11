3e04e9612e3faa67f483ebe4f3936341c701a659
CutyApple <dbtldhs0915@naver.com>
2020-12-12 01:14:32 +0900

    [FEAT] <D/Organisms/LoginWrap.js>

--- src/component/Desktop/Organisms/Account/LoginWrap/LoginWrap.js
+++ src/component/Desktop/Organisms/Account/LoginWrap/LoginWrap.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./styles";
 import LoginForm from "../../../Molecules/Form/LoginForm/LoginForm";
 import CenterLink from "../../../Atoms/Link/CenterLink/CenterLink";
 import Img from "../../../Atoms/Img/Img";
 import { LogoPick } from "../../../../../asset";
+import { useDispatch } from "react-redux";
+import { setLoginError } from "../../../../../module/action/account";
 
 const LoginWrap = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setLoginError(""));
+  }, []);
+
   return (
     <S.Container>
       <S.ImgWrap>
