71a7072f5cf4e90d478e54cdc2ade48f339da811
CutyApple <dbtldhs0915@naver.com>
2020-12-12 01:14:49 +0900

    [FEAT] <D/Organisms/SignupWrap.js>

--- src/component/Desktop/Organisms/Account/SignupWrap/SignupWrap.js
+++ src/component/Desktop/Organisms/Account/SignupWrap/SignupWrap.js
@@ -1,25 +1,60 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect } from "react";
 import * as S from "./styles";
 import CenterLink from "../../../Atoms/Link/CenterLink/CenterLink";
 import Img from "../../../Atoms/Img/Img";
 import SignupForm from "../../../Molecules/Form/LoginForm/SignupForm/SignupForm";
 import { LogoPick } from "../../../../../asset";
 import UserCheckForm from "../../../Molecules/Form/LoginForm/UserCheckForm/UserCheckForm";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setAuthenticateError,
+  setIsAccounted,
+  setSignUpError
+} from "../../../../../module/action/account";
 
 const SignupWrap = () => {
-  const [isAccounted, setIsAccounted] = useState(true);
+  const isAccounted = useSelector(state => state.account.isAccounted);
+
+  const dispatch = useDispatch();
+
+  const setAccountedOpposite = useCallback(
+    accountedState => {
+      dispatch(setIsAccounted(!accountedState));
+    },
+    [dispatch]
+  );
+  const setAuthenticateFormError = useCallback(
+    error => {
+      dispatch(setAuthenticateError(error));
+    },
+    [dispatch]
+  );
+  const setSignupFormError = useCallback(
+    error => {
+      dispatch(setSignUpError(error));
+    },
+    [dispatch]
+  );
+
+  const changeAccountedState = () => {
+    setAccountedOpposite(isAccounted);
+    setSignupFormError("");
+    setAuthenticateFormError("");
+  };
+
+  useEffect(() => {
+    setSignupFormError("");
+    setAuthenticateFormError("");
+  }, [dispatch]);
 
   return (
     <S.Container>
       <S.ImgWrap>
         <Img img={LogoPick} style={S.ImgStyle} />
       </S.ImgWrap>
-      {isAccounted ? (
+      {!isAccounted ? (
         <>
-          <UserCheckForm
-            isAccounted={isAccounted}
-            setIsAccounted={setIsAccounted}
-          />
+          <UserCheckForm />
           <CenterLink
             to={"/login"}
             text={"이미 계정이 있습니다"}
@@ -29,10 +64,7 @@ const SignupWrap = () => {
       ) : (
         <>
           <SignupForm />
-          <S.BackButton
-            onClick={() => setIsAccounted(isAccounted => !isAccounted)}
-            {...S.LinkStyle}
-          >
+          <S.BackButton onClick={changeAccountedState} {...S.LinkStyle}>
             이전으로 돌아가기
           </S.BackButton>
         </>
