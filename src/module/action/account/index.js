a299eb1ba0751b91c441f2160547ad4dece46040
CutyApple <dbtldhs0915@naver.com>
2020-12-12 01:06:41 +0900

    [FEAT] <action/account/index.js> Add setPwChangeError Feature

--- src/module/action/account/index.js
+++ src/module/action/account/index.js
@@ -3,6 +3,7 @@ export const SET_IS_ACCOUNTED = "account/SET_IS_ACCOUNTED";
 
 export const SET_LOGIN_ERROR = "account/SET_LOGIN_ERROR";
 export const SET_SIGN_UP_ERROR = "account/SET_SIGN_UP_ERROR";
+export const SET_PW_CHANGE_ERROR = "account/SET_PW_CHANGE_ERROR";
 export const SET_AUTHENTICATE_ERROR = "account/SET_AUTHENTICATE_ERROR";
 
 //
@@ -26,6 +27,10 @@ export const setLoginError = payload => ({
 });
 export const setSignUpError = payload => ({
   type: SET_SIGN_UP_ERROR,
+  payload
+});
+export const setPwChangeError = payload => ({
+  type: SET_PW_CHANGE_ERROR,
   payload
 });
 export const setAuthenticateError = payload => ({
