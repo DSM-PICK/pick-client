78e0073b4a036f16e0ed70bf57056c33026eedf2
CutyApple <dbtldhs0915@naver.com>
2020-12-12 01:11:58 +0900

    [FEAT] <D/Organisms/PwChangeWrap>

---
+++ src/component/Desktop/Organisms/Account/PwChangeWrap/PwChangeWrap.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import * as S from "./styles";
+import CenterLink from "../../../Atoms/Link/CenterLink/CenterLink";
+import Img from "../../../Atoms/Img/Img";
+import { LogoPick } from "../../../../../asset";
+import { useDispatch } from "react-redux";
+import { setLoginError } from "../../../../../module/action/account";
+import PwChangeForm from "../../../Molecules/Form/LoginForm/PwChangeForm/PwChangeForm";
+
+const PwChangeWrap = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setLoginError(""));
+  }, []);
+
+  return (
+    <S.Container>
+      <S.ImgWrap>
+        <Img img={LogoPick} style={S.ImgStyle} />
+      </S.ImgWrap>
+      <PwChangeForm />
+      <CenterLink to={"/change"} text={"돌아가기"} style={S.LinkStyle} />
+    </S.Container>
+  );
+};
+
+export default PwChangeWrap;
