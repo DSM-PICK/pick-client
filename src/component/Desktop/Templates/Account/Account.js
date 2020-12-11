665f24e3a6dbc79aacf69a08bf759c860abb3949
CutyApple <dbtldhs0915@naver.com>
2020-12-12 01:14:58 +0900

    [FEAT] <D/Templates/Acconut.js>

--- src/component/Desktop/Templates/Account/Account.js
+++ src/component/Desktop/Templates/Account/Account.js
@@ -3,6 +3,7 @@ import * as S from "./styles";
 import LoginWrap from "../../Organisms/Account/LoginWrap/LoginWrap";
 import SignupWrap from "../../Organisms/Account/SignupWrap/SignupWrap";
 import { Route, Switch } from "react-router-dom";
+import PwChangeWrap from "../../Organisms/Account/PwChangeWrap/PwChangeWrap";
 
 const Account = () => {
   return (
@@ -10,6 +11,7 @@ const Account = () => {
       <Switch>
         <Route exact path={"/login"} component={LoginWrap} />
         <Route exact path={"/signup"} component={SignupWrap} />
+        <Route exact path={"/change"} component={PwChangeWrap} />
       </Switch>
     </S.Container>
   );
