2defc5119dec4d4e24f7f73a0cfec22312fe3b78
CutyApple <dbtldhs0915@naver.com>
2020-12-15 15:46:38 +0900

    [FEAT] <Stats/Stats> Create File

---
+++ src/component/Stats/Stats.js
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "./styles";
+import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
+
+const Stats = () => {
+  return (
+    <S.Container>
+      <Header />
+      {/* <StatsBody /> */}
+      <Footer />
+    </S.Container>
+  );
+};
+
+export default Stats;
