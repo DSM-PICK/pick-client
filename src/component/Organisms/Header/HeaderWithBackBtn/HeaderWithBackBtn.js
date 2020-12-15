2df9f165ce241c2bd29913a4fce8458afb65a405
CutyApple <dbtldhs0915@naver.com>
2020-12-15 19:49:43 +0900

    [FEAT] <Organsims/HeaderWithBackBtn.js>

--- src/component/Organisms/Header/HeaderWithBackBtn/HeaderWithBackBtn.js
+++ src/component/Organisms/Header/HeaderWithBackBtn/HeaderWithBackBtn.js
@@ -1,8 +1,18 @@
 import React from "react";
 import * as S from "./styles";
+import HeaderTitle from "../../../Atoms/HeaderItem/HeaderTitle/HeaderTitle";
+import HeaderBackBtn from "../../../Atoms/HeaderItem/HeaderBackBtn/HeaderBackBtn";
+import Header from "../Header";
 
-const HeaderWithBackBtn = () => {
-  return <S.Container></S.Container>;
+const HeaderWithBackBtn = props => {
+  const { link, title } = props;
+
+  return (
+    <Header>
+      <HeaderBackBtn link={link} />
+      <HeaderTitle title={title} />
+    </Header>
+  );
 };
 
 export default HeaderWithBackBtn;
