9224dd4c90666a9d89e03a250570b8abbe705189
CutyApple <dbtldhs0915@naver.com>
2020-12-15 22:23:52 +0900

    [FEAT] <Organisms/StaticData.js> Add Static Data

---
+++ src/component/Organisms/Body/StatsBody/StaticData.js
@@ -0,0 +1,14 @@
+export const StatsBodyStaticData = [
+  {
+    link: "",
+    text: "일정 통계"
+  },
+  {
+    link: "",
+    text: "반별 통계(for 담임선생님)"
+  },
+  {
+    link: "",
+    text: "통계 그래프"
+  }
+];
