2d06d4a17f36cf7a5d45e087044a105bc341dd45
CutyApple <dbtldhs0915@naver.com>
2020-12-15 18:25:02 +0900

    [FEAT] <Organisms/Footer.js> Apply FooterItem Feature

--- src/component/Organsisms/Footer/Footer.js
+++ src/component/Organsisms/Footer/Footer.js
@@ -1,8 +1,18 @@
 import React from "react";
+import FooterItem from "../../Molecules/FooterItem/FooterItem";
+import { FooterStaticData } from "./StaticData";
 import * as S from "./styles";
 
-const Footer = props => {
-  return <S.Container></S.Container>;
+const Footer = () => {
+  const StaticDataArr = FooterStaticData;
+
+  return (
+    <S.Container>
+      {StaticDataArr.map(staticData => (
+        <FooterItem key={staticData.text} {...staticData} />
+      ))}
+    </S.Container>
+  );
 };
 
 export default Footer;
