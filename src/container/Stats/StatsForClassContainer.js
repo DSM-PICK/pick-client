f02e9268960d2552a9b44d4daa795ad2cccacde3
CutyApple <dbtldhs0915@naver.com>
2020-12-15 23:41:41 +0900

    [FEAT] <StatsForClassContainer.js>

---
+++ src/container/Stats/StatsForClassContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StatsForClass from "../../component/Stats/StatsForClass/StatsForClass";
+
+const StatsForClassContainer = () => {
+  return <StatsForClass />;
+};
+
+export default StatsForClassContainer;
