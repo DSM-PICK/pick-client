083ea478036ce836734b42b964df6fca4a3d0d4f
CutyApple <dbtldhs0915@naver.com>
2020-12-15 17:05:35 +0900

    [FEAT] <Molecules/FooterItem> Create File

---
+++ src/component/Molecules/FooterItem/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  width: 25%;
+  height: 100%;
+  padding: 5px 0;
+  box-sizing: border-box;
+  border: none;
+  text-decoration: none;
+`;
