98c8b16cb708958a0249dd56854c984295a9ce3d
CutyApple <dbtldhs0915@naver.com>
2020-12-15 17:02:16 +0900

    [FEAT] <Organisms/Footer/StaticData.js> Create File

---
+++ src/component/Organsisms/Footer/StaticData.js
@@ -0,0 +1,37 @@
+import {
+  AdminIcon,
+  AdminOnIcon,
+  CalendarIcon,
+  CalendarOnIcon,
+  HomeIcon,
+  HomeOnIcon,
+  StatsIcon,
+  StatsOnIcon
+} from "../../../asset";
+
+export const FooterStaticData = [
+  {
+    imgLink: HomeIcon,
+    imgOnLink: HomeOnIcon,
+    link: "/t/main",
+    text: "홈"
+  },
+  {
+    imgLink: CalendarIcon,
+    imgOnLink: CalendarOnIcon,
+    link: "/t/schedule",
+    text: "일정"
+  },
+  {
+    imgLink: StatsIcon,
+    imgOnLink: StatsOnIcon,
+    link: "/t/stats",
+    text: "통계"
+  },
+  {
+    imgLink: AdminIcon,
+    imgOnLink: AdminOnIcon,
+    link: "/t/admin",
+    text: "관리"
+  }
+];
