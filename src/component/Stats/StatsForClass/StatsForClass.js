e6eb0deb8c4fc218e78b3f0b6b0bd1082659a585
CutyApple <dbtldhs0915@naver.com>
2020-12-15 23:44:14 +0900

    [FEAT] <StatsForClass.js> Apply HeaderWithBackBtn Feature

--- src/component/Stats/StatsForClass/StatsForClass.js
+++ src/component/Stats/StatsForClass/StatsForClass.js
@@ -1,8 +1,13 @@
 import React from "react";
+import HeaderWithBackBtn from "../../Organisms/Header/HeaderWithBackBtn/HeaderWithBackBtn";
 import * as S from "./styles";
 
 const StatsForClass = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <HeaderWithBackBtn link={"/t/stats"} title={"반별 통계"} />
+    </S.Container>
+  );
 };
 
 export default StatsForClass;
