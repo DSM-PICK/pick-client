7ed5383d7518c8a861dcb019c340afac79412039
CutyApple <dbtldhs0915@naver.com>
2020-12-15 19:02:51 +0900

    [REFACTOR] Change File's Directory

--- src/component/Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle.js
+++ src/component/Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as S from "./styles";
 
 const HeaderOnlyTitle = () => {
-  return <S.Container></S.Container>;
+  return <S.Container>asdf</S.Container>;
 };
 
 export default HeaderOnlyTitle;
