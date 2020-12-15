96e538b7aed7eac117efb6e2ad8443af11ede4ca
CutyApple <dbtldhs0915@naver.com>
2020-12-15 22:27:21 +0900

    [FEAT] <Stats/Stats.js> Apply StatsBody Feature

--- src/component/Stats/Stats.js
+++ src/component/Stats/Stats.js
@@ -2,11 +2,13 @@ import React from "react";
 import * as S from "./styles";
 import HeaderOnlyTitle from "../Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle";
 import Footer from "../Organisms/Footer/Footer";
+import StatsBody from "../Organisms/Body/StatsBody/StatsBody";
 
 const Stats = () => {
   return (
     <S.Container>
       <HeaderOnlyTitle title={"통계"} />
+      <StatsBody />
       <Footer />
     </S.Container>
   );
