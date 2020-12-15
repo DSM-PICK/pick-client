82b587945f433c372530d98974d0f20587a6bb86
CutyApple <dbtldhs0915@naver.com>
2020-12-15 19:49:34 +0900

    [FEAT] <Organsims/HeaderOnlyTitle.js>

--- src/component/Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle.js
+++ src/component/Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle.js
@@ -1,8 +1,16 @@
 import React from "react";
 import * as S from "./styles";
+import HeaderTitle from "../../../Atoms/HeaderItem/HeaderTitle/HeaderTitle";
+import Header from "../Header";
 
-const HeaderOnlyTitle = () => {
-  return <S.Container>asdf</S.Container>;
+const HeaderOnlyTitle = props => {
+  const { title } = props;
+
+  return (
+    <Header>
+      <HeaderTitle title={title} />
+    </Header>
+  );
 };
 
 export default HeaderOnlyTitle;
