ed0d46ce11e38f9c729c15a6912464136095c44a
CutyApple <dbtldhs0915@naver.com>
2020-12-15 19:49:26 +0900

    [FEAT] <Organsims/HeaderFullOptions.js>

--- src/component/Organisms/Header/HeaderFullOptions/HeaderFullOptions.js
+++ src/component/Organisms/Header/HeaderFullOptions/HeaderFullOptions.js
@@ -1,8 +1,22 @@
 import React from "react";
 import * as S from "./styles";
+import HeaderDate from "../../../Atoms/HeaderItem/HeaderDate/HeaderDate";
+import HeaderTitle from "../../../Atoms/HeaderItem/HeaderTitle/HeaderTitle";
+import HeaderBackBtn from "../../../Atoms/HeaderItem/HeaderBackBtn/HeaderBackBtn";
+import HeaderTeacher from "../../../Atoms/HeaderItem/HeaderTeacher/HeaderTeacher";
+import Header from "../Header";
 
-const HeaderFullOptions = () => {
-  return <S.Container></S.Container>;
+const HeaderFullOptions = props => {
+  const { link, title, month, day, dayOfWeek, name } = props;
+
+  return (
+    <Header>
+      <HeaderBackBtn link={link} />
+      <HeaderTitle title={title} />
+      <HeaderDate month={month} day={day} dayOfWeek={dayOfWeek} />
+      <HeaderTeacher name={name} />
+    </Header>
+  );
 };
 
 export default HeaderFullOptions;
