a74eab8b4510768fa6a9ae163ceff3a03c38509c
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:20:55 +0900

    [FEAT] <Stats/StatsStaticData.js> Add STATS_PAGE_TITLE

--- src/component/Stats/StatsStaticData.js
+++ src/component/Stats/StatsStaticData.js
@@ -3,3 +3,9 @@ export const STATS_URL = {
   statsSchedule: "/t/stats/schedule",
   statsForClass: "/t/stats/class"
 };
+
+export const STATS_PAGE_TITLE = {
+  statsMain: "통계",
+  statsSchedule: "일정 통계",
+  statsForClass: "반별 통계"
+};
