4549af27f86a019fd119a155aa30f6a8c156a888
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:22:08 +0900

    [FEAT] <Stats/StatsForClassContainer.js> Apply Container Architecture

--- src/container/Stats/StatsForClassContainer.js
+++ src/container/Stats/StatsForClassContainer.js
@@ -1,8 +1,15 @@
 import React from "react";
 import StatsForClass from "../../component/Stats/StatsForClass/StatsForClass";
+import {
+  STATS_PAGE_TITLE,
+  STATS_URL
+} from "../../component/Stats/StatsStaticData";
 
 const StatsForClassContainer = () => {
-  return <StatsForClass />;
+  const statsMainUrl = STATS_URL.statsMain;
+  const statsForClassTitle = STATS_PAGE_TITLE.statsForClass;
+
+  return <StatsForClass link={statsMainUrl} title={statsForClassTitle} />;
 };
 
 export default StatsForClassContainer;
