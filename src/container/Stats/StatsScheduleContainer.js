0478ce5c88be58d6cca6ca4e52ada7ca0cfe1ad9
CutyApple <dbtldhs0915@naver.com>
2020-12-16 22:54:45 +0900

    [FEAT] <StatsScheduleContainer.js> date

--- src/container/Stats/StatsScheduleContainer.js
+++ src/container/Stats/StatsScheduleContainer.js
@@ -9,7 +9,22 @@ const StatsScheduleContainer = () => {
   const statsMainUrl = STATS_URL.statsMain;
   const statsScheduleTitle = STATS_PAGE_TITLE.statsSchedule;
 
-  return <StatsSchedule link={statsMainUrl} title={statsScheduleTitle} />;
+  const date = new Date();
+  const [month, day, dayOfWeek] = [
+    date.getMonth() + 1,
+    date.getDate(),
+    date.getDay()
+  ];
+
+  return (
+    <StatsSchedule
+      link={statsMainUrl}
+      title={statsScheduleTitle}
+      month={month}
+      day={day}
+      dayOfWeek={dayOfWeek}
+    />
+  );
 };
 
 export default StatsScheduleContainer;
