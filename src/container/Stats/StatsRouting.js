bb8e1487e33c61df34acd111f2b96f69dabdf2ad
CutyApple <dbtldhs0915@naver.com>
2020-12-16 22:21:13 +0900

    [REFACTOR] <StatsRouting.js> change import file's name

--- src/container/Stats/StatsRouting.js
+++ src/container/Stats/StatsRouting.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import StatsForClassContainer from "./StatsForClassFloorContainer";
+import StatsForClassContainer from "./StatsForClassContainer";
 import StatsForClassFloorContainer from "./StatsForClassFloorContainer";
 import StatsMainContainer from "./StatsMainContainer";
 import StatsScheduleFloorContainer from "./StatsScheduleFloorContainer";
