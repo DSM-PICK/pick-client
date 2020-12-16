1f9a44156487461febec21bc675c5b2cf143a021
CutyApple <dbtldhs0915@naver.com>
2020-12-16 22:22:16 +0900

    [FEAT] <StatsForClass.js> Apply HeaderFullOptions' props

--- src/component/Stats/StatsForClass/StatsForClass.js
+++ src/component/Stats/StatsForClass/StatsForClass.js
@@ -3,11 +3,17 @@ import * as S from "./styles";
 import HeaderFullOptions from "../../Organisms/Header/HeaderFullOptions/HeaderFullOptions";
 
 const StatsForClass = props => {
-  const { link, title } = props;
+  const { link, title, month, day, dayOfWeek } = props;
 
   return (
     <S.Container>
-      <HeaderFullOptions link={link} title={title} />
+      <HeaderFullOptions
+        link={link}
+        title={title}
+        month={month}
+        day={day}
+        dayOfWeek={dayOfWeek}
+      />
     </S.Container>
   );
 };
