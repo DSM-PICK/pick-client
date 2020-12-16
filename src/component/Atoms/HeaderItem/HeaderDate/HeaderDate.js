34d7110a7954c18eff1425b190f6609300285453
CutyApple <dbtldhs0915@naver.com>
2020-12-16 22:22:42 +0900

    [FEAT] <Atoms/HeaderDate.js> date

--- src/component/Atoms/HeaderItem/HeaderDate/HeaderDate.js
+++ src/component/Atoms/HeaderItem/HeaderDate/HeaderDate.js
@@ -4,12 +4,39 @@ import * as S from "./styles";
 const HeaderDate = props => {
   const { month, day, dayOfWeek } = props;
 
+  const dayOfWeek2DateText = dayOfWeek => {
+    let dateText = "";
+    switch (dayOfWeek) {
+      case 0:
+        dateText += "일";
+        break;
+      case 1:
+        dateText += "월";
+        break;
+      case 2:
+        dateText += "화";
+        break;
+      case 3:
+        dateText += "수";
+        break;
+      case 4:
+        dateText += "목";
+        break;
+      case 5:
+        dateText += "금";
+        break;
+      case 6:
+        dateText += "토";
+        break;
+    }
+
+    return dateText + "요일";
+  };
+
   return (
     <S.Container>
-      <S.Date>
-        `${month}월 ${day}일`
-      </S.Date>
-      <S.DayOfWeek>{dayOfWeek}</S.DayOfWeek>
+      <S.Date>{`${month}월 ${day}일`}</S.Date>
+      <S.DayOfWeek>{dayOfWeek2DateText(dayOfWeek)}</S.DayOfWeek>
     </S.Container>
   );
 };
