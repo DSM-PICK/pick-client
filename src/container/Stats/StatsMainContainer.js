091e8502c6de185d7d4588c41f8a205acda86998
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:23:51 +0900

    [FEAT] <StatsMainContainer.js> Apply Container Architecture

--- src/container/Stats/StatsMainContainer.js
+++ src/container/Stats/StatsMainContainer.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Stats from "../../component/Stats/StatsMain/Stats";
+import { STATS_PAGE_TITLE } from "../../component/Stats/StatsStaticData";
 
 const StatsMainContainer = () => {
-  return <Stats />;
+  const statsMainTitle = STATS_PAGE_TITLE.statsMain;
+
+  return <Stats title={statsMainTitle} />;
 };
 
 export default StatsMainContainer;
