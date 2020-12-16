42ba8f57a2aa0ad2787b0133ff523377a65ec676
CutyApple <dbtldhs0915@naver.com>
2020-12-16 21:56:16 +0900

    [FEAT] <StatsForClassContainer.js> Create File

---
+++ src/container/Stats/StatsForClassContainer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import StatsForClass from "../../component/Stats/StatsForClass/StatsForClass";
+
+import {
+  STATS_PAGE_TITLE,
+  STATS_URL
+} from "../../component/Stats/StatsStaticData";
+
+const StatsForClassContainer = () => {
+  const statsMainUrl = STATS_URL.statsMain;
+  const statsForClassTitle = STATS_PAGE_TITLE.statsForClass;
+
+  return <StatsForClass link={statsMainUrl} title={statsForClassTitle} />;
+};
+
+export default StatsForClassContainer;
