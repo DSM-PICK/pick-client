0e6ce594b2ecb98c3f7d6727b91d4fc400b78e94
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:50:18 +0900

    [FEAT] <Organisms/StatsBody/StaticData.js> StatsForClassBodyStaticData

--- src/component/Organisms/Body/StatsBody/StaticData.js
+++ src/component/Organisms/Body/StatsBody/StaticData.js
@@ -1,6 +1,6 @@
 import { STATS_PAGE_TITLE, STATS_URL } from "../../../Stats/StatsStaticData";
 
-export const StatsBodyStaticData = [
+export const StatsMainBodyStaticData = [
   {
     link: STATS_URL.statsSchedule,
     text: STATS_PAGE_TITLE.statsSchedule
@@ -12,5 +12,20 @@ export const StatsBodyStaticData = [
   {
     link: STATS_URL.statsGraph,
     text: STATS_PAGE_TITLE.statsGraph
+  }
+];
+
+export const StatsForClassBodyStaticData = [
+  {
+    link: STATS_URL.statsForClass + "/1",
+    text: "1학년"
+  },
+  {
+    link: STATS_URL.statsForClass + "/1",
+    text: "2학년"
+  },
+  {
+    link: STATS_URL.statsForClass + "/1",
+    text: "3학년"
   }
 ];
