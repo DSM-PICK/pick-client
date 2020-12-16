87ca5a8dd888ddd6c71e4269791ae8fee52e2ace
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:17:22 +0900

    [FEAT] <Stats/StatsRouting.js> Apply StatsForClassContainer

--- src/container/Stats/StatsRouting.js
+++ src/container/Stats/StatsRouting.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import StatsForClassContainer from "./StatsForClassContainer";
 import StatsMainContainer from "./StatsMainContainer";
 
 const StatsRouting = () => {
@@ -7,6 +8,12 @@ const StatsRouting = () => {
     <>
       <Switch>
         <Route exact path={"/t/stats"} component={StatsMainContainer} />
+        {/* <Route exact path={"/t/stats/schedule"} component={} /> */}
+        <Route
+          exact
+          path={"/t/stats/class"}
+          component={StatsForClassContainer}
+        />
       </Switch>
     </>
   );
