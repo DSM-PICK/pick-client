b05810aef48b28a33bcf8b246e6564b8410ce240
CutyApple <dbtldhs0915@naver.com>
2020-12-16 11:44:31 +0900

    [REFACTOR] <StatsMain/Stats.js> imported file's dir moving

--- src/component/Stats/StatsMain/Stats.js
+++ src/component/Stats/StatsMain/Stats.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as S from "./styles";
 import HeaderOnlyTitle from "../../Organisms/Header/HeaderOnlyTitle/HeaderOnlyTitle";
 import Footer from "../../Organisms/Footer/Footer";
-import StatsBody from "../../Organisms/Body/StatsBody/StatsBody";
+import StatsBody from "../../Organisms/Body/StatsBody/StatsMainBody/StatsBody";
 
 const Stats = props => {
   const { title } = props;
