d663a36578426d5859db961bf6809cc752bf0379
CutyApple <dbtldhs0915@naver.com>
2020-12-16 20:10:24 +0900

    [FEAT] <StatsForClass> Create File

---
+++ src/component/Stats/StatsForClass/StatsForClass.js
@@ -0,0 +1,19 @@
+import React from "react";
+import * as S from "./styles";
+import StatsForClassBody from "../../Organisms/Body/StatsBody/StatsForClassBody/StatsForClassBody";
+import HeaderWithBackBtn from "../../Organisms/Header/HeaderWithBackBtn/HeaderWithBackBtn";
+
+const StatsForClass = props => {
+  const { link, title } = props;
+
+  return (
+    <S.Container>
+      <S.HeaderOnlyTitleWrap>
+        <HeaderWithBackBtn link={link} title={title} />
+      </S.HeaderOnlyTitleWrap>
+      <StatsForClassBody />
+    </S.Container>
+  );
+};
+
+export default StatsForClass;
