7a283e3ad69d42067b2ddc12edeb53fc1f9c0060
CutyApple <dbtldhs0915@naver.com>
2020-12-16 22:20:42 +0900

    [FIX] <StatsForClassFloorContainer.js> change import file's name

--- src/container/Stats/StatsForClassFloorContainer.js
+++ src/container/Stats/StatsForClassFloorContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import StatsForClass from "../../component/Stats/StatsForClass/StatsForClass";
+import StatsForClassFloor from "../../component/Stats/StatsForClassFloor/StatsForClassFloor";
 import {
   STATS_PAGE_TITLE,
   STATS_URL
@@ -9,7 +9,7 @@ const StatsForClassContainer = () => {
   const statsMainUrl = STATS_URL.statsMain;
   const statsForClassTitle = STATS_PAGE_TITLE.statsForClass;
 
-  return <StatsForClass link={statsMainUrl} title={statsForClassTitle} />;
+  return <StatsForClassFloor link={statsMainUrl} title={statsForClassTitle} />;
 };
 
 export default StatsForClassContainer;
