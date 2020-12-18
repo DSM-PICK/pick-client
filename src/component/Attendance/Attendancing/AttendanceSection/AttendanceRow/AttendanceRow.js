51c6171db35d90e308aad11cb3464544e6e608eb
CutyApple <dbtldhs0915@naver.com>
2020-12-18 23:33:20 +0900

    [FEAT] <AttendanceRow.js> Auto Attendance

--- src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/AttendanceRow.js
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/AttendanceRow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import * as S from "./styles";
 import AttendanceCell from "./AttendanceCell/AttendanceCell";
 import { useDispatch } from "react-redux";
@@ -8,54 +8,57 @@ const AttendanceRow = props => {
   const { index } = props;
   const { name, state: states, gradeClassNumber } = props.attendance;
 
-  const statesArr = Object.values(states).filter(state => !!state);
+  const [statesArr, setStatesArr] = useState(
+    Object.values(states).filter(state => !!state)
+  );
+  const cascadeState = ["귀가"];
+  const periodArr = statesArr.length === 3 ? [8, 9, 10] : [7, 8, 9, 10];
 
   const dispatch = useDispatch();
-
-  const onSevenClick = value => {
-    dispatch(
-      postAttendanceStdDataSaga({
-        number: gradeClassNumber,
-        period: 7,
-        state: value
-      })
-    );
+  const postAttendanceStdData = useCallback(
+    (period, value) => {
+      dispatch(
+        postAttendanceStdDataSaga({
+          number: gradeClassNumber,
+          period,
+          state: value
+        })
+      );
+    },
+    [dispatch, gradeClassNumber]
+  );
+  const onStateChange = (period, value) => {
+    if (~cascadeState.findIndex(state => state === value)) {
+      cascadeClick(period, value);
+    } else {
+      postAttendanceStdData(period, value);
+      onClickItems(period, value);
+    }
   };
-
-  const onEightClick = value => {
-    dispatch(
-      postAttendanceStdDataSaga({
-        number: gradeClassNumber,
-        period: 8,
-        state: value
-      })
-    );
+  const cascadeClick = (period, value) => {
+    let tempArr = [];
+    for (let i = 0; i < period - 7; i++) {
+      tempArr.push(statesArr[i]);
+    }
+    for (let p = period; p <= 10; p++) {
+      postAttendanceStdData(p, value);
+      tempArr.push(value);
+    }
+    setStatesArr(tempArr);
   };
-
-  const onNineClick = value => {
-    dispatch(
-      postAttendanceStdDataSaga({
-        number: gradeClassNumber,
-        period: 9,
-        state: value
-      })
-    );
-  };
-
-  const onTenClick = value => {
-    dispatch(
-      postAttendanceStdDataSaga({
-        number: gradeClassNumber,
-        period: 10,
-        state: value
-      })
-    );
-  };
-
-  const onClickFuncArr =
-    statesArr.length === 3
-      ? [onEightClick, onNineClick, onTenClick]
-      : [onSevenClick, onEightClick, onNineClick, onTenClick];
+  const onClickItems = useCallback(
+    (period, value) => {
+      setStatesArr(
+        statesArr.map((state, index) => {
+          if (index === period - 7) {
+            return value;
+          }
+          return state;
+        })
+      );
+    },
+    [statesArr]
+  );
 
   return (
     <S.Containter>
@@ -67,8 +70,10 @@ const AttendanceRow = props => {
           <S.SectionClass key={state + idx}>
             <AttendanceCell
               index={index}
+              period={periodArr[idx]}
               periodState={state}
-              onClassClick={onClickFuncArr[idx]}
+              onStateChange={onStateChange}
+              onClickItems={onClickItems}
             />
           </S.SectionClass>
         ))}
