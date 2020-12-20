5e3ffc715d4133fd052c46be91140ace5bf3e783
CutyApple <dbtldhs0915@naver.com>
2020-12-21 07:33:59 +0900

    [FIX] <PreReportDate/styles.js> Modify CSS

--- src/component/Main/PreReport/PreReportDate/styles.js
+++ src/component/Main/PreReport/PreReportDate/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
   position: absolute;
   display: flex;
   left: 65px;
-  width: 219px;
+  width: min-content;
   height: 60px;
   background: #fafafa;
   flex-direction: column;
@@ -13,7 +13,7 @@ export const Container = styled.div`
 
 export const Date = styled.div`
   display: flex;
-  width: 219px;
+  width: min-content;
   height: 26px;
   font-size: 18px;
 `;
