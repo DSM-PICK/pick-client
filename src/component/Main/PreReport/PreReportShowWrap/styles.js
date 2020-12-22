199efca6695e825ba286098b7d8aa0ca24b0b4d4
CutyApple <dbtldhs0915@naver.com>
2020-12-22 14:24:31 +0900

    [FEAT] <PreReportShowWrap> Create File

---
+++ src/component/Main/PreReport/PreReportShowWrap/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  width: calc(100% - 40px);
+  height: 260px;
+  margin: 10px 0 30px 0;
+  padding: 20px !important;
+  border-radius: 40px;
+  background: #fafafa;
+  box-shadow: 0 6px 9px 0 rgba(0, 0, 0, 0.16);
+`;
