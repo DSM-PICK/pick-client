e785838b83afc5a80c35d2052b675984f7d2df0c
CutyApple <dbtldhs0915@naver.com>
2021-01-03 22:14:17 +0900

    [FEAT] <PreReportRemark> Create File

---
+++ src/component/Main/PreReport/PreReportRemark/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  width: 140px;
+  height: 27px;
+  flex-direction: column;
+  margin-right: 20px;
+`;
+
+export const Name = styled.input`
+  display: flex;
+  padding: 0;
+  width: 140px;
+  height: 27px;
+  border: none;
+  border-bottom: 1px solid #707070;
+  background: #fafafa;
+  color: black;
+  font-size: 24px;
+  text-align: center;
+  outline: none;
+`;
