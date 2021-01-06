8ce10f19303b27670711cb5b73d6b30d7d5c31d0
CutyApple <dbtldhs0915@naver.com>
2021-01-06 18:49:15 +0900

    [FEAT] <D/Molecules/HeaderNavLinkWrap>

---
+++ src/component/Desktop/Molecules/HeaderNavLinkWrap/HeaderNavLinkWrap.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavLink from "../../Atoms/NavLink/NavLink";
+import * as S from "./styles";
+
+const HeaderNavLinkWrap = () => {
+  return (
+    <S.Container>
+      <NavLink />
+    </S.Container>
+  );
+};
+
+export default HeaderNavLinkWrap;
