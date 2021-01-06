b05af19c658b21910bb26b7fb81ae84e9fe55dc4
CutyApple <dbtldhs0915@naver.com>
2021-01-06 19:00:02 +0900

    [FEAT] <D/Atoms/NavLink/styles.js> Set Style

--- src/component/Desktop/Atoms/NavLink/styles.js
+++ src/component/Desktop/Atoms/NavLink/styles.js
@@ -1,4 +1,15 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export const Container = styled(NavLink)``;
+export const Container = styled(NavLink)`
+  width: ${props => props.width || "auto"};
+  height: ${props => props.height || "auto"};
+  color: ${props => props.color || "#000000"};
+  font-size: ${props => props.fontSize || "16px"};
+  font-weight: ${props => props.fontWeight || "400"};
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.color_hover || "#505050"};
+  }
+`;
