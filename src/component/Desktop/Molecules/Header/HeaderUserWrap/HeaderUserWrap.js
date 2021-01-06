e383a5d83957f2c1ed0b8575f217c98c78ab82aa
CutyApple <dbtldhs0915@naver.com>
2021-01-06 21:39:49 +0900

    [FEAT] <D/Molecules/HeaderUserWrap> Complete

--- src/component/Desktop/Molecules/Header/HeaderUserWrap/HeaderUserWrap.js
+++ src/component/Desktop/Molecules/Header/HeaderUserWrap/HeaderUserWrap.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { DropDownArrow } from "../../../../../asset/Desktop";
+import Img from "../../../Atoms/Img/Img";
 import * as S from "./styles";
 
-const HeaderUserWrap = () => {
-  return <S.Container></S.Container>;
+const HeaderUserWrap = props => {
+  const { userName } = props;
+
+  return (
+    <S.Container>
+      <S.UserName>{`${userName} 선생님`}</S.UserName>
+      <Img src={DropDownArrow} css={S.ImgCSS} />
+    </S.Container>
+  );
 };
 
 export default HeaderUserWrap;
--- src/component/Desktop/Molecules/Header/HeaderUserWrap/styles.js
+++ src/component/Desktop/Molecules/Header/HeaderUserWrap/styles.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 122px;
+  height: 23px;
+  padding: 0;
+  border: none;
+  outline: none;
+  background: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const UserName = styled.div`
+  width: max-content;
+  height: max-content;
+  color: #000000;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const ImgCSS = {
+  width: "16px",
+  height: "8px"
+};
