0193d8ee902719379bb061ce01eb7df0bf89a486
CutyApple <dbtldhs0915@naver.com>
2021-01-06 18:59:43 +0900

    [FEAT] <D/Molecules/HeaderNavLinkWrap.js> Apply NavLink

--- src/component/Desktop/Molecules/HeaderNavLinkWrap/HeaderNavLinkWrap.js
+++ src/component/Desktop/Molecules/HeaderNavLinkWrap/HeaderNavLinkWrap.js
@@ -2,10 +2,38 @@ import React from "react";
 import NavLink from "../../Atoms/NavLink/NavLink";
 import * as S from "./styles";
 
-const HeaderNavLinkWrap = () => {
+const HeaderNavLinkWrap = props => {
+  const { css, activeStyle } = props;
+
+  const HeaderNavLinkList = [
+    {
+      to: "/",
+      text: "출석체크"
+    },
+    {
+      to: "/pre-report",
+      text: "출결등록"
+    },
+    {
+      to: "/schedule",
+      text: "일정확인"
+    },
+    {
+      to: "/stats",
+      text: "출석조회"
+    }
+  ];
+
   return (
     <S.Container>
-      <NavLink />
+      {HeaderNavLinkList.map(dataObj => (
+        <NavLink
+          key={dataObj.text}
+          {...dataObj}
+          css={css}
+          activeStyle={activeStyle}
+        />
+      ))}
     </S.Container>
   );
 };
