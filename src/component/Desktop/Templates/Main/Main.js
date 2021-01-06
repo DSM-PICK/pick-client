79d281e4bc3a4f9dc240214fd480dd62d29da118
CutyApple <dbtldhs0915@naver.com>
2021-01-06 11:52:26 +0900

    [FEAT] <D/Templates/Main.js> Apply Header Component

--- src/component/Desktop/Templates/Main/Main.js
+++ src/component/Desktop/Templates/Main/Main.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Header from "../../Organisms/Header/Header";
 import * as S from "./styles";
 
 const Main = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <Header />
+    </S.Container>
+  );
 };
 
 export default Main;
