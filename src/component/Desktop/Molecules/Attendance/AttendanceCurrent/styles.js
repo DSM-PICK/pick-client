90b54103c0318a243d757fbdc3aa3491f4a23246
CutyApple <dbtldhs0915@naver.com>
2021-01-07 12:41:46 +0900

    [FIX] <D/Molecules/AttendanceCurrent/styles.js> Fix Style

--- src/component/Desktop/Molecules/Attendance/AttendanceCurrent/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/styles.js
@@ -15,7 +15,7 @@ export const LabelCSS = {
 
 export const BackgroundCSS = {
   width: "100%",
-  height: "100%",
+  height: "calc(100% - 22px)",
   borderRadius: "12px",
   background: "#ffffff",
   boxShadow: "0px 1px 1px 0px rgba(144, 144, 144, 0.2)"
