21dcc607b289b689d4017b4993aef12f3f72dd76
CutyApple <dbtldhs0915@naver.com>
2021-01-07 09:49:11 +0900

    [FEAT] <D/Atoms/Background/styles.js> Add Style

--- src/component/Desktop/Atoms/Background/styles.js
+++ src/component/Desktop/Atoms/Background/styles.js
@@ -6,5 +6,6 @@ export const Container = styled.div`
   justify-content: ${props => props.justifycontent || "baseline"};
   width: ${props => props.width || "auto"};
   height: ${props => props.height || "auto"};
+  border-radius: ${props => props.borderRadius || "0"};
   background: ${props => props.background || "#ffffff"};
 `;
