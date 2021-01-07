8ead507dbbcc4b74555ee3eeb0d5bc76f050021c
CutyApple <dbtldhs0915@naver.com>
2021-01-07 10:02:39 +0900

    [FEAT] <D/Molecules/AttendancePlace> Set Background

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlace.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlace.js
@@ -1,9 +1,14 @@
 import React from "react";
 import * as S from "./styles";
+import Label from "../../../Atoms/Label/Label";
 import Background from "../../../Atoms/Background/Background";
+
 const AttendancePlace = () => {
   return (
     <S.Container>
+      <S.LableHeader>
+        <Label text={"출석 위치 선택"} css={S.LabelCSS} />
+      </S.LableHeader>
       <Background css={S.BackgroundCSS}></Background>
     </S.Container>
   );
