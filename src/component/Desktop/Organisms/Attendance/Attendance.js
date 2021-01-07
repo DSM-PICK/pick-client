26c5f90158d78cbe025951b1a45d917095b4fcb1
CutyApple <dbtldhs0915@naver.com>
2021-01-07 09:50:09 +0900

    [FEAT] <D/Organisms/Attendance> Init

---
+++ src/component/Desktop/Organisms/Attendance/Attendance.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AttendancePlace from "../../Molecules/Attendance/AttendancePlace/AttendancePlace";
+import * as S from "./styles";
+
+const Attendance = () => {
+  return (
+    <S.Container>
+      <AttendancePlace />
+    </S.Container>
+  );
+};
+
+export default Attendance;
