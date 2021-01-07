896e0f1db250b43470de60bded885bb9acb98659
CutyApple <dbtldhs0915@naver.com>
2021-01-07 15:09:04 +0900

    [FEAT] <D/Molecules/Toggle> Toggle Complete

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/Toggle/Toggle.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/Toggle/Toggle.js
@@ -1,8 +1,24 @@
 import React from "react";
 import * as S from "./styles";
 
-const Toggle = () => {
-  return <S.Container></S.Container>;
+const Toggle = props => {
+  const { toggle, onClick } = props;
+
+  console.log(toggle);
+
+  return (
+    <S.Container>
+      <S.Toggle toggle={toggle} onClick={onClick}>
+        <S.ToggleItem toggle={toggle} />
+        <S.ToggleText toggle={toggle} state={true}>
+          교실자습
+        </S.ToggleText>
+        <S.ToggleText toggle={toggle} state={false}>
+          전공동아리
+        </S.ToggleText>
+      </S.Toggle>
+    </S.Container>
+  );
 };
 
 export default Toggle;
--- src/component/Desktop/Molecules/Attendance/AttendancePlace/Toggle/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/Toggle/styles.js
@@ -1,3 +1,40 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Toggle = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 120px;
+  height: 20px;
+  padding: 1px;
+  border-radius: 5px;
+  background: #ffffff;
+  box-shadow: 0px 1px 1px 0px rgba(144, 144, 144, 0.2);
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const ToggleText = styled.div`
+  position: relative;
+  z-index: 3;
+  color: ${props => (props.state === props.toggle ? "#909090" : "#ffffff")};
+  font-size: 12px;
+  font-weight: 400;
+  transition: all 0.3s ease-in-out;
+`;
+
+export const ToggleItem = styled.div`
+  position: absolute;
+  z-index: 2;
+  width: ${props => (props.toggle ? "64px" : "52px")};
+  height: 100%;
+  border-radius: 5px;
+  background: rgba(64, 108, 255, 1);
+  left: ${props => (props.toggle ? "calc(100% - 64px)" : "0")};
+  transition: all 0.3s ease-in-out;
+`;
