63d35ed430e1cab0b4f90927a1d22e504088ebb9
CutyApple <dbtldhs0915@naver.com>
2021-01-07 12:41:55 +0900

    [FIX] <D/Molecules/AttendanceStdList/styles.js> Fix Style

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
 
 export const LableHeader = styled.div`
   display: flex;
@@ -15,7 +18,7 @@ export const LabelCSS = {
 
 export const BackgroundCSS = {
   width: "100%",
-  height: "100%",
+  height: "calc(100% - 22px)",
   borderRadius: "12px",
   background: "#ffffff",
   boxShadow: "0px 1px 1px 0px rgba(144, 144, 144, 0.2)"
