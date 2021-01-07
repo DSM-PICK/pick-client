1cede447f3cc20b3d411e6cd88a838372fddfa76
CutyApple <dbtldhs0915@naver.com>
2021-01-07 12:42:21 +0900

    [FEAT] <D/Organisms/Attendance> Init Background

--- src/component/Desktop/Organisms/Attendance/Attendance.js
+++ src/component/Desktop/Organisms/Attendance/Attendance.js
@@ -1,11 +1,19 @@
 import React from "react";
+import AttendanceCurrent from "../../Molecules/Attendance/AttendanceCurrent/AttendanceCurrent";
 import AttendancePlace from "../../Molecules/Attendance/AttendancePlace/AttendancePlace";
+import AttendanceStdList from "../../Molecules/Attendance/AttendanceStdList/AttendanceStdList";
 import * as S from "./styles";
 
 const Attendance = () => {
   return (
     <S.Container>
-      <AttendancePlace />
+      <S.LeftGridWrap>
+        <AttendancePlace />
+        <AttendanceCurrent />
+      </S.LeftGridWrap>
+      <S.RightWrap>
+        <AttendanceStdList />
+      </S.RightWrap>
     </S.Container>
   );
 };
