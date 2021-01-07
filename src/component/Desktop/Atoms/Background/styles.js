d41a825f206fcac805b7680180e6f07a6e8cd5ef
CutyApple <dbtldhs0915@naver.com>
2021-01-07 09:32:23 +0900

    [FEAT] <D/Atoms/Background> Create Files

---
+++ src/component/Desktop/Atoms/Background/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: ${props => props.display || "flex"};
+  align-items: ${props => props.alignitems || "baseline"};
+  justify-content: ${props => props.justifycontent || "baseline"};
+  width: ${props => props.width || "auto"};
+  height: ${props => props.height || "auto"};
+  background: ${props => props.background || "#ffffff"};
+`;
