f237ac38f6d85644c3dcece1908aeec7d577fd75
CutyApple <dbtldhs0915@naver.com>
2021-01-09 01:26:13 +0900

    [FEAT] <D/DesktopSelectWrapContainer.js> Set Static Container

--- src/container/Desktop/Main/Attendance/DesktopSelectWrapContainer.js
+++ src/container/Desktop/Main/Attendance/DesktopSelectWrapContainer.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import SelectWrap from "../../../../component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap";
 
 const DesktopSelectWrapContainer = () => {
-  const SelectArr = [
+  const selectArr = [
     {
       header: "학년",
       bodyItem: ["1학년", "2학년", "3학년", "기타"]
@@ -13,7 +13,26 @@ const DesktopSelectWrapContainer = () => {
     }
   ];
 
-  return <SelectWrap selectArr={SelectArr} />;
+  const [currentArr, setCurrentArr] = useState(
+    Array.from({ length: selectArr.length }, () => 0)
+  );
+
+  const setCurrentArrByIndex = useCallback(
+    (row, col) => {
+      setCurrentArr(
+        currentArr.map((current, index) => (index === row ? col : current))
+      );
+    },
+    [currentArr]
+  );
+
+  return (
+    <SelectWrap
+      selectArr={selectArr}
+      currentArr={currentArr}
+      onClick={setCurrentArrByIndex}
+    />
+  );
 };
 
 export default DesktopSelectWrapContainer;
