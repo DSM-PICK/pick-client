43ecadda987439b116091dfe4b1f8fd49dc25997
CutyApple <dbtldhs0915@naver.com>
2021-01-08 13:19:00 +0900

    [FEAT] <D/Templates/Main> Set Grid

--- src/component/Desktop/Templates/Main/Main.js
+++ src/component/Desktop/Templates/Main/Main.js
@@ -1,11 +1,17 @@
 import React from "react";
 import DesktopHeaderContainer from "../../../../container/Desktop/Header/DesktopHeaderContainer";
+import Attendance from "../../Organisms/Attendance/Attendance";
 import * as S from "./styles";
 
 const Main = () => {
   return (
     <S.Container>
       <DesktopHeaderContainer />
+      <S.AttendanceWrap>
+        <S.AttendanceGridWrap>
+          <Attendance />
+        </S.AttendanceGridWrap>
+      </S.AttendanceWrap>
     </S.Container>
   );
 };
--- src/component/Desktop/Templates/Main/styles.js
+++ src/component/Desktop/Templates/Main/styles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const AttendanceWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: calc(100% - 60px);
+`;
+
+export const AttendanceGridWrap = styled.div`
+  display: grid;
+  width: 1072px;
+  height: 100%;
+`;
