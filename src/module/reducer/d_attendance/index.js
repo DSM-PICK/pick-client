8ace159b717f8f83313a2001e61fa7d9bd763696
CutyApple <dbtldhs0915@naver.com>
2021-01-09 01:40:48 +0900

    [FEAT] <reducer/d_attendance/index.js> Add Reducer

---
+++ src/module/reducer/d_attendance/index.js
@@ -0,0 +1,23 @@
+import { DAttendanceAction } from "../../action/d_attendance";
+
+const initialState = {
+    currentAttendanceIndexArr = [],
+};
+
+const DAttendanceReducer = (state = initialState, action) => {
+    const { SET_CURRENT_ATTENDANCE_INDEX_ARR } = DAttendanceAction;
+
+    switch (action.type) {
+        case SET_CURRENT_ATTENDANCE_INDEX_ARR : {
+            return {
+                ...state,
+                currentAttendanceIndexArr: action.payload
+            }
+        }
+        default : {
+            return state;
+        }
+    }
+}
+
+export default DAttendanceReducer;
