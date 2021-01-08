5e468708471d2ac19bd168c226255ad6a12f7562
CutyApple <dbtldhs0915@naver.com>
2021-01-08 11:10:41 +0900

    [FEAT] <D/DesktopAttendancePlaceContainer.js> Create File

---
+++ src/container/Desktop/Main/Attendance/DesktopAttendancePlaceContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AttendancePlace from "../../../../component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlace";
+
+const DesktopAttendancePlaceContainer = () => {
+  return <AttendancePlace />;
+};
+
+export default DesktopAttendancePlaceContainer;
