59a85241e1c03e7df55d41577d331cc8701ade56
CutyApple <dbtldhs0915@naver.com>
2021-01-09 01:42:02 +0900

    [FIX] <reducer/d_attendance/index.js>

--- src/module/reducer/d_attendance/index.js
+++ src/module/reducer/d_attendance/index.js
@@ -1,23 +1,23 @@
 import { DAttendanceAction } from "../../action/d_attendance";
 
 const initialState = {
-    currentAttendanceIndexArr = [],
+  currentAttendanceIndexArr: []
 };
 
 const DAttendanceReducer = (state = initialState, action) => {
-    const { SET_CURRENT_ATTENDANCE_INDEX_ARR } = DAttendanceAction;
+  const { SET_CURRENT_ATTENDANCE_INDEX_ARR } = DAttendanceAction;
 
-    switch (action.type) {
-        case SET_CURRENT_ATTENDANCE_INDEX_ARR : {
-            return {
-                ...state,
-                currentAttendanceIndexArr: action.payload
-            }
-        }
-        default : {
-            return state;
-        }
+  switch (action.type) {
+    case SET_CURRENT_ATTENDANCE_INDEX_ARR: {
+      return {
+        ...state,
+        currentAttendanceIndexArr: action.payload
+      };
     }
-}
+    default: {
+      return state;
+    }
+  }
+};
 
 export default DAttendanceReducer;
