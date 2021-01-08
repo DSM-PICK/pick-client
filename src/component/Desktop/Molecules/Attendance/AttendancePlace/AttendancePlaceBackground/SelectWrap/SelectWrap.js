b0ca6b1f0182d1c34dd276fba2a3ccca33d95151
CutyApple <dbtldhs0915@naver.com>
2021-01-09 01:26:34 +0900

    [REFACTOR] <D/Molecules/SelectWrap.js> Apply Container

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
@@ -1,34 +1,13 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import * as S from "./styles";
 
-const SelectWrap = () => {
-  const SelectArr = [
-    {
-      header: "학년",
-      bodyItem: ["1학년", "2학년", "3학년", "기타"]
-    },
-    {
-      header: "반",
-      bodyItem: ["1반", "2반", "3반", "4반"]
-    }
-  ];
-
-  const [currentArr, setCurrentArr] = useState(
-    Array.from({ length: SelectArr.length }, () => 0)
-  );
-
-  const setCurrentArrByIndex = useCallback(
-    (row, col) => {
-      setCurrentArr(
-        currentArr.map((current, index) => (index === row ? col : current))
-      );
-    },
-    [currentArr]
-  );
+const SelectWrap = props => {
+  const { selectArr, currentArr } = props;
+  const { onClick } = props;
 
   return (
     <S.Container>
-      {SelectArr.map((data, rowIndex) => (
+      {selectArr.map((data, rowIndex) => (
         <S.Select key={data.header}>
           <S.SelectHeader>{data.header}</S.SelectHeader>
           <S.SelectBody>
@@ -36,7 +15,7 @@ const SelectWrap = () => {
               <S.SelectItem
                 key={item}
                 select={currentArr[rowIndex] === colIndex}
-                onClick={() => setCurrentArrByIndex(rowIndex, colIndex)}
+                onClick={() => onClick(rowIndex, colIndex)}
               >
                 {item}
               </S.SelectItem>
