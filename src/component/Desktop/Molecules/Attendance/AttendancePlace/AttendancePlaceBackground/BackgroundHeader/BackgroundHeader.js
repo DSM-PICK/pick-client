abda50fa0d06259e3984b3fd314f24fc30dba122
CutyApple <dbtldhs0915@naver.com>
2021-01-08 13:18:02 +0900

    [FEAT] <D/Molecules/BackgroundHeader> Create Files

---
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/BackgroundHeader/BackgroundHeader.js
@@ -0,0 +1,26 @@
+import React from "react";
+import * as S from "./styles";
+import Button from "../../../../../Atoms/Button/Button";
+
+const BackgroundHeader = () => {
+  const userName = "최아린";
+  const subText = "2-2 담임이십니다";
+
+  return (
+    <S.Container>
+      <S.TextWrap>
+        <S.HeaderText>{`${userName} 선생님은`}</S.HeaderText>
+        <S.HeaderText>{`${subText}`}</S.HeaderText>
+      </S.TextWrap>
+      <S.ButtonWrap>
+        <Button
+          text={"빠른 조회하기"}
+          css={S.HeaderButtonCSS}
+          onClick={() => console.log("빠른 조회하기")}
+        />
+      </S.ButtonWrap>
+    </S.Container>
+  );
+};
+
+export default BackgroundHeader;
