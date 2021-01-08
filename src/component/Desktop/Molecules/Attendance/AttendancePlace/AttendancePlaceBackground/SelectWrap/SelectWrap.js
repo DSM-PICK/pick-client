62ad45ae90c75e41f00e1f254441842ad7fed503
CutyApple <dbtldhs0915@naver.com>
2021-01-09 00:56:15 +0900

    [FEAT] <D/Molecules/SelectWrap> Static

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
@@ -2,7 +2,28 @@ import React from "react";
 import * as S from "./styles";
 
 const SelectWrap = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.Select>
+        <S.SelectHeader>학년</S.SelectHeader>
+        <S.SelectBody>
+          <S.SelectItem>1학년</S.SelectItem>
+          <S.SelectItem>2학년</S.SelectItem>
+          <S.SelectItem>3학년</S.SelectItem>
+          <S.SelectItem>기타</S.SelectItem>
+        </S.SelectBody>
+      </S.Select>
+      <S.Select>
+        <S.SelectHeader>반</S.SelectHeader>
+        <S.SelectBody>
+          <S.SelectItem>1반</S.SelectItem>
+          <S.SelectItem>2반</S.SelectItem>
+          <S.SelectItem>3반</S.SelectItem>
+          <S.SelectItem>4반</S.SelectItem>
+        </S.SelectBody>
+      </S.Select>
+    </S.Container>
+  );
 };
 
 export default SelectWrap;
