3516a1cafd09324fb4e1886e8d29337747ea44a1
CutyApple <dbtldhs0915@naver.com>
2021-01-08 13:18:39 +0900

    [REFACTOR] <D/Molecules/AttendancePlaceBackground>

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
@@ -9,47 +9,6 @@ export const Container = styled.div`
   padding: 12px 20px;
 `;
 
-export const BackgroundHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 100%;
-`;
-
-export const TextWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: fit-content;
-  height: 100%;
-`;
-
-export const HeaderText = styled.p`
-  display: flex;
-  align-items: center;
-  width: max-content;
-  height: 20px;
-  font-size: 16px;
-`;
-
-export const ButtonWrap = styled.div`
-  width: 140px;
-  height: 100%;
-`;
-
-export const HeaderButtonCSS = {
-  width: "100%",
-  height: "100%",
-  color: "#ffffff",
-  borderRadius: "12px",
-  background: "#406CFF",
-
-  hoverBackground: "#2D54D5",
-  activeBackground: "#2849b5"
-};
-
 export const BackgroundButtonWrap = styled.div`
   width: 100%;
   height: 100%;
