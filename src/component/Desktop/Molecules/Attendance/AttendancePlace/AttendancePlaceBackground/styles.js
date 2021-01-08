b002064e830ba7a596262f41feed7e5c55d2325e
CutyApple <dbtldhs0915@naver.com>
2021-01-08 10:44:04 +0900

    [FIX] <D/Molecules/AttendancePlace/styles.js> Set Grid

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
@@ -22,19 +22,20 @@ export const TextWrap = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  width: 100%;
+  width: fit-content;
   height: 100%;
 `;
 
 export const HeaderText = styled.p`
   display: flex;
   align-items: center;
+  width: max-content;
   height: 20px;
   font-size: 16px;
 `;
 
 export const ButtonWrap = styled.div`
-  width: 100%;
+  width: 140px;
   height: 100%;
 `;
 
