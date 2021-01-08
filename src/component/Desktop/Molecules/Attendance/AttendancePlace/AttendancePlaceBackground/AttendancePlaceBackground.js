42b1c3b75c21fa71bec1aae31223b5b066e05a4b
CutyApple <dbtldhs0915@naver.com>
2021-01-08 10:26:21 +0900

    [FEAT] <D/Molecules/AttendancePlaceBackground> Set Button

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground.js
@@ -1,8 +1,20 @@
 import React from "react";
 import * as S from "./styles";
+import Button from "../../../../Atoms/Button/Button";
 
 const AttendancePlaceBackground = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.BackgroundHeader></S.BackgroundHeader>
+      <S.BackgroundButtonWrap>
+        <Button
+          text={"조회하기"}
+          css={S.ButtonCSS}
+          onClick={() => alert("wow!")}
+        />
+      </S.BackgroundButtonWrap>
+    </S.Container>
+  );
 };
 
 export default AttendancePlaceBackground;
--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/styles.js
@@ -1,3 +1,29 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 50px 1fr 40px;
+  width: 100%;
+  height: 100%;
+  padding: 12px 20px;
+`;
+
+export const BackgroundHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+export const BackgroundButtonWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+export const ButtonCSS = {
+  width: "100%",
+  height: "100%",
+  color: "#ffffff",
+  borderRadius: "12px",
+  background: "#406CFF"
+};
