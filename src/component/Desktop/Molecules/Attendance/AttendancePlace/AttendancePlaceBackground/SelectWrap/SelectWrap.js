eff4bccb2c547905f6329b70e4f81dca2a1ff5c8
CutyApple <dbtldhs0915@naver.com>
2021-01-09 01:20:11 +0900

    [FEAT] <D/Molecules/SelectWrap.js> Dynamic Variable

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import * as S from "./styles";
 
 const SelectWrap = () => {
@@ -8,19 +8,38 @@ const SelectWrap = () => {
       bodyItem: ["1학년", "2학년", "3학년", "기타"]
     },
     {
-      header: "학년",
-      bodyItem: ["1학년", "2학년", "3학년", "기타"]
+      header: "반",
+      bodyItem: ["1반", "2반", "3반", "4반"]
     }
   ];
 
+  const [currentArr, setCurrentArr] = useState(
+    Array.from({ length: SelectArr.length }, () => 0)
+  );
+
+  const setCurrentArrByIndex = useCallback(
+    (row, col) => {
+      setCurrentArr(
+        currentArr.map((current, index) => (index === row ? col : current))
+      );
+    },
+    [currentArr]
+  );
+
   return (
     <S.Container>
-      {SelectArr.map(data => (
+      {SelectArr.map((data, rowIndex) => (
         <S.Select key={data.header}>
           <S.SelectHeader>{data.header}</S.SelectHeader>
           <S.SelectBody>
-            {data.bodyItem.map(item => (
-              <S.SelectItem key={item}>{item}</S.SelectItem>
+            {data.bodyItem.map((item, colIndex) => (
+              <S.SelectItem
+                key={item}
+                select={currentArr[rowIndex] === colIndex}
+                onClick={() => setCurrentArrByIndex(rowIndex, colIndex)}
+              >
+                {item}
+              </S.SelectItem>
             ))}
           </S.SelectBody>
         </S.Select>
