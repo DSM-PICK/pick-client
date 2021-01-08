b114de71092850a6793fe7e4baf6f3d342e9e162
CutyApple <dbtldhs0915@naver.com>
2021-01-08 09:49:23 +0900

    [FEAT] <D/Molecules/AttendancePlaceBackground> Create Files

---
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const AttendancePlaceBackground = () => {
+  return <S.Container></S.Container>;
+};
+
+export default AttendancePlaceBackground;
