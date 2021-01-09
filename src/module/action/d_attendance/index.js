225dd87d50eb0ec17e7a23cfce99e83b358dfe38
CutyApple <dbtldhs0915@naver.com>
2021-01-09 13:11:55 +0900

    [FEAT] <action/d_attendance/index.js> Add Action

--- src/module/action/d_attendance/index.js
+++ src/module/action/d_attendance/index.js
@@ -1,15 +1,25 @@
+const SET_SELECT_ATTENDANCE_ARR = "d_attendance/SET_SELECT_ATTENDANCE_ARR";
+const setSelectAttendanceArr = payload => ({
+  type: SET_SELECT_ATTENDANCE_ARR,
+  payload
+});
+const GET_SELECT_ATTENDANCE_ARR_SAGA =
+  "d_attendance/GET_SELECT_ATTENDANCE_ARR_SAGA";
+
 const SET_CURRENT_ATTENDANCE_INDEX_ARR =
   "d_attendance/SET_CURRENT_ATTENDANCE_INDEX_ARR";
-
 const setCurrentAttendanceIndexArr = payload => ({
   type: SET_CURRENT_ATTENDANCE_INDEX_ARR,
   payload
 });
 
 export const DAttendanceAction = {
+  SET_SELECT_ATTENDANCE_ARR,
+  GET_SELECT_ATTENDANCE_ARR_SAGA,
   SET_CURRENT_ATTENDANCE_INDEX_ARR
 };
 
 export const DAttendanceActionCreater = {
+  setSelectAttendanceArr,
   setCurrentAttendanceIndexArr
 };
