15aa239c3d8e86a85647d68445e299b3a0f4081a
CutyApple <dbtldhs0915@naver.com>
2021-01-09 18:35:30 +0900

    [FEAT] <D/DAttendancePlaceBackgroundContainer.js> Create File

---
+++ src/container/Desktop/Main/Attendance/DAttendancePlaceBackgroundContainer..s
@@ -0,0 +1,8 @@
+import React from "react";
+import AttendancePlaceBackground from "../../../../component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground";
+
+const DAttendancePlaceBackgroundContainer = () => {
+  return <AttendancePlaceBackground></AttendancePlaceBackground>;
+};
+
+export default DAttendancePlaceBackgroundContainer;
