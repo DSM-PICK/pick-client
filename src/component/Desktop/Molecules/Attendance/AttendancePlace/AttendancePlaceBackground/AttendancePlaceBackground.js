766bdc6d43ca01dff4653c50dbe79b41aea840be
CutyApple <dbtldhs0915@naver.com>
2021-01-09 18:38:44 +0900

    [FEAT] <D/AttendancePlaceBackground.js> Apply Props

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground.js
@@ -4,17 +4,15 @@ import Button from "../../../../Atoms/Button/Button";
 import BackgroundHeader from "./BackgroundHeader/BackgroundHeader";
 import DesktopSelectWrapContainer from "../../../../../../container/Desktop/Main/Attendance/DesktopSelectWrapContainer";
 
-const AttendancePlaceBackground = () => {
+const AttendancePlaceBackground = props => {
+  const { onClick } = props;
+
   return (
     <S.Container>
       <BackgroundHeader />
       <DesktopSelectWrapContainer />
       <S.BackgroundButtonWrap>
-        <Button
-          text={"조회하기"}
-          css={S.ButtonCSS}
-          onClick={() => console.log("조회하기")}
-        />
+        <Button text={"조회하기"} css={S.ButtonCSS} onClick={onClick} />
       </S.BackgroundButtonWrap>
     </S.Container>
   );
