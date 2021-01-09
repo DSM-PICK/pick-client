58d68047005b308b55097fd8454fe69d2800dee9
CutyApple <dbtldhs0915@naver.com>
2021-01-09 18:31:56 +0900

    [FIX] <action/d_attendance/index.js> Delete Non-used Code

--- src/module/action/d_attendance/index.js
+++ src/module/action/d_attendance/index.js
@@ -10,12 +10,6 @@ const getSelectAttendanceArrSaga = payload => ({
   payload
 });
 
-const SET_SELECT_PRIORITY_ARR = "d_attendance/SET_SELECT_PRIORITY_ARR";
-const setSelectPriorityArr = payload => ({
-  type: SET_SELECT_PRIORITY_ARR,
-  payload
-});
-
 const SET_CURRENT_ATTENDANCE_INDEX_ARR =
   "d_attendance/SET_CURRENT_ATTENDANCE_INDEX_ARR";
 const setCurrentAttendanceIndexArr = payload => ({
@@ -26,13 +20,11 @@ const setCurrentAttendanceIndexArr = payload => ({
 export const DAttendanceAction = {
   SET_SELECT_ATTENDANCE_ARR,
   GET_SELECT_ATTENDANCE_ARR_SAGA,
-  SET_SELECT_PRIORITY_ARR,
   SET_CURRENT_ATTENDANCE_INDEX_ARR
 };
 
 export const DAttendanceActionCreater = {
   setSelectAttendanceArr,
   getSelectAttendanceArrSaga,
-  setSelectPriorityArr,
   setCurrentAttendanceIndexArr
 };
