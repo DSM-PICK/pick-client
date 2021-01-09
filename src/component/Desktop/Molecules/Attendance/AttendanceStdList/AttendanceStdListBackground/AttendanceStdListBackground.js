3deebeebd42bc1f359b1db5799818c80202e34c9
CutyApple <dbtldhs0915@naver.com>
2021-01-10 03:18:34 +0900

    [FEAT] <D/Molecules/AttendnaceStdListBackgroudn> Create File

---
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/AttendanceStdListBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const AttendanceStdListBackground = () => {
+  return <S.Container></S.Container>;
+};
+
+export default AttendanceStdListBackground;
