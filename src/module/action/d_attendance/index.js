c11ccca8ad897dbc01518b6947ee0d0ebe062ec1
CutyApple <dbtldhs0915@naver.com>
2021-01-09 17:29:40 +0900

    [FIX] <action/d_attendance/index.js>

--- src/module/action/d_attendance/index.js
+++ src/module/action/d_attendance/index.js
@@ -25,6 +25,6 @@ export const DAttendanceAction = {
 
 export const DAttendanceActionCreater = {
   setSelectAttendanceArr,
-  getAutoCompleteTextSaga,
+  getSelectAttendanceArrSaga,
   setCurrentAttendanceIndexArr
 };
