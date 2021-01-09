4b34a568772f4032bb85df9bcb49aa7b9f4c2167
CutyApple <dbtldhs0915@naver.com>
2021-01-09 19:39:41 +0900

    [FEAT] <saga/d_attendance/index.js> Success or Failure

--- src/module/saga/d_attendance/index.js
+++ src/module/saga/d_attendance/index.js
@@ -9,15 +9,25 @@ import { requestGetApiWithAccessToken } from "../../../lib/requestApi";
 function* getSelectAttendanceArr(action) {
   try {
     const { schedule, floor } = action.payload;
-    const { setSelectAttendanceArr } = DAttendanceActionCreater;
+    const { SUCCESS_GET_SELECT_ATTENDANCE_ARR_SAGA } = DAttendanceAction;
     const REQUEST_URL = ATTENDANCE.ATTENDANCE_NAVIGATION_URL(schedule, floor);
 
     const floorData = yield call(requestGetApiWithAccessToken, REQUEST_URL);
 
-    const locationArr = floorData.data.locations;
-
-    yield put(setSelectAttendanceArr(locationArr));
+    yield put({
+      type: SUCCESS_GET_SELECT_ATTENDANCE_ARR_SAGA,
+      payload: floorData
+    });
   } catch (error) {}
+}
+
+function* successGetSelectAttendanceArr(action) {
+  const floorData = action.payload;
+  const locationArr = floorData.data.locations;
+
+  const { setSelectAttendanceArr } = DAttendanceActionCreater;
+
+  yield put(setSelectAttendanceArr(locationArr));
 }
 
 function* getAttendanceStdData(action) {
@@ -42,11 +52,31 @@ function* getAttendanceStdData(action) {
 function* dAttendanceSaga() {
   const {
     GET_ATTENDANCE_STD_DATA_SAGA,
-    GET_SELECT_ATTENDANCE_ARR_SAGA
+    SUCCESS_GET_ATTENDANCE_STD_DATA_SAGA,
+    FAILURE_GET_ATTENDANCE_STD_DATA_SAGA,
+    GET_SELECT_ATTENDANCE_ARR_SAGA,
+    SUCCESS_GET_SELECT_ATTENDANCE_ARR_SAGA,
+    FAILURE_GET_SELECT_ATTENDANCE_ARR_SAGA
   } = DAttendanceAction;
 
-  yield takeEvery(GET_SELECT_ATTENDANCE_ARR_SAGA, getSelectAttendanceArr);
   yield takeEvery(GET_ATTENDANCE_STD_DATA_SAGA, getAttendanceStdData);
+  // yield takeEvery(
+  //   SUCCESS_GET_ATTENDANCE_STD_DATA_SAGA,
+  //   successGetAttendanceStdData
+  // );
+  // yield takeEvery(
+  //   FAILURE_GET_ATTENDANCE_STD_DATA_SAGA,
+  //   failureGetAttendanceStdData
+  // );
+  yield takeEvery(GET_SELECT_ATTENDANCE_ARR_SAGA, getSelectAttendanceArr);
+  yield takeEvery(
+    SUCCESS_GET_SELECT_ATTENDANCE_ARR_SAGA,
+    successGetSelectAttendanceArr
+  );
+  // yield takeEvery(
+  //   FAILURE_GET_SELECT_ATTENDANCE_ARR_SAGA,
+  //   failureGetSelectAttendanceArr
+  // );
 }
 
 export default dAttendanceSaga;
