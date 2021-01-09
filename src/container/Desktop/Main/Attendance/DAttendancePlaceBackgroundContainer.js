974e3d7217168728dbc5e1db263f056bbcae23c3
CutyApple <dbtldhs0915@naver.com>
2021-01-09 19:25:21 +0900

    [FEAT] <D/DAttendancePlaceBackgroundContainer.js>

--- src/container/Desktop/Main/Attendance/DAttendancePlaceBackgroundContainer.js
+++ src/container/Desktop/Main/Attendance/DAttendancePlaceBackgroundContainer.js
@@ -2,6 +2,7 @@ import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AttendancePlaceBackground from "../../../../component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/AttendancePlaceBackground";
 import { getFloor } from "../../../../lib/attendanceApi";
+import { DAttendanceActionCreater } from "../../../../module/action/d_attendance";
 import { staticSelectArr } from "./StaticData";
 
 const DAttendancePlaceBackgroundContainer = () => {
@@ -15,21 +16,20 @@ const DAttendancePlaceBackgroundContainer = () => {
     staticSelectArr[selectSchedule === "교실자습" ? "selfStudy" : "club"];
   const selectPriority = selectAttendance[selectArrIndex[1]]?.priority;
 
-  const dispatch = useDispatch();
-
-  // const a = useCallback(() => {
-  //   dispatch();
-  // }, [dispatch])
-
-  console.log({
+  const payload = {
     schedule: selectSchedule === "교실자습" ? "self-study" : "club",
     floor: getFloor(selectSelfStudyOrClub.bodyItem[selectArrIndex[0]]),
     priority: selectPriority
-  });
+  };
 
-  return (
-    <AttendancePlaceBackground onClick={() => console.log(selectPriority)} />
-  );
+  const { getAttendanceStdDataSaga } = DAttendanceActionCreater;
+
+  const dispatch = useDispatch();
+  const getAttendanceStdData = useCallback(() => {
+    dispatch(getAttendanceStdDataSaga(payload));
+  }, [dispatch, payload]);
+
+  return <AttendancePlaceBackground onClick={getAttendanceStdData} />;
 };
 
 export default DAttendancePlaceBackgroundContainer;
