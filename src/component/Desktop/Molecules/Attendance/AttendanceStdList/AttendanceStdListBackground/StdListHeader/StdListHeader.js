ca46a63a6a76918e9f79472a8521b671f006868f
CutyApple <dbtldhs0915@naver.com>
2021-01-10 20:53:16 +0900

    [FEAT] <D/Molecules/StdListHeader> Apply Static Data

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/StdListHeader.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/StdListHeader.js
@@ -3,9 +3,18 @@ import StdListGrid from "../StdListGrid/StdListGrid";
 import * as S from "./styles";
 
 const StdListHeader = () => {
+  const name = "학생";
+  const attendanceList = ["7교시", "8교시", "9교시", "10교시"];
+  const memo = "비고";
+
   return (
     <S.Container>
-      <StdListGrid />
+      <StdListGrid
+        name={name}
+        attendanceList={attendanceList}
+        memo={memo}
+        css={S.StdListGridCSS}
+      />
     </S.Container>
   );
 };
