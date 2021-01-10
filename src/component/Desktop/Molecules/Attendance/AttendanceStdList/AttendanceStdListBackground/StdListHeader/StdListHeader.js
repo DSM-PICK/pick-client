9f4e392a5266457bc0cc56c9d348cc07e1170538
CutyApple <dbtldhs0915@naver.com>
2021-01-10 20:40:05 +0900

    [FEAT] <D/Molecules/StdListHeader> Apply StdListGrid

---
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/StdListHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StdListGrid from "../StdListGrid/StdListGrid";
+import * as S from "./styles";
+
+const StdListHeader = () => {
+  return (
+    <S.Container>
+      <StdListGrid />
+    </S.Container>
+  );
+};
+
+export default StdListHeader;
