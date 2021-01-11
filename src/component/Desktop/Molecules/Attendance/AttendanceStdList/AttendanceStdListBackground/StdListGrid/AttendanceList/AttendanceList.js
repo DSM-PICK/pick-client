b2d7d628c0b449a42222b643c5fc529e76d90a8d
CutyApple <dbtldhs0915@naver.com>
2021-01-11 14:25:21 +0900

    [FEAT] <D/Molecules/AttendanceList.js> Apply Props

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/AttendanceList.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/AttendanceList.js
@@ -1,10 +1,18 @@
 import React from "react";
+import ItemBtn from "./ItemBtn/ItemBtn";
 import * as S from "./styles";
 
 const AttendanceList = props => {
   const { attendanceList, css } = props;
 
-  return <S.Container {...css}></S.Container>;
+  return (
+    <S.Container {...css}>
+      {attendanceList.map(data => {
+        console.log(data);
+        return <ItemBtn key={data} text={data} />;
+      })}
+    </S.Container>
+  );
 };
 
 export default AttendanceList;
