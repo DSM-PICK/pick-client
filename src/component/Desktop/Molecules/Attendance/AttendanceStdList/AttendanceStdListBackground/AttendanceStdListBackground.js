b9e33e3edbcf2694e5509db843ca149a847eb4db
CutyApple <dbtldhs0915@naver.com>
2021-01-11 14:16:17 +0900

    [FEAT] <D/Molecules/AttendanceStdListBackground> Apply Component

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/AttendanceStdListBackground.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/AttendanceStdListBackground.js
@@ -1,8 +1,15 @@
 import React from "react";
+import StdListBody from "./StdListBody/StdListBody";
+import StdListHeader from "./StdListHeader/StdListHeader";
 import * as S from "./styles";
 
 const AttendanceStdListBackground = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <StdListHeader />
+      <StdListBody />
+    </S.Container>
+  );
 };
 
 export default AttendanceStdListBackground;
