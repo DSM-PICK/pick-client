d5c136de8085497f949c98681fdc7c2f84f5a959
CutyApple <dbtldhs0915@naver.com>
2021-01-11 21:33:29 +0900

    [FEAT] <D/DesktopAttendanceListContainer.js> Create File

---
+++ src/container/Desktop/Main/Attendance/DesktopAttendanceListContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AttendanceList from "../../../../component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/AttendanceList";
+
+const DesktopAttendanceListContainer = () => {
+  return <AttendanceList />;
+};
+
+export default DesktopAttendanceListContainer;
