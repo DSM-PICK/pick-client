4f51e92e23d6b02f63b8a6fd1b2d69d842582d06
CutyApple <dbtldhs0915@naver.com>
2021-01-11 18:21:29 +0900

    [FEAT] <D/Molecules/StateItem>

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/StateItem/StateItem.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/StateItem/StateItem.js
@@ -1,8 +1,14 @@
 import React from "react";
 import * as S from "./styles";
 
-const StateItem = () => {
-  return <S.Container></S.Container>;
+const StateItem = props => {
+  const { current, value } = props;
+
+  return (
+    <S.Container current={current} value={value}>
+      {value}
+    </S.Container>
+  );
 };
 
 export default StateItem;
--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/StateItem/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/StateItem/styles.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: ${props => (props.current === props.value ? 0 : 1)};
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  transition: all 0.1s ease-in-out;
+`;
