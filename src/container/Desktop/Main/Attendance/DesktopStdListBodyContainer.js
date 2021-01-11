9051332aed374158b92a7d5238a8bd2decb41a22
CutyApple <dbtldhs0915@naver.com>
2021-01-11 14:31:24 +0900

    [FEAT] <D/DesktopStdListBodyContainer.js> Craete File

---
+++ src/container/Desktop/Main/Attendance/DesktopStdListBodyContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StdListBody from "../../../../component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListBody/StdListBody";
+
+const DesktopStdListBodyContainer = () => {
+  return <StdListBody />;
+};
+
+export default DesktopStdListBodyContainer;
