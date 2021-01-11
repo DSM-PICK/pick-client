60a91fa48d52d9011c71b401f06cc55d5d85c15b
CutyApple <dbtldhs0915@naver.com>
2021-01-11 18:21:42 +0900

    [FEAT] <D/Molecules/ItemBtn>

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/ItemBtn.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/ItemBtn.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import * as S from "./styles";
+import StateItem from "./StateItem/StateItem";
 
 const ItemBtn = props => {
   const { text } = props;
 
-  return <S.Container state={text}>{text}</S.Container>;
+  const [isOpen, setIsOpen] = useState(false);
+  const staticStates = [
+    {
+      value: "출석"
+    },
+    {
+      value: "이동"
+    },
+    {
+      value: "외출"
+    },
+    {
+      value: "현체"
+    },
+    {
+      value: "귀가"
+    },
+    {
+      value: "무단"
+    }
+  ];
+
+  const onClick = useCallback(() => {
+    if (text !== "취업") {
+      setIsOpen(!isOpen);
+    }
+    console.log("cl");
+  }, [isOpen]);
+
+  return (
+    <S.Container text={text} isOpen={isOpen} onClick={onClick}>
+      {!isOpen && text !== "출석" && text}
+      <S.Wrap>
+        <StateItem current={text} value={text} />
+        {staticStates.map(
+          state =>
+            state.value !== text && (
+              <StateItem key={state.value} current={text} value={state.value} />
+            )
+        )}
+      </S.Wrap>
+    </S.Container>
+  );
 };
 
 export default ItemBtn;
--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/styles.js
@@ -1,9 +1,15 @@
 import styled, { css } from "styled-components";
 
 export const Container = styled.button`
+  position: relative;
+  padding: 0;
   color: ${props => props.color || "#000000"};
   font-size: ${props => props.fontSize || "16px"};
   font-weight: ${props => props.fontWeight || "400"};
+  box-sizing: border-box;
+  border-radius: 8px;
+  outline: none;
+
   ${props => {
     switch (props.text) {
       case "이동":
@@ -35,8 +41,6 @@ export const Container = styled.button`
         `;
     }
   }};
-  border-radius: 8px;
-  outline: none;
 
   &:hover {
     ${props =>
@@ -45,4 +49,32 @@ export const Container = styled.button`
         cursor: pointer;
       `}
   }
+
+  &:focus,
+  &:active {
+    & > div {
+      opacity: ${props => (props.isOpen ? 1 : 0)};
+      visibility: ${props => (props.isOpen ? "visible" : "hidden")};
+    }
+  }
+
+  & > div {
+    opacity: 0;
+    visibility: hidden;
+    transition: all 0.3s ease-in-out;
+  }
+
+  & > div > li {
+  }
+`;
+
+export const Wrap = styled.div`
+  position: absolute;
+  top: -1.25px;
+  left: -1.25px;
+  z-index: 3;
+  width: 100%;
+  border: 1px solid #909090;
+  border-radius: 8px;
+  background: #ffffff;
 `;
