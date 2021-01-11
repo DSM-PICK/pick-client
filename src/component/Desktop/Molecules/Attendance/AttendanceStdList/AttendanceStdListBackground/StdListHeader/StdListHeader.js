38d7ad32a6309c97873c371e5d2c2ac1da1331ae
CutyApple <dbtldhs0915@naver.com>
2021-01-11 16:43:41 +0900

    [FEAT] <D/Molecules/StdListHeader> Complete

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/StdListHeader.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/StdListHeader.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import StdListGrid from "../StdListGrid/StdListGrid";
 import * as S from "./styles";
 
 const StdListHeader = () => {
+  const attendanceLists = useSelector(
+    state => state.dAttendance.attendanceData
+  );
+
+  let length = 0;
+  if (attendanceLists.length) {
+    length = Object.values(attendanceLists[0].state).filter(data => data)
+      .length;
+  }
+
   const name = "학생";
-  const attendanceList = ["7교시", "8교시", "9교시", "10교시"];
+  const attendanceList = ["8교시", "9교시", "10교시"];
   const memo = "비고";
 
   return (
     <S.Container>
-      <StdListGrid
-        name={name}
-        attendanceList={attendanceList}
-        memo={memo}
-        css={S.StdListGridCSS}
-      />
+      <S.ListHeader>
+        <S.HeaderItem>{name}</S.HeaderItem>
+        <S.HeaderList length={length}>
+          {attendanceList.map(text => (
+            <S.HeaderItem key={text}>{text}</S.HeaderItem>
+          ))}
+        </S.HeaderList>
+        <S.HeaderItem>{memo}</S.HeaderItem>
+      </S.ListHeader>
     </S.Container>
   );
 };
--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListHeader/styles.js
@@ -5,6 +5,27 @@ export const Container = styled.div`
   height: 24px;
 `;
 
-export const StdListGridCSS = {
-  fontWeight: "400"
-};
+export const ListHeader = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+  width: 100%;
+  height: 24px;
+`;
+
+export const HeaderItem = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000000;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const HeaderList = styled.div`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: ${props =>
+    `repeat(${props.length}, minmax(70px, 1fr))`};
+  width: 100%;
+  height: 100%;
+`;
