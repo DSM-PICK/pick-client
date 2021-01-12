07afd051ca7771323ae164f718a6ec26ae26c7d3
CutyApple <dbtldhs0915@naver.com>
2021-01-12 20:07:09 +0900

    [FEAT] <D/Molecules/AttendanceCurrentBackgronud.js> Apply STATIC DATA

--- src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/AttendanceCurrentBackground.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/AttendanceCurrentBackground.js
@@ -1,8 +1,34 @@
 import React from "react";
+import CurrentWrap from "./CurrentWrap/CurrentWrap";
 import * as S from "./styles";
 
 const AttendanceCurrentBackground = () => {
-  return <S.Container></S.Container>;
+  const STATIC_DONE_ARR = [
+    {
+      title: "1학년",
+      data: ["1학년 1반", "1학년 2반", "1학년 3반", "1학년 4반"]
+    },
+    {
+      title: "2학년",
+      data: ["2학년 1반", "2학년 2반", "2학년 3반", "2학년 4반"]
+    },
+    {
+      title: "3학년",
+      data: ["3학년 1반", "3학년 2반", "3학년 3반", "3학년 4반"]
+    },
+    {
+      title: "기타",
+      data: ["창조실"]
+    }
+  ];
+
+  return (
+    <S.Container>
+      {STATIC_DONE_ARR.map(floorData => (
+        <CurrentWrap key={floorData.title} floorData={floorData} />
+      ))}
+    </S.Container>
+  );
 };
 
 export default AttendanceCurrentBackground;
