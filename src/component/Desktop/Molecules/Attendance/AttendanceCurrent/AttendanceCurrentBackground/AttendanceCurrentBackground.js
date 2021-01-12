b47acfc95c8ca83d232732f855b1618e8851d6b7
CutyApple <dbtldhs0915@naver.com>
2021-01-12 19:52:08 +0900

    [FEAT] <D/Molecules/AttendanceCurrentBackground> Init

---
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/AttendanceCurrentBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styles";
+
+const AttendanceCurrentBackground = () => {
+  return <S.Container></S.Container>;
+};
+
+export default AttendanceCurrentBackground;
