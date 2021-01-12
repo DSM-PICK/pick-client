a87ab061661d313297e354146be63bc2ef054103
CutyApple <dbtldhs0915@naver.com>
2021-01-12 19:57:31 +0900

    [FEAT] <D/Molecules/AttendanceCurrent.js> Apply Background Component

--- src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrent.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Background from "../../../Atoms/Background/Background";
 import Label from "../../../Atoms/Label/Label";
+import AttendanceCurrentBackground from "./AttendanceCurrentBackground/AttendanceCurrentBackground";
 import * as S from "./styles";
 
 const AttendanceCurrent = () => {
@@ -9,7 +10,9 @@ const AttendanceCurrent = () => {
       <S.LableHeader>
         <Label text={"출석 현황"} css={S.LabelCSS} />
       </S.LableHeader>
-      <Background css={S.BackgroundCSS}></Background>
+      <Background css={S.BackgroundCSS}>
+        <AttendanceCurrentBackground />
+      </Background>
     </S.Container>
   );
 };
