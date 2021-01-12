e7318caff945c91ca066627b2c4cfe872f423821
CutyApple <dbtldhs0915@naver.com>
2021-01-12 20:06:45 +0900

    [FEAT] <D/Molecules/CurrentWrap> Apply Header

--- src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/CurrentWrap/CurrentWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/CurrentWrap/CurrentWrap.js
@@ -1,8 +1,18 @@
 import React from "react";
 import * as S from "./styles";
 
-const CurrentWrap = () => {
-  return <S.Container></S.Container>;
+const CurrentWrap = props => {
+  const { floorData } = props;
+  const { title, data } = floorData;
+  console.log(floorData);
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.HeaderTitle>{title}</S.HeaderTitle>
+      </S.Header>
+    </S.Container>
+  );
 };
 
 export default CurrentWrap;
--- src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/CurrentWrap/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrent/AttendanceCurrentBackground/CurrentWrap/styles.js
@@ -5,3 +5,21 @@ export const Container = styled.div`
   height: 100%;
   border: 1px solid black;
 `;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 30px;
+`;
+
+export const HeaderTitle = styled.p`
+  color: #000000;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const Body = styled.div`
+  display: grid;
+`;
