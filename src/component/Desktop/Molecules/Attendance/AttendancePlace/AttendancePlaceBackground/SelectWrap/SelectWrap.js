508f5a8979d12c8ebb855c9e661304e87b01cdd5
CutyApple <dbtldhs0915@naver.com>
2021-01-20 21:22:02 +0900

    [FEAT] <D/Molecules/SelectWrap.js>

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
@@ -5,19 +5,22 @@ const SelectWrap = props => {
   const { selectArr, currentArr } = props;
   const { onClick } = props;
 
+  console.log(currentArr)
+
   return (
     <S.Container>
       {selectArr.map((data, rowIndex) => (
         <S.Select key={data.header}>
           <S.SelectHeader>{data.header}</S.SelectHeader>
           <S.SelectBody>
+            {data.bodyItem.length === 0 && <S.SelectItem>데이터가 없습니다.</S.SelectItem>}
             {data.bodyItem.map((item, colIndex) => (
               <S.SelectItem
                 key={item}
                 select={currentArr[rowIndex] === colIndex}
                 onClick={() => onClick(rowIndex, colIndex)}
               >
-                {item}
+                {item || "데이터가 없습니다."}
               </S.SelectItem>
             ))}
           </S.SelectBody>
