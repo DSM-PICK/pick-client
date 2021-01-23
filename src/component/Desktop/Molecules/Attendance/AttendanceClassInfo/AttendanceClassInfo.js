b5e588c8700dd316e5094a17919a78af9357f337
CutyApple <dbtldhs0915@naver.com>
2021-01-23 21:01:38 +0900

    [FEAT] <D/Molecules/AttendanceClassInfo> Complete

--- src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo.js
@@ -3,10 +3,23 @@ import Background from '../../../Atoms/Background/Background';
 import * as S from "./styles";
 
 const AttendanceClassInfo = () => {
+
+    const className = "2학년 2반";
+    const teacherName = "신요셉";
+    const allStdCnt = 20;
+    const attendanceStdCnt = 18;
+
     return (
-        <Background css={S.BackgroundCSS}>
-            
-        </Background>
+        <S.Container>
+            <Background css={S.BackgroundCSS}>
+                <S.ClassInfoContainer>
+                    <S.Header>{className}</S.Header>
+                    <S.Text>{teacherName ? `담임교사 : ${teacherName}` : `선생님이 없습니다.`}</S.Text>
+                    <S.Text>{allStdCnt ? `총 학생 : ${allStdCnt}명` : `학생 데이터가 없습니다.`}</S.Text>
+                    <S.Text>{attendanceStdCnt ? `출석 예정 : ${attendanceStdCnt}명` : `학생 데이터가 없습니다.`}</S.Text>
+                </S.ClassInfoContainer>
+            </Background>
+        </S.Container>
     );
 };
 
--- src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/styles.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/styles.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
+    display: flex;
+
+    @media (max-width: 1480px) {
+        display: none;
+    }
 `;
 
 export const BackgroundCSS = {
@@ -12,4 +15,28 @@ export const BackgroundCSS = {
     background: "#ffffff",
     boxShadow: "0px 1px 1px 0px rgba(144, 144, 144, 0.2)"
 };
+
+    export const ClassInfoContainer = styled.div`
+        display: grid;
+        align-items: center;
+        justify-content: center;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
+        width: calc(100% - 28px);
+        height: calc(100% - 28px);
+        padding: 14px;
+    `;
   
+export const Header = styled.p`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    color: #000000;
+    font-size: 24px;
+    font-weight: 600;
+`;
+
+export const Text = styled.p`
+    color: #000000;
+    font-size: 16px;
+    font-weight: 400;    
+`;
