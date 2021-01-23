d80e30da4ac84b3322d7f40cb1a308274a1f955c
CutyApple <dbtldhs0915@naver.com>
2021-01-23 20:13:05 +0900

    [FEAT] <D/Molecules/AttendanceClassInfo> Create File

---
+++ src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from "./styles";
+
+const AttendanceClassInfo = () => {
+    return (
+        <S.Container>
+            
+        </S.Container>
+    );
+};
+
+export default AttendanceClassInfo;
