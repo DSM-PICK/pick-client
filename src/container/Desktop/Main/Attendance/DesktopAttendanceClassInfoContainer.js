0dda3b72f58db6a6b650206f58f3c8d9164ba508
CutyApple <dbtldhs0915@naver.com>
2021-01-23 21:18:04 +0900

    [FEAT] <D/DesktopAttendanceClassInfoContainer.js>

---
+++ src/container/Desktop/Main/Attendance/DesktopAttendanceClassInfoContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AttendanceClassInfo from "../../../../component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo";
+
+const DesktopAttendanceClassInfoContainer = () => {
+  return <AttendanceClassInfo />;
+};
+
+export default DesktopAttendanceClassInfoContainer;
