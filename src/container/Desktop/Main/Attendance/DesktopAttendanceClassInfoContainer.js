f6268d4ddefeef2b92918564c9565ea343b6b53e
CutyApple <dbtldhs0915@naver.com>
2021-01-26 01:49:42 +0900

    [FEAT] <D/DesktopAttendanceClassInfoContainer.js>

--- src/container/Desktop/Main/Attendance/DesktopAttendanceClassInfoContainer.js
+++ src/container/Desktop/Main/Attendance/DesktopAttendanceClassInfoContainer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import AttendanceClassInfo from "../../../../component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo";
 
 const DesktopAttendanceClassInfoContainer = () => {
+  const attendanceData = useSelector(state => state.dAttendance.attendanceData);
+
   const className = "2학년 2반";
   const teacherName = "신요셉";
-  const allStdCnt = 20;
-  const attendanceStdCnt = 18;
+  const allStdCnt = attendanceData.length;
+  const attendanceStdCnt =
+    attendanceData.length > 0 &&
+    attendanceData.filter(data => data.state.eight !== "취업").length;
+
+  console.log({ attendanceData });
 
   return (
     <AttendanceClassInfo
