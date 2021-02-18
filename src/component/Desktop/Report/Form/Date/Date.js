3f23f96c9e09ee04428b567f7173cafd20ba774e
ohjunsang <pandati@dsm.hs.kr>
2021-02-18 21:46:34 +0900

    [FIX] 시작 날짜와 끝날짜로 변경 후, 교시까지 추가

--- src/component/Desktop/Report/Form/Date/Date.js
+++ src/component/Desktop/Report/Form/Date/Date.js
@@ -1,17 +1,55 @@
 import React from "react";
+import { useDesktopReportState } from "../../../../../lib/hooks/desktop/report";
 import * as S from "../../styles";
 
-const Date = ({ date, setDate }) => {
-  const renderDate = date => {
-    if (!date.year || !date.month || !date.date) {
+const Date = () => {
+  const { state, setState } = useDesktopReportState();
+  const { startDate, endDate, endPeriod, startPeriod, selectedStudent } = state;
+  const { setEndPeriod, setStartPeriod } = setState;
+  const renderDate = (startDate, endDate, startPeriod, endPeriod) => {
+    if (
+      !startDate.year ||
+      !startDate.month ||
+      !startDate.date ||
+      !endDate.year ||
+      !endDate.month ||
+      !endDate.date
+    ) {
       return "좌측 캘린더에서 날짜를 클릭하세요.";
     }
-    return `${date.month}월 ${date.date}일 5교시 ~ 6교시`;
+    const startPeriodInputChangeHandler = () => event => {
+      setStartPeriod({ id: selectedStudent, period: event.target.value });
+    };
+    const endPeriodInputChangeHandler = () => event => {
+      setEndPeriod({ id: selectedStudent, period: event.target.value });
+    };
+    return (
+      <S.FomrDatePeriodInputWrapper>
+        {getTextDate(startDate)}{" "}
+        <S.FormDatePeriodInput
+          value={startPeriod}
+          onChange={startPeriodInputChangeHandler()}
+          type="number"
+        />
+        교시 ~ {getTextDate(endDate)}
+        <S.FormDatePeriodInput
+          value={endPeriod}
+          onChange={endPeriodInputChangeHandler()}
+          type="number"
+        />
+        교시
+      </S.FomrDatePeriodInputWrapper>
+    );
+  };
+  const getTextDate = date => {
+    return `${date.month}월 ${date.date}일`;
   };
   return (
     <S.FormDate>
       <S.FormText>결석일</S.FormText>
-      <S.FormDateInput>{renderDate(date)}</S.FormDateInput>
+      <S.FormDateInput>
+        {renderDate(startDate, endDate, startPeriod, endPeriod)}
+      </S.FormDateInput>
     </S.FormDate>
   );
 };
