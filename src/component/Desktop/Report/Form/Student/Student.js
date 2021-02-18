82ed76b5ebd757e19a1a6fb64b8a8f1b8874c455
ohjunsang <pandati@dsm.hs.kr>
2021-02-18 21:48:42 +0900

    [ADD] 선택중인 학생을 고를 수 있게 하고, 자동완성이 켜져있는지 아닌지 state 추가

--- src/component/Desktop/Report/Form/Student/Student.js
+++ src/component/Desktop/Report/Form/Student/Student.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "../../styles";
 import StudentItem from "./StudentItem";
 import StudentAddButton from "./StudentAddButton";
+import StudentAddInput from "./StudentAddInput";
+import { useDesktopReportState } from "../../../../../lib/hooks/desktop/report";
 
-const Student = ({
-  setSelectedStudent,
-  selectedStudent,
-  setAttendanceChangeStudent,
-  deleteAttendanceChangeStudent,
-  attendanceChangeStudentList
-}) => {
+const Student = () => {
+  const { state, setState } = useDesktopReportState();
+  const {
+    selectedStudent,
+    attendanceChangeStudentList,
+    similerStudents
+  } = state;
+  const {
+    deleteAttendanceChangeStudent,
+    setSelectedStudent,
+    setNewAttendanceChangeStudent,
+    getSimilerStudent
+  } = setState;
+  const [isStudentAdding, setIsStudentAdding] = useState(false);
+  const addButtonClickHandler = () => {
+    setIsStudentAdding(true);
+  };
+  const inputChangeHandler = value => {
+    getSimilerStudent(value);
+  };
+  const studentClickHandler = value => {
+    const [number, name] = value.split(" ");
+    setNewAttendanceChangeStudent({
+      number,
+      name
+    });
+    setIsStudentAdding(false);
+    getSimilerStudent("");
+  };
   const renderStudentItem = studentList =>
     studentList.map(student => (
       <StudentItem
@@ -19,6 +43,7 @@ const Student = ({
         setSelectedStudent={setSelectedStudent}
         selectedStudent={selectedStudent}
         deleteAttendanceChangeStudent={deleteAttendanceChangeStudent}
+        key={`${student.name}-${student.id}-${student.number}`}
       />
     ));
   return (
@@ -26,7 +51,15 @@ const Student = ({
       <S.FormText>결석자</S.FormText>
       <S.FormStudentWrapper>
         {renderStudentItem(attendanceChangeStudentList)}
-        <StudentAddButton />
+        {isStudentAdding ? (
+          <StudentAddInput
+            inputChangeHandler={inputChangeHandler}
+            similerStudents={similerStudents}
+            studentClickHandler={studentClickHandler}
+          />
+        ) : (
+          <StudentAddButton buttonClickHandler={addButtonClickHandler} />
+        )}
       </S.FormStudentWrapper>
     </S.FormStudent>
   );
