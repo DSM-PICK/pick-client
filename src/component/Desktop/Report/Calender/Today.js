acf584a97d82e661997d1469d3471bdaf56b7575
ohjunsang <pandati@dsm.hs.kr>
2021-02-18 21:45:37 +0900

    [ADD] 고정된 더미데이터 대신 오늘 연,월을 나오게 수정

--- src/component/Desktop/Report/Calender/Today.js
+++ src/component/Desktop/Report/Calender/Today.js
@@ -2,9 +2,16 @@ import React from "react";
 import * as S from "../styles";
 
 const Today = () => {
+  const todayDateObj = new Date();
+  const today = {
+    year: todayDateObj.getFullYear(),
+    month: todayDateObj.getMonth() + 1
+  };
   return (
     <S.CalenderToday>
-      <p>2021년 1월</p>
+      <p>
+        {today.year}년 {today.month}월
+      </p>
       <S.CalenderButton>오늘</S.CalenderButton>
     </S.CalenderToday>
   );
