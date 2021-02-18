5879c3a84680bc14606eb32e0e28d6fdf6410c9e
ohjunsang <pandati@dsm.hs.kr>
2021-02-18 21:52:04 +0900

    [FIX] props로 내려받던 state를 내부에서 hooks 사용해서 이용

--- src/component/Desktop/Report/Form/Type/Type.js
+++ src/component/Desktop/Report/Form/Type/Type.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useDesktopReportState } from "../../../../../lib/hooks/desktop/report";
 import * as S from "../../styles";
 import FormTypeButtons from "./FormTypeButtons";
 
-const Type = ({ setType, type }) => {
+const Type = () => {
+  const { state, setState } = useDesktopReportState();
+  const { attendanceChangeType } = state;
+  const { setAttendanceChangeType } = setState;
   return (
     <S.FormType>
       <S.FormText>종류</S.FormText>
-      <FormTypeButtons type={type} setType={setType} />
+      <FormTypeButtons
+        type={attendanceChangeType}
+        setType={setAttendanceChangeType}
+      />
     </S.FormType>
   );
 };
