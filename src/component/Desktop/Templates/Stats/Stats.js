d737c44ad2ab622b15f1b0fd3f96bc78ae429f18
CutyApple <dbtldhs0915@naver.com>
2021-03-03 23:04:55 +0900

    [FEAT] <Desktop/Templates/Stats> Apply DesktopHeaderContainer

--- src/component/Desktop/Templates/Stats/Stats.js
+++ src/component/Desktop/Templates/Stats/Stats.js
@@ -1,8 +1,13 @@
 import React from "react";
+import DesktopHeaderContainer from "../../../../container/Desktop/Header/DesktopHeaderContainer";
 import * as S from "./style";
 
 const Stats = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <DesktopHeaderContainer />
+    </S.Container>
+  );
 };
 
 export default Stats;
