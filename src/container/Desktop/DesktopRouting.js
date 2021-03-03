85c623618150c6dc56676efe7d08d8f86922365f
CutyApple <dbtldhs0915@naver.com>
2021-03-03 22:34:24 +0900

    [FEAT] <container/Desktop/DesktopRouting.js> Set Routing

--- src/container/Desktop/DesktopRouting.js
+++ src/container/Desktop/DesktopRouting.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFoundContainer from "../NotFound/NotFoundContainer";
 import DesktopMainContainer from "./Main/DesktopMainContainer";
+import DesktopStatsContainer from "./Stats/DesktopStatsContainer";
 
 const DesktopRouting = () => {
   return (
@@ -10,7 +11,7 @@ const DesktopRouting = () => {
       {/* <Route path="/login" exact component={} /> */}
       {/* <Route path="/pre-report" exact component={} /> */}
       {/* <Route path="/schedule" exact component={} /> */}
-      {/* <Route path="/stats" exact component={} /> */}
+      <Route path="/stats" exact component={DesktopStatsContainer} />
 
       <Route component={NotFoundContainer} />
     </Switch>
