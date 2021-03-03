d8a0a2b55468a7077d3d57cd6c6e3d1854648f47
CutyApple <dbtldhs0915@naver.com>
2021-03-03 22:43:04 +0900

    [FEAT] <container/~/DesktopStatsContainer.js> Stats

--- src/container/Desktop/Stats/DesktopStatsContainer.js
+++ src/container/Desktop/Stats/DesktopStatsContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Stats from "../../../component/Desktop/Templates/Stats/Stats";
 
 const DesktopStatsContainer = () => {
-  return <div></div>;
+  return <Stats />;
 };
 
 export default DesktopStatsContainer;
