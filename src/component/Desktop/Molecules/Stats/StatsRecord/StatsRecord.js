98fd17579adcf19a445af46bed0d9c92ad583c96
CutyApple <dbtldhs0915@naver.com>
2021-03-04 14:44:54 +0900

    [FEAT] <Desktop/Molecules/~/StatsRecord> Init

--- src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecord.js
+++ src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecord.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Background from "../../../Atoms/Background/Background";
+import Label from "../../../Atoms/Label/Label";
 import * as S from "./style";
 
 const StatsRecord = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.LableHeader>
+        <Label text={"출결 기록"} css={S.LabelCSS} />
+      </S.LableHeader>
+      <Background css={S.BackgroundCSS}></Background>
+    </S.Container>
+  );
 };
 
 export default StatsRecord;
