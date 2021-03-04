3aaed8932897fb2c5debc6a5fbeaa7f03dd69607
CutyApple <dbtldhs0915@naver.com>
2021-03-04 14:31:08 +0900

    [FEAT] <Desktop/Organisms/Stats> Apply Child Components

--- src/component/Desktop/Organisms/Stats/style.js
+++ src/component/Desktop/Organisms/Stats/style.js
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+  display: grid;
+  column-gap: 24px;
+  grid-template-columns: 6fr 11fr;
+  padding: 30px 0;
+`;
+
+export const LeftGridWrap = styled.div`
+  display: grid;
+  row-gap: 34px;
+  grid-template-rows: 10fr 11fr;
+  width: 100%;
+  height: 100%;
+`;
+
+export const LeftAdditionalGridWrap = styled.div`
+  display: flex;
+`;
+
+export const RightWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
