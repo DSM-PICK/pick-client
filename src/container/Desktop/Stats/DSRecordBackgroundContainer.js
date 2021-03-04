062fd8d82ab6951ed6ed476644e8e10acbbe6d66
CutyApple <dbtldhs0915@naver.com>
2021-03-04 19:59:26 +0900

    [FEAT] <container/~/DSRecordBackgroundContainer.js> apply recordData

--- src/container/Desktop/Stats/DSRecordBackgroundContainer.js
+++ src/container/Desktop/Stats/DSRecordBackgroundContainer.js
@@ -2,7 +2,19 @@ import React from "react";
 import StatsRecordBackground from "../../../component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground";
 
 const DSRecordBackgroundContainer = () => {
-  return <StatsRecordBackground />;
+  const recordData = [
+    {
+      title: "1학년"
+    },
+    {
+      title: "2학년"
+    },
+    {
+      title: "3학년"
+    }
+  ];
+
+  return <StatsRecordBackground recordData={recordData} />;
 };
 
 export default DSRecordBackgroundContainer;
