246769c7f69e61f34e9e78caf25be8f35653f9ce
CutyApple <dbtldhs0915@naver.com>
2021-03-04 20:12:38 +0900

    [FEAT] <container/~/DSRecordBackgroundContainer.js> Create File

---
+++ src/container/Desktop/Stats/DSRecordBackgroundContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import StatsRecordBackground from "../../../component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground";
+
+const DSRecordBackgroundContainer = () => {
+  return <StatsRecordBackground />;
+};
+
+export default DSRecordBackgroundContainer;
