76a455981a9c19c6da8383ceb8fc0e170ed73e5a
CutyApple <dbtldhs0915@naver.com>
2021-03-04 14:44:48 +0900

    [FEAT] <Desktop/Molecules/~/StatsPlace> Init

--- src/component/Desktop/Molecules/Stats/StatsPlace/StatsPlace.js
+++ src/component/Desktop/Molecules/Stats/StatsPlace/StatsPlace.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Background from "../../../Atoms/Background/Background";
+import Label from "../../../Atoms/Label/Label";
 import * as S from "./style";
 
 const StatsPlace = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.LableHeader>
+        <Label text={"교실 선택"} css={S.LabelCSS} />
+      </S.LableHeader>
+      <Background css={S.BackgroundCSS}></Background>
+    </S.Container>
+  );
 };
 
 export default StatsPlace;
