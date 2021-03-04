b9757ee6bb66e4e5f97905e142e9d5ebede5d05f
CutyApple <dbtldhs0915@naver.com>
2021-03-04 19:59:46 +0900

    [FEAT] <Molecules/~/StatsRecordBackground>

--- src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
+++ src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
@@ -1,18 +1,27 @@
 import React from "react";
 import * as S from "./style";
 
-const StatsRecordBackground = () => {
+const StatsRecordBackground = props => {
+  const { recordData } = props;
+
   return (
     <S.Container>
-      <S.GradeWrapper>
-        <S.GradeTitle>1학년</S.GradeTitle>
-      </S.GradeWrapper>
-      <S.GradeWrapper>
-        <S.GradeTitle>2학년</S.GradeTitle>
-      </S.GradeWrapper>
-      <S.GradeWrapper>
-        <S.GradeTitle>3학년</S.GradeTitle>
-      </S.GradeWrapper>
+      {recordData.map(data => (
+        <S.GradeWrapper key={data.title}>
+          <S.GradeTitle>{data.title}</S.GradeTitle>
+          <S.RecordLine isGray={true}>
+            <S.RecordText>{`이동 : 번`}</S.RecordText>
+            <S.RecordText>{`외출 : 번`}</S.RecordText>
+          </S.RecordLine>
+          <S.RecordLine>
+            <S.RecordText>{`현체 : 번`}</S.RecordText>
+            <S.RecordText>{`귀가 : 번`}</S.RecordText>
+          </S.RecordLine>
+          <S.RecordLine isGray={true}>
+            <S.RecordText isHighlight={true}>{`무단 : 번`}</S.RecordText>
+          </S.RecordLine>
+        </S.GradeWrapper>
+      ))}
     </S.Container>
   );
 };
--- src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/style.js
+++ src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/style.js
@@ -2,20 +2,44 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
+  gap: 10px;
   grid-template-rows: 1fr 1fr 1fr;
-  width: calc(100% - 48px);
-  height: calc(100% - 30px);
-  padding: 15px 24px;
+  width: 100%;
+  height: 100%;
 `;
 
 export const GradeWrapper = styled.div`
-  display: grid;
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
 `;
 
 export const GradeTitle = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
   color: #000000;
   font-size: 16px;
   font-weight: bold;
+`;
+
+export const RecordLine = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: ${props => (props.isGray ? "#F8F8F8" : "#ffffff")};
+`;
+
+export const RecordText = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
+  color: ${props => (props.isHighlight ? "#FF406E" : "#000000")};
+  font-size: 16px;
 `;
