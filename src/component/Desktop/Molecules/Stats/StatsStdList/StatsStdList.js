1085abc5f31285c06376491e8be501b97e0a65af
CutyApple <dbtldhs0915@naver.com>
2021-03-04 14:44:59 +0900

    [FEAT] <Desktop/Molecules/~/StatsStdList> Init

--- src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdList.js
+++ src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdList.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Background from "../../../Atoms/Background/Background";
+import Label from "../../../Atoms/Label/Label";
 import * as S from "./style";
 
 const StatsStdList = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.LableHeader>
+        <Label text={"날짜 선택"} css={S.LabelCSS} />
+      </S.LableHeader>
+      <Background css={S.BackgroundCSS}></Background>
+    </S.Container>
+  );
 };
 
 export default StatsStdList;
