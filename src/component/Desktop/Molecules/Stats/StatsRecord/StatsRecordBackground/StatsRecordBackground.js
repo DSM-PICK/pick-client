1b1154feda70ce0712cb98bdc412a5ecb6d02183
CutyApple <dbtldhs0915@naver.com>
2021-03-05 11:09:32 +0900

    [FEAT] <Molecules/~/StatsRecordBackground.js> Apply "recordDataArr"

--- src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
+++ src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
@@ -2,23 +2,25 @@ import React from "react";
 import * as S from "./style";
 
 const StatsRecordBackground = props => {
-  const { recordData } = props;
+  const { recordDataArr } = props;
 
   return (
     <S.Container>
-      {recordData.map(data => (
+      {recordDataArr.map(data => (
         <S.GradeWrapper key={data.title}>
           <S.GradeTitle>{data.title}</S.GradeTitle>
           <S.RecordLine isGray={true}>
-            <S.RecordText>{`이동 : 번`}</S.RecordText>
-            <S.RecordText>{`외출 : 번`}</S.RecordText>
+            <S.RecordText>{`이동 : ${data.move}번`}</S.RecordText>
+            <S.RecordText>{`외출 : ${data.outing}번`}</S.RecordText>
           </S.RecordLine>
           <S.RecordLine>
-            <S.RecordText>{`현체 : 번`}</S.RecordText>
-            <S.RecordText>{`귀가 : 번`}</S.RecordText>
+            <S.RecordText>{`현체 : ${data.fieldExperience}번`}</S.RecordText>
+            <S.RecordText>{`귀가 : ${data.homeComing}번`}</S.RecordText>
           </S.RecordLine>
           <S.RecordLine isGray={true}>
-            <S.RecordText isHighlight={true}>{`무단 : 번`}</S.RecordText>
+            <S.RecordText
+              isHighlight={true}
+            >{`무단 : ${data.truancy}번`}</S.RecordText>
           </S.RecordLine>
         </S.GradeWrapper>
       ))}
