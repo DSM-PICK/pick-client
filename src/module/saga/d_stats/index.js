d80837d148850ba4338afdc4758d9096211322fc
CutyApple <dbtldhs0915@naver.com>
2021-03-05 16:54:19 +0900

    [FEAT] <saga/d_stats> complete

--- src/module/saga/d_stats/index.js
+++ src/module/saga/d_stats/index.js
@@ -1,16 +1,18 @@
-import { call, takeEvery } from "@redux-saga/core/effects";
+import { put, call, takeEvery } from "@redux-saga/core/effects";
 import { requestGetApiWithAccessToken } from "../../../lib/requestApi";
 import { ATTENDANCE } from "../../../lib/requestUrl";
-import { DStatsAction } from "../../action/d_stats";
+import { DStatsAction, DStatsActionCreater } from "../../action/d_stats";
 
 function* getStats(action) {
-  const { grade } = action.payload;
+  const grade = action.payload;
 
   const REQUEST_URL = ATTENDANCE.RECORD_BY_GRADE_URL(grade);
 
   const res = yield call(requestGetApiWithAccessToken, REQUEST_URL);
 
-  console.log(res);
+  const { setStats } = DStatsActionCreater;
+
+  yield put(setStats({ grade, data: res.data }));
 }
 
 function* dStatsSaga() {
