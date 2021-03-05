fa8f9639ee322bd84bc0d5e36d33225bf8d74532
CutyApple <dbtldhs0915@naver.com>
2021-03-05 19:03:40 +0900

    [FEAT] <Molecules/~/StatsRecordBackground.js> Set default value by or

--- src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
+++ src/component/Desktop/Molecules/Stats/StatsRecord/StatsRecordBackground/StatsRecordBackground.js
@@ -10,17 +10,19 @@ const StatsRecordBackground = props => {
         <S.GradeWrapper key={data.title}>
           <S.GradeTitle>{data.title}</S.GradeTitle>
           <S.RecordLine isGray={true}>
-            <S.RecordText>{`이동 : ${data.move}번`}</S.RecordText>
-            <S.RecordText>{`외출 : ${data.outing}번`}</S.RecordText>
+            <S.RecordText>{`이동 : ${data.move || 0}번`}</S.RecordText>
+            <S.RecordText>{`외출 : ${data.outing || 0}번`}</S.RecordText>
           </S.RecordLine>
           <S.RecordLine>
-            <S.RecordText>{`현체 : ${data.fieldExperience}번`}</S.RecordText>
-            <S.RecordText>{`귀가 : ${data.homeComing}번`}</S.RecordText>
+            <S.RecordText>{`현체 : ${
+              data.fieldExperience || 0
+            }번`}</S.RecordText>
+            <S.RecordText>{`귀가 : ${data.homeComing || 0}번`}</S.RecordText>
           </S.RecordLine>
           <S.RecordLine isGray={true}>
-            <S.RecordText
-              isHighlight={true}
-            >{`무단 : ${data.truancy}번`}</S.RecordText>
+            <S.RecordText isHighlight={true}>{`무단 : ${
+              data.truancy || 0
+            }번`}</S.RecordText>
           </S.RecordLine>
         </S.GradeWrapper>
       ))}
