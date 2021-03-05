93052691028bf2971db766924e7c473ea0acf4b4
CutyApple <dbtldhs0915@naver.com>
2021-03-05 18:43:40 +0900

    [FEAT] <container/~/DSPlaceBackgroundContainer.js> onClassItemClick

--- src/container/Desktop/Stats/DSPlaceBackgrondContainer.js
+++ src/container/Desktop/Stats/DSPlaceBackgrondContainer.js
@@ -8,6 +8,16 @@ const DSPlaceBackgrondContainer = () => {
     { text: "2층" },
     { text: "기타" }
   ]);
+  const [classItemData, setclassItemData] = useState([
+    { text: "교실 2-1", isClicked: true },
+    { text: "세미나실 2-1" },
+    { text: "소프트웨어 개발 1실" },
+    { text: "소프트웨어 개발 2실" },
+    { text: "소프트웨어 개발 3실" },
+    { text: "교실 2-2" },
+    { text: "교실 2-3" },
+    { text: "교실 2-4" }
+  ]);
 
   const onTextButtonClick = useCallback(
     dataIndex => {
@@ -21,11 +31,25 @@ const DSPlaceBackgrondContainer = () => {
     },
     [textButtonData]
   );
+  const onClassItemClick = useCallback(
+    dataIndex => {
+      setclassItemData(
+        classItemData.map((data, mapIndex) =>
+          mapIndex !== dataIndex
+            ? { ...data, isClicked: false }
+            : { ...data, isClicked: true }
+        )
+      );
+    },
+    [classItemData]
+  );
 
   return (
     <StatsPlaceBackground
       textButtonData={textButtonData}
+      classItemData={classItemData}
       onTextButtonClick={onTextButtonClick}
+      onClassItemClick={onClassItemClick}
     />
   );
 };
