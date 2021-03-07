ed8725e8fef71606beffd71fedac14efe16f989d
CutyApple <dbtldhs0915@naver.com>
2021-03-07 21:05:37 +0900

    [FEAT] <container/Desktop/DesktopRouting.js> Routing Additional Setting

--- src/container/Desktop/DesktopRouting.js
+++ src/container/Desktop/DesktopRouting.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NotFoundContainer from "../NotFound/NotFoundContainer";
-import DesktopAccountContainer from "./DesktopAccountContainer";
 import DesktopReportContainer from "./DesktopReportContainer";
 import { DScheduleContainer } from "../../container";
 import { DAuthPage } from "../../page";
@@ -12,11 +11,9 @@ const DesktopRouting = () => {
   return (
     <Switch>
       <Route path="/" exact component={DesktopMainContainer} />
-      {/* <Route path="/login" exact component={} /> */}
-      {/* <Route path="/pre-report" exact component={} /> */}
-      <Route path="/schedule" exact component={DScheduleContainer} />
-      <Route path="/report" component={DesktopReportContainer} />
       <Route path="/auth" exact component={DAuthPage} />
+      <Route path="/report" component={DesktopReportContainer} />
+      <Route path="/schedule" exact component={DScheduleContainer} />
       <Route path="/stats" exact component={DesktopStatsContainer} />
 
       <Route component={NotFoundContainer} />
