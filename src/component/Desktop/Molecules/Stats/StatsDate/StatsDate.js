de3901c34e29912a511d2e2c2d47abd29bf9bdb9
CutyApple <dbtldhs0915@naver.com>
2021-03-08 22:50:52 +0900

    [FEAT] <Molecules/~/StatsDate.js> Apply StatsDateBackground

--- src/component/Desktop/Molecules/Stats/StatsDate/StatsDate.js
+++ src/component/Desktop/Molecules/Stats/StatsDate/StatsDate.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Background from "../../../Atoms/Background/Background";
 import Label from "../../../Atoms/Label/Label";
+import StatsDateBackground from "./StatsDateBackground/StatsDateBackground";
 import * as S from "./style";
 
 const StatsDate = () => {
@@ -9,7 +10,9 @@ const StatsDate = () => {
       <S.LableHeader>
         <Label text={"날짜 선택"} css={S.LabelCSS} />
       </S.LableHeader>
-      <Background css={S.BackgroundCSS}></Background>
+      <Background css={S.BackgroundCSS}>
+        <StatsDateBackground />
+      </Background>
     </S.Container>
   );
 };
