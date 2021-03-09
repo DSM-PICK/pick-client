7ae6e0071359c0c044acfaac337508e7773ba450
CutyApple <dbtldhs0915@naver.com>
2021-03-10 07:43:14 +0900

    [FEAT] <Molecules/~/StatsList.js> Apply "memo" For Rendering

--- src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdListBackground/SStdListGrid/StatsList/StatsList.js
+++ src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdListBackground/SStdListGrid/StatsList/StatsList.js
@@ -2,14 +2,14 @@ import React from "react";
 import * as S from "./styles";
 
 const StatsList = props => {
-  const { css, length, stateList } = props;
+  const { css, length, stateList, memoList } = props;
 
   return (
     <S.Container {...css} length={length}>
       {stateList.map((state, idx) => {
         return (
           <S.StateText key={idx} state={state}>
-            {state}
+            {!!memoList[idx] ? memoList[idx] : state}
           </S.StateText>
         );
       })}
