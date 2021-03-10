865f0503c8bec998cb380aa54eae1f757c8c15b8
CutyApple <dbtldhs0915@naver.com>
2021-03-10 15:22:11 +0900

    [FEAT] <Molecules/~/AttendanceClassInfo.js> Apply "managerTeacher"

--- src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceClassInfo/AttendanceClassInfo.js
@@ -3,14 +3,24 @@ import Background from "../../../Atoms/Background/Background";
 import * as S from "./styles";
 
 const AttendanceClassInfo = props => {
-  const { className, head, stdCount, stdCountWithoutEmployment } = props;
+  const {
+    managerTeacher,
+    className,
+    head,
+    stdCount,
+    stdCountWithoutEmployment
+  } = props;
 
   return (
     <S.Container>
       <Background css={S.BackgroundCSS}>
         <S.ClassInfoContainer>
           <S.Header>{className}</S.Header>
-          <S.Text>{head ? `담임교사 : ${head}` : `선생님이 없습니다.`}</S.Text>
+          <S.Text>
+            {managerTeacher
+              ? `담임교사 : ${managerTeacher}`
+              : `선생님이 없습니다.`}
+          </S.Text>
           <S.Text>
             {stdCount ? `총 학생 : ${stdCount}명` : `학생 데이터가 없습니다.`}
           </S.Text>
