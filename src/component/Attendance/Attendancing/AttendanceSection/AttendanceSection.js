4091b5ddfb6ef03a95e9ed658fc6ab9f0711a910
유시온 <dbtldhs0915@naver.com>
2021-03-16 17:02:57 +0900

    [FEAT] <Attendance/~/AttendanceSection.js> Add CheckBtn

--- src/component/Attendance/Attendancing/AttendanceSection/AttendanceSection.js
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import * as S from "./styles";
 import AttendanceRowTop from "./AttendanceRowTop/AttendanceRowTop";
 import AttendanceRow from "./AttendanceRow/AttendanceRow";
@@ -11,6 +11,8 @@ const AttendanceSection = props => {
 
     const information = useSelector(state => state.attendance);
     const { clubHead, attendanceData } = information;
+    const [checkArr, setCheckArr] = useState([]);
+    const [checkAllState, setCheckAllState] = useState(false);
 
     const isSevenNull =
       attendanceData.length && attendanceData[0].state.seven === null;
@@ -18,12 +20,40 @@ const AttendanceSection = props => {
       attendanceData.length && attendanceData.map(data => data.state);
     const employmentCount =
       stateData && stateData.filter(state => state.eight === "취업").length;
-    const attendanceCountArr = stateData && [
-      stateData.filter(state => state.seven === "출석").length,
-      stateData.filter(state => state.eight === "출석").length,
-      stateData.filter(state => state.nine === "출석").length,
-      stateData.filter(state => state.ten === "출석").length
-    ];
+
+    const checkArrAll = useCallback(() => {
+      setCheckArr(
+        Array.from(
+          { length: attendanceData ? attendanceData.length : 0 },
+          () => !checkAllState
+        )
+      );
+    }, [checkAllState, attendanceData]);
+    const handleCheckArr = useCallback(
+      index => {
+        setCheckArr(
+          checkArr.map((check, mapIndex) =>
+            mapIndex === index ? !check : check
+          )
+        );
+      },
+      [checkArr]
+    );
+
+    useEffect(() => {
+      setCheckAllState(
+        checkArr.every(check => check === checkArr[0]) ? checkArr[0] : false
+      );
+    }, [checkArr]);
+
+    useEffect(() => {
+      setCheckArr(
+        Array.from(
+          { length: attendanceData ? attendanceData.length : 0 },
+          () => false
+        )
+      );
+    }, [attendanceData]);
 
     return (
       <S.Container>
@@ -47,7 +77,11 @@ const AttendanceSection = props => {
             <S.Location location="창조실">{`창조실`}</S.Location>
           </S.Article>
         )}
-        <AttendanceRowTop isSevenNull={isSevenNull} />
+        <AttendanceRowTop
+          isSevenNull={isSevenNull}
+          checkArrAll={checkArrAll}
+          checkAllState={checkAllState}
+        />
         <S.Attendance>
           {attendanceData.length &&
             attendanceData.map((attendance, index) => (
@@ -55,6 +89,8 @@ const AttendanceSection = props => {
                 key={attendance.gradeClassNumber}
                 index={index}
                 attendance={attendance}
+                checkArr={checkArr}
+                handleCheckArr={handleCheckArr}
               />
             ))}
         </S.Attendance>
