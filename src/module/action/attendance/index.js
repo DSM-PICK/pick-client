1e10dc42561befe34fa435687d8c2499f701216d
유시온 <dbtldhs0915@naver.com>
2021-03-18 09:54:59 +0900

    [FEAT] <src/module/action/attendance/index.js>

--- src/module/action/attendance/index.js
+++ src/module/action/attendance/index.js
@@ -15,6 +15,8 @@ export const SET_SCHEDULE = "attendance/SET_SCHEDULE";
 export const SET_CLUB_NAME = "attendance/SET_CLUB_NAME";
 export const SET_HEAD = "attendance/SET_HEAD";
 
+export const SET_CURRENT_CLASS_INFO = "attendance/SET_CURRENT_CLASS_INFO";
+
 export const GET_FLOOR_DATA_SAGA = "attendance/GET_FLOOR_DATA_SAGA";
 
 export const SET_ATTENDANCE_STD_DATA = "attendance/SET_ATTENDANCE_STD_DATA";
@@ -27,6 +29,9 @@ export const POST_ATTENDANCE_STD_DATA_SAGA =
   "attendance/POST_ATTENDANCE_STD_DATA_SAGA";
 export const FAILURE_POST_ATTENDANCE_STD_DATA_SAGA =
   "attendance/FAILURE_POST_ATTENDANCE_STD_DATA_SAGA";
+
+export const PUT_ATTENDANCE_STD_DATA_SAGA =
+  "attendance/PUT_ATTENDANCE_STD_DATA_SAGA";
 
 export const setSecondFloorData = payload => ({
   type: SET_SECOND_FLOOR_DATA,
@@ -93,6 +98,11 @@ export const setHead = payload => ({
   payload
 });
 
+export const setCurrentClassInfo = payload => ({
+  type: SET_CURRENT_CLASS_INFO,
+  payload
+});
+
 export const getFloorDataSaga = payload => ({
   type: GET_FLOOR_DATA_SAGA,
   payload
@@ -120,5 +130,10 @@ export const postAttendanceStdDataSaga = payload => ({
 
 export const failurePostAttendanceStdDataSaga = payload => ({
   type: FAILURE_POST_ATTENDANCE_STD_DATA_SAGA,
+  payload
+});
+
+export const putAttendanceStdDataSaga = payload => ({
+  type: PUT_ATTENDANCE_STD_DATA_SAGA,
   payload
 });
