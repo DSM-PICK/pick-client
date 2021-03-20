9adc274927dd69d9b9f175e5cc10a65cbb66fcf0
유시온 <dbtldhs0915@naver.com>
2021-03-20 16:45:06 +0900

    [FEAT] check isLogin

--- src/container/Desktop/Main/DesktopMainContainer.js
+++ src/container/Desktop/Main/DesktopMainContainer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import Main from "../../../component/Desktop/Templates/Main/Main";
+import { checkIsLogin } from "../../../lib/requestApi";
 
 const DesktopMainContainer = () => {
+  const history = useHistory();
+  const [login, setLogin] = useState(false);
+  checkIsLogin().then(isLogin => {
+    if (!isLogin) {
+      alert("로그인 후 이용하실 수 있습니다.");
+      history.push("/auth");
+    }
+    setLogin(true);
+  }, []);
+
+  if (!login) {
+    return <div></div>;
+  }
+
   return <Main />;
 };
 
