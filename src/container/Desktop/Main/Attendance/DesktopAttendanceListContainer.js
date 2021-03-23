2e4dfad8343abb73f49e9ff8aed10f1f44ad1aa0
유시온 <dbtldhs0915@naver.com>
2021-03-23 17:00:50 +0900

    [FEAT] <src/container/~/DesktopAttendanceListContainer.js>

--- src/container/Desktop/Main/Attendance/DesktopAttendanceListContainer.js
+++ src/container/Desktop/Main/Attendance/DesktopAttendanceListContainer.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useCallback, useEffect, useState } from "react";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import AttendanceList from "../../../../component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/AttendanceList";
 import { DAttendanceActionCreater } from "../../../../module/action/d_attendance";
 
@@ -8,12 +8,23 @@ const DesktopAttendanceListContainer = props => {
   const [statesArr, setStatesArr] = useState(
     Object.values(stateList).filter(state => state)
   );
+  const { selectArr } = useSelector(state => state.dAttendance, shallowEqual);
+  const attendanceData = useSelector(state => state.dAttendance.attendanceData);
 
   const startPeriod = 11 - statesArr.length;
   const periodArr = [10, 9, 8, 7].slice(0, statesArr.length).reverse();
 
-  const { patchAttendanceStdDataSaga } = DAttendanceActionCreater;
+  const {
+    putAttendanceStdDataSaga,
+    patchAttendanceStdDataSaga
+  } = DAttendanceActionCreater;
   const dispatch = useDispatch();
+  const putAttendanceStdData = useCallback(
+    (numbers, periods, value) => {
+      dispatch(putAttendanceStdDataSaga({ numbers, periods, state: value }));
+    },
+    [dispatch, stdNum]
+  );
   const patchAttendanceStdData = useCallback(
     (period, value) => {
       dispatch(
@@ -22,7 +33,10 @@ const DesktopAttendanceListContainer = props => {
     },
     [dispatch, stdNum]
   );
+
   const onStateChange = useCallback((period, value) => {
+    // putAttendanceStdData(period, value);
+
     patchAttendanceStdData(period, value);
     viewChange(period, value);
   }, []);
