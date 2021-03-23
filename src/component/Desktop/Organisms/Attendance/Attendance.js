5b6bf894fbc30b8323259e549cda33f01f7635ee
유시온 <dbtldhs0915@naver.com>
2021-03-23 17:03:22 +0900

    [FEAT] <src/component/Desktop/Organisms/Attendance>

--- src/component/Desktop/Organisms/Attendance/Attendance.js
+++ src/component/Desktop/Organisms/Attendance/Attendance.js
@@ -1,7 +1,6 @@
 import React from "react";
 import DesktopAttendanceClassInfoContainer from "../../../../container/Desktop/Main/Attendance/DesktopAttendanceClassInfoContainer";
 import DesktopAttendancePlaceContainer from "../../../../container/Desktop/Main/Attendance/DesktopAttendancePlaceContainer";
-import AttendanceCurrent from "../../Molecules/Attendance/AttendanceCurrent/AttendanceCurrent";
 import AttendanceStdList from "../../Molecules/Attendance/AttendanceStdList/AttendanceStdList";
 import * as S from "./styles";
 
@@ -10,7 +9,6 @@ const Attendance = () => {
     <S.Container>
       <S.LeftGridWrap>
         <DesktopAttendancePlaceContainer />
-        <AttendanceCurrent />
       </S.LeftGridWrap>
       <S.RightWrap>
         <AttendanceStdList />
