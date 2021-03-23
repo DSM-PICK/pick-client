2a5be7819f07c564b7b31497f78bd142523753f3
유시온 <dbtldhs0915@naver.com>
2021-03-23 16:06:14 +0900

    [FEAT] <src/~/Molecules/~/StateItem>

---
+++ src/component/Desktop/Molecules/Temp/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/StateItem/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: ${props => (props.current === props.value ? 0 : 1)};
+  color: #000000;
+  font-weight: 400;
+
+  &:hover {
+    font-weight: ${props => (props.isOpen ? "bold" : "400")};
+  }
+
+  transition: all 0.1s ease-in-out;
+`;
