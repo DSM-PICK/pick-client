37cdbe4bd8eedc94ba6fb8ae21d204c69e0f1f6f
유시온 <dbtldhs0915@naver.com>
2021-03-23 17:07:17 +0900

    [FEAT] <src/component/~/Molecules/~/BackgroundHeader.js>

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/BackgroundHeader/BackgroundHeader.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/BackgroundHeader/BackgroundHeader.js
@@ -3,27 +3,36 @@ import * as S from "./styles";
 import Button from "../../../../../Atoms/Button/Button";
 
 const BackgroundHeader = props => {
-  const { teacherName, locationName, withoutGrantedClass } = props;
+  const { teacherName, locationName, nowUngranted } = props;
   const { onClickFastSearchBtn } = props;
 
   return (
     <S.Container>
       <S.TextWrap>
-        <S.HeaderText>{`${teacherName} 선생님은`}</S.HeaderText>
-        <S.HeaderText>{`${locationName} 담임입니다.`}</S.HeaderText>
+        {nowUngranted ? (
+          <>
+            <S.HeaderText>{`${teacherName} 선생님은`}</S.HeaderText>
+            <S.HeaderText>{`담당이 없습니다.`}</S.HeaderText>
+          </>
+        ) : (
+          <>
+            <S.HeaderText>{`${teacherName} 선생님은`}</S.HeaderText>
+            <S.HeaderText>{`${locationName} 담당입니다.`}</S.HeaderText>
+          </>
+        )}
       </S.TextWrap>
       <S.ButtonWrap>
         <Button
           text={"빠른 조회하기"}
           css={S.HeaderButtonCSS}
           onClick={() =>
-            withoutGrantedClass ? "" : onClickFastSearchBtn("빠른 조회하기")
+            nowUngranted ? "" : onClickFastSearchBtn("빠른 조회하기")
           }
-          buttonDisabled={withoutGrantedClass}
+          buttonDisabled={nowUngranted}
         />
       </S.ButtonWrap>
     </S.Container>
   );
 };
 
-export default BackgroundHeader;
+export default React.memo(BackgroundHeader);
