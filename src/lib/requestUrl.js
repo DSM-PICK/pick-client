f8ba95909af85189fcaad021699e708a91b969cd
유시온 <dbtldhs0915@naver.com>
2021-03-25 13:43:32 +0900

    [FEAT] <src/lib/requestUrl.js> Add URL

--- src/lib/requestUrl.js
+++ src/lib/requestUrl.js
@@ -15,6 +15,9 @@ export const ATTENDANCE = {
   },
   ACTIVITY_BY_DATE_URL: date => {
     return `/saturn/attendance/activity/${date}`;
+  },
+  SET_ATTENCANE_MEMO: () => {
+    return `/saturn/attendance/student-memo`;
   }
 };
 
