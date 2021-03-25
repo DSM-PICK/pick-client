23f9d5f37394a692c2e08dfdb20036582140ac21
유시온 <dbtldhs0915@naver.com>
2021-03-25 13:21:38 +0900

    [FEAT] <src/component/~/Molecules/~/ACGoOutBackground.js> Create File

---
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrentGoOut/AttendanceCurrentGoOutBackground/AttendanceCurrentGoOutBackground.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const AttendanceCurrentGoOutBackground = () => {
+  return <S.Container></S.Container>;
+};
+
+export default AttendanceCurrentGoOutBackground;
