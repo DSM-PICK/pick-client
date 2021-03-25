c9b700d75d390c87c3d36c052ef0f3e12cda3158
유시온 <dbtldhs0915@naver.com>
2021-03-25 19:43:54 +0900

    [FEAT] <src/component/~/Molecules/~/MemoItem.js>

--- src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/MemoItem/MemoItem.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceStdList/AttendanceStdListBackground/StdListGrid/AttendanceList/ItemBtn/MemoItem/MemoItem.js
@@ -1,47 +1,19 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
 import * as S from "./style";
 
 const MemoItem = props => {
   const { onWriteMemo, writeMemoByInput } = props;
-  const [selectIndex, setSelectIndex] = useState(0);
 
-  const FloorClassData = [
-    {
-      floor: "4층",
-      floorData: [
-        "세미나실 2-1",
-        "세미나실 2-2",
-        "세미나실 2-3",
-        "세미나실 2-4",
-        "1학년 1반",
-        "1학년 2반"
-      ]
-    },
-    {
-      floor: "3층",
-      floorData: [
-        "세미나실 3-1",
-        "세미나실 3-2",
-        "세미나실 3-3",
-        "세미나실 3-4"
-      ]
-    },
-    {
-      floor: "2층",
-      floorData: [
-        "세미나실 4-1",
-        "세미나실 4-2",
-        "세미나실 4-3",
-        "세미나실 4-4"
-      ]
-    },
-    {
-      floor: "입력",
-      floorData: []
-    }
-  ];
-  const FloorData = FloorClassData.map(data => data.floor);
-  const ClassData = FloorClassData.map(data => data.floorData);
+  const memoFloorData = useSelector(state => state.dAttendance.memoFloorData);
+
+  const [selectIndex, setSelectIndex] = useState(0);
+  const floorData = useMemo(() => memoFloorData.map(data => data.floor), [
+    memoFloorData
+  ]);
+  const classData = useMemo(() => memoFloorData.map(data => data.floorData), [
+    memoFloorData
+  ]);
 
   const onClickFloor = useCallback(idx => {
     if (idx === 3) {
@@ -57,14 +29,14 @@ const MemoItem = props => {
   return (
     <S.Container>
       <S.FloorWrap>
-        {FloorData.map((data, mapIndex) => (
+        {floorData.map((data, mapIndex) => (
           <S.FloorItem key={data} onClick={() => onClickFloor(mapIndex)}>
             {data}
           </S.FloorItem>
         ))}
       </S.FloorWrap>
       <S.ClassWrap>
-        {ClassData[selectIndex].map(data => (
+        {classData[selectIndex].map(data => (
           <S.ClassItem key={data} onClick={() => onClickClass(data)}>
             {data}
           </S.ClassItem>
