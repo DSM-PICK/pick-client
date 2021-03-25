d941263d4088c02526e7c41ff3fa1c4b141b7135
유시온 <dbtldhs0915@naver.com>
2021-03-25 16:49:32 +0900

    [FEAT] <src/container/~/DACurrentGoOutContainer.js>

--- src/container/Desktop/Main/Attendance/DACurrentGoOutContainer.js
+++ src/container/Desktop/Main/Attendance/DACurrentGoOutContainer.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AttendanceCurrentGoOut from "../../../../component/Desktop/Molecules/Attendance/AttendanceCurrentGoOut/AttendanceCurrentGoOut";
+import { useDispatch, useSelector } from "react-redux";
+import { DAttendanceActionCreater } from "../../../../module/action/d_attendance";
 
 const DACurrentGoOutContainer = () => {
+  const dispatch = useDispatch();
+  const schedule = useSelector(state => state.toggle.selectSchedule);
+
+  const { getStudentByStateSaga } = DAttendanceActionCreater;
+
+  const studentState = "외출";
+
+  useEffect(() => {
+    dispatch(getStudentByStateSaga({ schedule, studentState }));
+  }, [schedule]);
+
   return <AttendanceCurrentGoOut />;
 };
 
