93dac64d0707917d81dc8ecb0fa59c557c292657
유시온 <dbtldhs0915@naver.com>
2021-03-25 14:17:03 +0900

    [FEAT] <src/container/~/DACurrentGoOutContaner.js>  Create File

---
+++ src/container/Desktop/Main/Attendance/DACurrentGoOutContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AttendanceCurrentGoOut from "../../../../component/Desktop/Molecules/Attendance/AttendanceCurrentGoOut/AttendanceCurrentGoOut";
+
+const DACurrentGoOutContainer = () => {
+  return <AttendanceCurrentGoOut />;
+};
+
+export default DACurrentGoOutContainer;
