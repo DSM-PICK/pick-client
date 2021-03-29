c16c3370ec7ddd3a81ea537dcf540376418cb4a8
유시온 <dbtldhs0915@naver.com>
2021-03-29 17:52:06 +0900

    [FEAT] <src/component/~/MemoItem> Apply Memo

--- src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/AttendanceCell/MemoItem/MemoItem.js
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/AttendanceCell/MemoItem/MemoItem.js
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux";
 import * as S from "./style";
 
 const MemoItem = props => {
-  const { onWriteMemo, writeMemoByInput } = props;
+  const { index, period, inputSelectButtonRef, checkArr } = props;
+  const { onWriteMemo, writeMemoByInput, setShowMemoSelect } = props;
 
   const memoFloorData = useSelector(state => state.dAttendance.memoFloorData);
 
@@ -17,23 +18,38 @@ const MemoItem = props => {
 
   const onClickFloor = useCallback(idx => {
     if (idx === 3) {
+      setShowMemoSelect(false);
       writeMemoByInput();
     } else {
       setSelectIndex(idx);
     }
   }, []);
-  const onClickClass = useCallback(className => {
-    onWriteMemo(className);
-  }, []);
+  const onClickClass = useCallback(
+    className => {
+      onWriteMemo(className, period, checkArr);
+    },
+    [period, checkArr]
+  );
 
   return (
     <S.Container>
-      <S.FloorWrap>
-        {floorData.map((data, mapIndex) => (
-          <S.FloorItem key={data} onClick={() => onClickFloor(mapIndex)}>
-            {data}
-          </S.FloorItem>
-        ))}
+      <S.FloorWrap className={`input-select-button-wrap-${index}`}>
+        {floorData.map((data, mapIndex) =>
+          mapIndex === 3 ? (
+            <S.FloorItem
+              key={data}
+              ref={inputSelectButtonRef}
+              className={`input-select-button-${index}`}
+              onClick={() => onClickFloor(mapIndex)}
+            >
+              {data}
+            </S.FloorItem>
+          ) : (
+            <S.FloorItem key={data} onClick={() => onClickFloor(mapIndex)}>
+              {data}
+            </S.FloorItem>
+          )
+        )}
       </S.FloorWrap>
       <S.ClassWrap>
         {classData[selectIndex].map(data => (
