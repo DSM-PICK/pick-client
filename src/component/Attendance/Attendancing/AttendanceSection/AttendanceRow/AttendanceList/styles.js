e20bf110fab9b91518b01e6a7530956c06249230
cutyapple <dbtldhs0915@naver.com>
2021-03-31 12:05:38 +0900

    [FEAT] <src/~/AttendanceList> Apply Container

---
+++ src/component/Attendance/Attendancing/AttendanceSection/AttendanceRow/AttendanceList/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Containter = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`;
+
+export const CellWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
