375f58294e193c1dbad651336e2ffb6fa7cb109d
cutyapple <dbtldhs0915@naver.com>
2021-03-31 19:57:17 +0900

    [FEAT] <src/container/~/DSStdListBodyContainer.js> Apply Store

--- src/container/Desktop/Stats/DSStdListBodyContainer.js
+++ src/container/Desktop/Stats/DSStdListBodyContainer.js
@@ -1,10 +1,11 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import SStdListBody from "../../../component/Desktop/Molecules/Stats/StatsStdList/StatsStdListBackground/SStdListBody/SStdListBody";
 import { makeDate2Digit } from "../../../lib/attendanceApi";
 import { DStatsActionCreater } from "../../../module/action/d_stats";
 
 const DSStdListBodyContainer = () => {
+  const dispatch = useDispatch();
   const {
     clickedFloor: clickedFloorArr,
     clickedPriority: clickedPriorityArr,
@@ -15,13 +16,41 @@ const DSStdListBodyContainer = () => {
   const selectedDateStr = `${year}-${makeDate2Digit(month)}-${makeDate2Digit(
     day
   )}`;
-  const dispatch = useDispatch();
-  const { getSAttendanceDataSaga } = DStatsActionCreater;
+  const { calander } = useSelector(store => store.schedule.mini);
+  const { selected } = useSelector(store => store.schedule);
+
+  const [prevFloorArr, setPrevFloorArr] = useState([]);
+
+  const {
+    getSAttendanceDataSaga,
+    setSClickedPriorityArrPriority,
+    setSAttendanceData
+  } = DStatsActionCreater;
+  const dispatchSetSAttendanceData = useCallback(
+    statsAttendance => {
+      dispatch(setSAttendanceData({ statsAttendance }));
+    },
+    [dispatch]
+  );
+  const dispatchSetSClickedPriorityDispatch = useCallback(
+    priority => {
+      dispatch(
+        setSClickedPriorityArrPriority({ clickedPriorityArrPriority: priority })
+      );
+    },
+    [dispatch]
+  );
   const getSAttendanceDataSagaDispatch = useCallback(
     payload => {
       dispatch(getSAttendanceDataSaga(payload));
     },
     [dispatch]
+  );
+  const selectObj = calander.find(
+    ({ year, month, date }) =>
+      year === selected.year &&
+      month === selected.month &&
+      date === selected.day
   );
 
   const floorText2apiFloorText = {
@@ -35,16 +64,35 @@ const DSStdListBodyContainer = () => {
     .filter(data => data);
 
   useEffect(() => {
-    getSAttendanceDataSagaDispatch({
-      schedule: "self-study",
-      floor: clickedFloorText,
-      priority: clickedPriorityArrPriority,
-      date: selectedDateStr
-    });
+    const isFloorChange =
+      JSON.stringify(prevFloorArr.map(data => data.text)) !==
+      JSON.stringify(clickedPriorityArr.map(data => data.text));
+
+    setPrevFloorArr(prev => (isFloorChange ? clickedPriorityArr : prev));
+
+    if (selectObj && selectObj.schedule === "non-schedule") {
+      dispatchSetSAttendanceData([]);
+    } else {
+      if (clickedPriorityArr.length && selectObj) {
+        getSAttendanceDataSagaDispatch({
+          schedule: selectObj.schedule,
+          floor: clickedFloorText,
+          priority: isFloorChange
+            ? clickedPriorityArr[0].priority
+            : clickedPriorityArr[clickedPriorityArrPriority].priority,
+          date: selectedDateStr
+        });
+      }
+    }
+    if (isFloorChange) {
+      dispatchSetSClickedPriorityDispatch(0);
+    }
   }, [
+    selectObj,
     clickedFloorText,
     clickedPriorityArrPriority,
     clickedPriorityArr,
+    prevFloorArr,
     selectedDateStr
   ]);
 
