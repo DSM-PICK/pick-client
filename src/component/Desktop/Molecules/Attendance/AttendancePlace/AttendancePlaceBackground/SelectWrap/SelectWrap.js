8fbecc0e747384717f900e6a83fae599b966e513
cutyapple <dbtldhs0915@naver.com>
2021-04-01 20:22:11 +0900

    [FEAT] <src/~/Molecules/~/SelectWrap.js> Apply todaySchedule

--- src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
+++ src/component/Desktop/Molecules/Attendance/AttendancePlace/AttendancePlaceBackground/SelectWrap/SelectWrap.js
@@ -1,13 +1,23 @@
 import React from "react";
 import * as S from "./styles";
+import NoScheduleWrap from "../../../../../../NoScheduleWrap/NoScheduleWrap";
+import { NonScheduleGif } from "../../../../../../../asset/Desktop";
 
 const SelectWrap = props => {
-  const { selectArr, currentArr, selectFloorArray } = props;
+  const { selectArr, currentArr, selectFloorArray, todaySchedule } = props;
   const { onClick } = props;
 
   const selectWrapData = selectFloorArray[1]?.bodyItem?.length
     ? selectFloorArray
     : selectArr;
+
+  if (todaySchedule === "self-study") {
+    return (
+      <S.Container>
+        <NoScheduleWrap text={"오늘은 일정이 없습니다."} gif={NonScheduleGif} />
+      </S.Container>
+    );
+  }
 
   return (
     <S.Container>
