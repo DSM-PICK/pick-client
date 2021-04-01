ea6607bded24b4adfee189962c980968ad92f52d
cutyapple <dbtldhs0915@naver.com>
2021-04-01 11:09:03 +0900

    [MODIFY] <src/~/Molecules/~/AttendanceCurrentGoOutBg/style.js> FIX CSS

--- src/component/Desktop/Molecules/Attendance/AttendanceCurrentGoOut/AttendanceCurrentGoOutBackground/style.js
+++ src/component/Desktop/Molecules/Attendance/AttendanceCurrentGoOut/AttendanceCurrentGoOutBackground/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-rows: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 1fr);
   width: 100%;
   height: 100%;
   padding: 12px 15px;
@@ -15,6 +15,7 @@ export const FloorWrap = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+  overflow: scroll;
 `;
 
 export const FloorTitle = styled.p`
