f226b586c942e2a7735f202226a7a4c80bcdb3cf
cutyapple <dbtldhs0915@naver.com>
2021-04-03 23:39:59 +0900

    [MODIFY] <src/~/Molecules/~/styles.js> Modify CSS

--- src/component/NoScheduleWrap/style.js
+++ src/component/NoScheduleWrap/style.js
@@ -6,6 +6,8 @@ export const Container = styled.div`
 `;
 
 export const Text = styled.p`
+  position: absolute;
+  top: calc(50% + 130px);
   display: flex;
   align-items: flex-end;
   box-sizing: border-box;
@@ -15,10 +17,12 @@ export const Text = styled.p`
 `;
 
 export const GifWrapper = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   width: 100%;
   height: 100%;
+
   padding: ${props => props.text || "30px 0"};
   background: ${props => `url(${props.gif}) no-repeat ${props.gifSize}`};
 `;
