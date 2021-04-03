c3b76c23658e5df6bff555a99207bd82797d31db
cutyapple <dbtldhs0915@naver.com>
2021-04-03 15:07:25 +0900

    [FEAT] <src/~/Molecules/~/SStdListBody.js> Set Error Handling

--- src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdListBackground/SStdListBody/SStdListBody.js
+++ src/component/Desktop/Molecules/Stats/StatsStdList/StatsStdListBackground/SStdListBody/SStdListBody.js
@@ -1,26 +1,37 @@
 import React from "react";
+import { NonScheduleGif } from "../../../../../../../asset/Desktop";
+import NoScheduleWrap from "../../../../../../NoScheduleWrap/NoScheduleWrap";
 import SStdListGrid from "../SStdListGrid/SStdListGrid";
 import * as S from "./styles";
 
 const SStdListBody = props => {
   const { statsAttendance } = props;
 
+  if (!statsAttendance.length) {
+    return (
+      <NoScheduleWrap
+        text="선택하신 날은 데이터가 없어요..."
+        gif={NonScheduleGif}
+        gifSize="53% 40% / 60%"
+        textPadding="240px 0"
+      />
+    );
+  }
+
   return (
     <S.Container>
-      {statsAttendance.length
-        ? statsAttendance.map((data, index) => (
-            <SStdListGrid
-              key={data.gradeClassNumber}
-              index={index}
-              name={data.name}
-              stdNum={data.gradeClassNumber}
-              stateList={data.state}
-              reason={data.reason}
-              memo={data.memo}
-              css={S.StdListGridCSS}
-            />
-          ))
-        : "학생 데이터가 없습니다."}
+      {statsAttendance.map((data, index) => (
+        <SStdListGrid
+          key={data.gradeClassNumber}
+          index={index}
+          name={data.name}
+          stdNum={data.gradeClassNumber}
+          stateList={data.state}
+          reason={data.reason}
+          memo={data.memo}
+          css={S.StdListGridCSS}
+        />
+      ))}
     </S.Container>
   );
 };
