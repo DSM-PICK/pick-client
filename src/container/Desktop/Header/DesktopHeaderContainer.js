348a3c142093601f24bc2b537d3a1b876725cd5a
CutyApple <dbtldhs0915@naver.com>
2021-05-18 15:41:25 +0900

    [FEAT] <src/~/DesktopHeaderContainer.js> add onChangePassword function

--- src/container/Desktop/Header/DesktopHeaderContainer.js
+++ src/container/Desktop/Header/DesktopHeaderContainer.js
@@ -1,14 +1,25 @@
 import React, { useCallback } from "react";
+import { useHistory } from "react-router";
 import Header from "../../../component/Desktop/Organisms/Header/Header";
 import { Logout } from "../../../lib/requestApi";
 
 const DesktopHeaderContainer = () => {
+  const history = useHistory();
   const userName = localStorage.getItem("teacherName");
   const onLogout = useCallback(() => {
     Logout("desktop");
   });
+  const onChangePassword = useCallback(() => {
+    history.push("/password-change");
+  });
 
-  return <Header userName={userName} onLogout={onLogout} />;
+  return (
+    <Header
+      userName={userName}
+      onLogout={onLogout}
+      onChangePassword={onChangePassword}
+    />
+  );
 };
 
 export default React.memo(DesktopHeaderContainer);
